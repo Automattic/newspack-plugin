19ac27e25acf6345efbfb0961ace4937b4a577ee
claudiulodro <claudiulodro@gmail.com>
2019-02-28 10:29:37 -0800

    Minimal plugin

---
+++ newspack.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Newspack
+ * Description: An advanced open-source publishing and revenue-generating platform for news organizations.
+ * Version: 0.0.1-alpha
+ * Author: Automattic
+ * Author URI: https://newspack.blog/
+ * License: GPL3
+ * Text Domain: newspack
+ * Domain Path: /languages/
+ */
+
+defined( 'ABSPATH' ) || exit;
