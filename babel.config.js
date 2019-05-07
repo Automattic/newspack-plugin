3a83dfe4e0d4f39934bdbd0862e078b3f59225fe
claudiulodro <claudiulodro@gmail.com>
2019-05-07 07:10:12 -0700

    Would be fixed if not for recent style changes

---
+++ babel.config.js
@@ -0,0 +1,39 @@
+/*module.exports = {
+	extends: require.resolve( '@automattic/calypso-build/babel.config.js' ),
+	presets: [  ],
+
+};*/
+const config = {
+	presets: [
+		[
+			'@babel/env',
+			{
+				useBuiltIns: 'entry',
+				corejs: 2,
+				// Exclude transforms that make all code slower, see https://github.com/facebook/create-react-app/pull/5278
+				exclude: [ 'transform-typeof-symbol' ],
+			},
+		],
+		'@babel/react',
+		'@babel/preset-typescript',
+		require( '@automattic/calypso-build/babel/wordpress-element' )
+	],
+	plugins: [
+		'@babel/plugin-proposal-class-properties',
+		'@babel/plugin-proposal-export-default-from',
+		'@babel/plugin-proposal-export-namespace-from',
+		'@babel/plugin-syntax-dynamic-import',
+		[
+			'@babel/transform-runtime',
+			{
+				corejs: false, // we polyfill so we don't need core-js
+				helpers: true,
+				regenerator: false,
+				useESModules: false,
+			},
+		],
+	],
+};
+
+module.exports = config;
+
