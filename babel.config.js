b763b324a83f48fe84c4c00451455934d9e8feee
claudiulodro <claudiulodro@gmail.com>
2019-05-08 06:37:26 -0700

    Simplified babel config

--- babel.config.js
+++ babel.config.js
@@ -1,34 +1,7 @@
 const config = {
 	presets: [
-		[
-			'@babel/env',
-			{
-				useBuiltIns: 'entry',
-				corejs: 2,
-				// Exclude transforms that make all code slower, see https://github.com/facebook/create-react-app/pull/5278
-				exclude: [ 'transform-typeof-symbol' ],
-			},
-		],
-		'@babel/react',
-		'@babel/preset-typescript',
-		require( '@automattic/calypso-build/babel/wordpress-element' )
-	],
-	plugins: [
-		'@babel/plugin-proposal-class-properties',
-		'@babel/plugin-proposal-export-default-from',
-		'@babel/plugin-proposal-export-namespace-from',
-		'@babel/plugin-syntax-dynamic-import',
-		[
-			'@babel/transform-runtime',
-			{
-				corejs: false, // we polyfill so we don't need core-js
-				helpers: true,
-				regenerator: false,
-				useESModules: false,
-			},
-		],
-	],
+		'@automattic/calypso-build/babel/default',
+		'@automattic/calypso-build/babel/wordpress-element',
+	]
 };
-
 module.exports = config;
-
