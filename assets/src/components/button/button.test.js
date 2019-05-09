8148495b316e823d4a158f9f8fac15c6406002fb
Philip John <phil@philipjohn.me.uk>
2019-05-09 18:21:31 +0100

    More tests for other button styles

--- assets/src/components/button/button.test.js
+++ assets/src/components/button/button.test.js
@@ -9,10 +9,22 @@ describe( 'Button', () => {
 			expect( button.render().hasClass( 'newspack-button' ) ).toBe( true );
 		} );
 	} );
+	describe( 'rendering default', () => {
+		it( 'should render a Button element with a class of is-default', () => {
+			const button = shallow( <Button isDefault>Continue</Button> );
+			expect( button.render().hasClass( 'is-default' ) ).toBe( true );
+		} );
+	} );
 	describe( 'rendering primary', () => {
 		it( 'should render a Button element with a class of is-primary', () => {
 			const button = shallow( <Button isPrimary>Continue</Button> );
 			expect( button.render().hasClass( 'is-primary' ) ).toBe( true );
+		} );
+	} );
+	describe( 'rendering tertiary', () => {
+		it( 'should render a Button element with a class of is-tertiary', () => {
+			const button = shallow( <Button isTertiary>Continue</Button> );
+			expect( button.render().hasClass( 'is-tertiary' ) ).toBe( true );
 		} );
 	} );
 } );
