91620dfc0599c01a8f8a7aa6a91dca0badc901ce
Philip John <phil@philipjohn.me.uk>
2019-05-09 18:19:58 +0100

    Ensure tests run on the generated HTML, not the React component

--- assets/src/components/button/button.test.js
+++ assets/src/components/button/button.test.js
@@ -6,7 +6,13 @@ describe( 'Button', () => {
 	describe( 'basic rendering', () => {
 		it( 'should render a Button element with a value of "Continue"', () => {
 			const button = shallow( <Button isPrimary>Continue</Button> );
-			expect( button.hasClass( 'newspack-button' ) ).toBe( true );
+			expect( button.render().hasClass( 'newspack-button' ) ).toBe( true );
+		} );
+	} );
+	describe( 'rendering primary', () => {
+		it( 'should render a Button element with a class of is-primary', () => {
+			const button = shallow( <Button isPrimary>Continue</Button> );
+			expect( button.render().hasClass( 'is-primary' ) ).toBe( true );
 		} );
 	} );
 } );
