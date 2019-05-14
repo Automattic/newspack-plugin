f81a0dc01f6ca2b9bd635d809dc1f3e7d1e0bcc6
Jefferson Rabb <jeff@atavist.net>
2019-05-14 10:48:57 -0400

    Progress bar implementation.

--- assets/src/components/checklist/index.js
+++ assets/src/components/checklist/index.js
@@ -10,6 +10,7 @@ import { Component, Children, cloneElement } from '@wordpress/element';
 /**
  * Internal dependencies.
  */
+import ProgressBar from '../progressBar';
 import './style.scss';
 
 class Checklist extends Component {
@@ -17,11 +18,11 @@ class Checklist extends Component {
 	 * Render.
 	 */
 	render() {
-		const { children, currentTask } = this.props;
+		const { children, currentTask, progressBarText } = this.props;
 		return (
 			<div className="muriel-checklist">
 				<div className="muriel-checklist__progress-bar">
-					OK
+					<ProgressBar completed={ currentTask } total={ Children.count( children ) } displayFraction label={ progressBarText } />
 				</div>
 				<div className="muriel-checklist__tasks">
 					{ Children.map( children, ( child, index ) => {
