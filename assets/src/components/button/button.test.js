e4087f1772e4ece7bf1bed81d376f42d0a0adb9d
Philip John <phil@philipjohn.me.uk>
2019-05-14 08:15:51 +0100

    Fix tests after we removed our wrapping div

--- assets/src/components/button/button.test.js
+++ assets/src/components/button/button.test.js
@@ -12,19 +12,19 @@ describe( 'Button', () => {
 	describe( 'rendering default', () => {
 		it( 'should render a Button element with a class of is-default', () => {
 			const button = shallow( <Button isDefault>Continue</Button> );
-			expect( button.render().children('button').hasClass( 'is-default' ) ).toBe( true );
+			expect( button.render().hasClass( 'is-default' ) ).toBe( true );
 		} );
 	} );
 	describe( 'rendering primary', () => {
 		it( 'should render a Button element with a class of is-primary', () => {
 			const button = shallow( <Button isPrimary>Continue</Button> );
-			expect( button.render().children('button').hasClass( 'is-primary' ) ).toBe( true );
+			expect( button.render().hasClass( 'is-primary' ) ).toBe( true );
 		} );
 	} );
 	describe( 'rendering tertiary', () => {
 		it( 'should render a Button element with a class of is-tertiary', () => {
 			const button = shallow( <Button isTertiary>Continue</Button> );
-			expect( button.render().children('button').hasClass( 'is-tertiary' ) ).toBe( true );
+			expect( button.render().hasClass( 'is-tertiary' ) ).toBe( true );
 		} );
 	} );
 } );
