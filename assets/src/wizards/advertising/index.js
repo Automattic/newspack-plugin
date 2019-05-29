5f3e9556aacac4f46d5553ead6e56740cd1bee96
Philip John <phil@philipjohn.me.uk>
2019-05-29 16:33:36 +0100

    Improved wording, basic buttons, awaiting the ActionCard component

--- assets/src/wizards/advertising/index.js
+++ assets/src/wizards/advertising/index.js
@@ -9,6 +9,7 @@ import { Component, Fragment, render } from '@wordpress/element';
 import { ToggleControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
+
 /**
  * Internal dependencies
  */
@@ -24,77 +25,40 @@ import './style.scss';
  */
 class Advertising extends Component {
 	/**
-	 * constructor. Demo of how the parent interacts with the components, and controls their values.
-	 */
-	constructor() {
-		super( ...arguments );
-		this.state = {
-			wordads: 0,
-			adsense: 0,
-			admanager: 0,
-		};
-	}
-
-	/**
 	 * Render the example stub.
 	 */
 	render() {
-		const {
-			wordads,
-			adsense,
-			admanager,
-		} = this.state;
-
 		return (
 			<Fragment>
 				<FormattedHeader
-					headerText={ __( 'Advertising' ) }
+					headerText={ __( 'Which Ad Service would you like to use?' ) }
 					subHeaderText={ __( 'Enhance your Newspack site with advertising. Choose from any combination of the products below.' ) }
 				/>
 				<Card className="newspack-manage-advertising-screen__advertising-card">
-					<ToggleControl
-						checked={ wordads }
-						onChange={ () => this.setState( ( state ) => ( { wordads: ! state.wordads } ) ) }
-					/>
+					<img src="http://placehold.it/16x16" />
 					<div className="newspack-manage-advertising-screen__advertising-card__product-info">
 						<div className="product-name">WordAds from WordPress.com</div>
 						<div className="product-price">The Bestest ad network thingy ever.</div>
 					</div>
-					{ wordads && (
-						<div className="newspack-manage-advertising-screen__advertising-card__product-actions">
-							<Button isPrimary>Configure</Button>
-						</div>
-					) }
+					<div className="newspack-manage-advertising-screen__advertising-card__product-actions">
+						<Button isTertiary>Set Up</Button>
+					</div>
 				</Card>
 				<Card className="newspack-manage-advertising-screen__advertising-card">
-					<ToggleControl
-						checked={ adsense }
-						onChange={ () => this.setState( ( state ) => ( { adsense: ! state.adsense } ) ) }
-					/>
+					<img src="http://placehold.it/16x16" />
 					<div className="newspack-manage-advertising-screen__advertising-card__product-info">
 						<div className="product-name">Google AdWords</div>
 						<div className="product-price">The Bestest ad network thingy ever.</div>
 					</div>
-					{ adsense && (
-						<div className="newspack-manage-advertising-screen__advertising-card__product-actions">
-							<Button isPrimary>Configure</Button>
-						</div>
-					) }
+					<Button isTertiary>Set Up</Button>
 				</Card>
 				<Card className="newspack-manage-advertising-screen__advertising-card">
-					<ToggleControl
-						checked={ admanager }
-						onChange={ () => this.setState( ( state ) => ( { admanager: ! state.admanager } ) ) }
-					/>
+					<img src="http://placehold.it/16x16" />
 					<div className="newspack-manage-advertising-screen__advertising-card__product-info">
 						<div className="product-name">Google AdManager</div>
 						<div className="product-price">The Bestest ad network thingy ever.</div>
 					</div>
-					{ admanager && (
-						<div className="newspack-manage-advertising-screen__advertising-card__product-actions">
-							<Button isPrimary>Configure</Button>
-						</div>
-					) }
+					<Button isTertiary>Set Up</Button>
 				</Card>
 			</Fragment>
 		);
