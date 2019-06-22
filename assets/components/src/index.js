53ffb2cc1c7c85c804a089a13444e933b7d3bb61
Jefferson Rabb <jeff@atavist.net>
2019-06-22 18:07:45 -0400

    Clean up an unused component.

--- assets/components/src/index.js
+++ assets/components/src/index.js
@@ -9,7 +9,6 @@ export { default as InfoButton } from './info-button';
 export { default as ImageUpload } from './image-upload';
 export { default as Modal } from './modal';
 export { default as PluginInstaller } from './plugin-installer';
-export { default as PluginRequirements } from './plugin-requirements';
 export { default as ProgressBar } from './progress-bar';
 export { default as SelectControl } from './select-control';
 export { default as Task } from './task';
