946c15a19178f873181cfb7755c85ac43a8574d8
Jefferson Rabb <jeff@atavist.net>
2019-06-23 13:43:05 -0400

    Add withWizardScreen to components index.

--- assets/components/src/index.js
+++ assets/components/src/index.js
@@ -14,3 +14,4 @@ export { default as SelectControl } from './select-control';
 export { default as Task } from './task';
 export { default as TextControl } from './text-control';
 export { default as withWizard } from './with-wizard';
+export { default as withWizardScreen } from './with-wizard-screen';
