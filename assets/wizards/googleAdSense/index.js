9aaa363c6096ea701a135c286e3b1a9d5314607d
Claudiu Lodromanean <claudiulodro@gmail.com>
2019-07-02 11:23:41 -0700

    Clean up

--- assets/wizards/googleAdSense/index.js
+++ assets/wizards/googleAdSense/index.js
@@ -35,19 +35,27 @@ class GoogleAdSenseWizard extends Component {
 		};
 	}
 
+	/**
+	 * Check completion status when wizard is ready.
+	 */
 	onWizardReady = () => {
 		this.refreshComplete();
 	};
 
+	/**
+	 * When the wizard is marked complete, mark the checklist item complete.
+	 */
 	componentDidUpdate( prevProps, prevState ) {
 		const { complete: alreadyComplete } = prevState;
 		const { complete } = this.state;
 		if ( ! alreadyComplete && complete ) {
-			console.log('updating');
 			this.markWizardComplete();
 		}
 	}
 
+	/**
+	 * Get whether AdSense setup is complete.
+	 */
 	refreshComplete() {
 		const { setError } = this.props;
 		return apiFetch( { path: '/newspack/v1/wizard/newspack-google-adsense-wizard/adsense-setup-complete' } )
@@ -89,6 +97,9 @@ class GoogleAdSenseWizard extends Component {
 		} );
 	}
 
+	/**
+	 * Render.
+	 */
 	render() {
 		const { pluginRequirements } = this.props;
 		const { complete } = this.state;
@@ -106,7 +117,7 @@ class GoogleAdSenseWizard extends Component {
 									subHeaderText={ __( 'Connect to your AdSense account using the Site Kit plugin, then enable Auto Ads.' ) }
 								/>
 								{ complete && (
-									<div class='newspack-google-adsense-wizard__success'>
+									<div className='newspack-google-adsense-wizard__success'>
 										<Dashicon icon="yes-alt" />
 										<h4>{ __( 'AdSense is set up' ) }</h4>
 									</div>
