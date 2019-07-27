339da8138e807193fe7dca6ddb845d525387c30f
Jefferson Rabb <jeff@atavist.net>
2019-07-27 07:16:53 -0400

    Control visibility of Components Demo using "hidden" class variable.

--- includes/wizards/class-components-demo.php
+++ includes/wizards/class-components-demo.php
@@ -37,9 +37,7 @@ class Components_Demo extends Wizard {
 		parent::__construct();
 
 		// Only show a link to the Components Demo if WP_DEBUG is enabled.
-		if ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG ) {
-			add_action( 'admin_head', array( $this, 'hide_from_menus' ) );
-		}
+		$this->hidden = ! defined( 'WP_DEBUG' ) || ! WP_DEBUG;
 	}
 
 	/**
