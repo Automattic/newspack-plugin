a9bdb827cfbf31e9f7e23267d59f387c8fc67a9c
Jefferson Rabb <jeff@atavist.net>
2019-07-28 11:38:49 -0400

    Refactor of plugin installer component in Setup wizard to enable displaying the component as a normal view.

--- assets/wizards/setup/index.js
+++ assets/wizards/setup/index.js
@@ -13,8 +13,8 @@ import { Spinner } from '@wordpress/components';
 /**
  * Internal dependencies
  */
-import { About, ConfigurePlugins, Newsroom, Welcome } from './views/';
-import { Card, PluginInstaller, withWizard, WizardPagination } from '../../components/src';
+import { About, ConfigurePlugins, Newsroom, Welcome, InstallationProgress } from './views/';
+import { Card, withWizard, WizardPagination } from '../../components/src';
 import './style.scss';
 
 /**
@@ -146,128 +146,122 @@ class SetupWizard extends Component {
 		} = this.state;
 		const installProgress = Object.keys( installedPlugins ).length;
 		const installTotal = REQUIRED_PLUGINS.length;
+		const allPaths = [ '/', '/about', '/newsroom', '/configure-plugins', '/installation-progress' ];
 		return (
 			<Fragment>
 				<HashRouter hashType="slash">
 					<WizardPagination routes={ [ '/', '/about', '/newsroom', '/configure-plugins' ] } />
-					<Switch>
-						<Route
-							path="/"
-							exact
-							render={ routeProps => (
-								<Welcome
-									buttonText={ __( 'Get started' ) }
-									buttonAction={ {
-										href: '#/about',
-										onClick: () => this.updateProfile(),
-									} }
-									profile={ profile }
-								/>
-							) }
-						/>
-						<Route
-							path="/about"
-							render={ routeProps => (
-								<About
-									headerText={ __( 'About your publication' ) }
-									subHeaderText={ __(
-										'Share a few details so we can start setting up your profile'
-									) }
-									buttonText={ __( 'Continue' ) }
-									buttonAction={ {
-										href: '#/newsroom',
-										onClick: () => this.updateProfile(),
-									} }
-									profile={ profile }
-									currencies={ currencies }
-									countries={ countries }
-									updateProfile={ ( key, value ) => {
-										this.setState( { profile: { ...profile, [ key ]: value } } );
-									} }
-								/>
-							) }
-						/>
-						<Route
-							path="/newsroom"
-							render={ routeProps => (
-								<Newsroom
-									headerText={ __( 'Tell us about your Newsroom' ) }
-									subHeaderText={ __(
-										'The description helps set the stage for the step content below'
-									) }
-									buttonText={
-										installationComplete ? __( 'Continue' ) : <div>{ __( 'Plugins Installing') }<Spinner /></div>
-									}
-									buttonAction={ {
-										href: '#/configure-plugins',
-										onClick: () => this.updateProfile(),
-									} }
-									buttonDisabled={ ! installationComplete }
-									profile={ profile }
-									currencies={ currencies }
-									countries={ countries }
-									updateProfile={ ( key, value ) => {
-										this.setState( { profile: { ...profile, [ key ]: value } } );
-									} }
-								/>
-							) }
-						/>
-						<Route
-							path="/configure-plugins"
-							render={ routeProps => (
-								<ConfigurePlugins
-									noBackground
-									headerText={ __( 'Configure Core Plugins' ) }
-									subHeaderText={ __(
-										'You’re almost done. Please configure the following core plugins to start using Newspack.'
-									) }
-									buttonText={
-										this.isConfigurationComplete() ? __( 'Finish' ) : __( 'Start Configuration' )
-									}
-									buttonAction={
-										this.isConfigurationComplete()
-											? {
-													onClick: () =>
-														this.updateProfile().then( response =>
-															this.completeSetup().then(
-																() => ( window.location = newspack_urls.dashboard )
-															)
-														),
-											  }
-											: this.getFirstPluginToConfigure()
-									}
-									pluginInfoReady={ plugin => {
-										const { pluginInfo } = this.state;
-										this.setState( { pluginInfo: { ...pluginInfo, [ plugin.Slug ]: plugin } } );
-									} }
-								/>
-							) }
-						/>
-						<Redirect to="/" />
-					</Switch>
 					<Route
-						path={ [ '/about', '/newsroom' ] }
+						path="/"
+						exact
 						render={ routeProps => (
-							<div className="newspack-setup-wizard_plugin-installer">
-								<PluginInstaller
-									asProgressBar
-									plugins={ REQUIRED_PLUGINS }
-									onStatus={ status => this.setState( { installationComplete: status.complete } ) }
-								/>
-								{ ! installationComplete && (
-									<p className="newspack-setup-wizard_progress_bar_explainer">
-										{ __(
-											"We're installing the core plugins and Newspack theme in the background. You can navigate away from this page"
-										) }
-									</p>
-								) }
-								{ !! installationComplete && (
-									<p className="newspack-setup-wizard_progress_bar_explainer">
-										{ __( 'Plugin installation is complete!' ) }
-									</p>
-								) }
-							</div>
+							<Welcome
+								buttonText={ __( 'Get started' ) }
+								buttonAction={ {
+									href: '#/about',
+									onClick: () => this.updateProfile(),
+								} }
+								profile={ profile }
+							/>
 						) }
+					/>
+					<Route
+						path="/about"
+						render={ routeProps => (
+							<About
+								headerText={ __( 'About your publication' ) }
+								subHeaderText={ __(
+									'Share a few details so we can start setting up your profile'
+								) }
+								buttonText={ __( 'Continue' ) }
+								buttonAction={ {
+									href: '#/newsroom',
+									onClick: () => this.updateProfile(),
+								} }
+								profile={ profile }
+								currencies={ currencies }
+								countries={ countries }
+								updateProfile={ ( key, value ) => {
+									this.setState( { profile: { ...profile, [ key ]: value } } );
+								} }
+							/>
+						) }
+					/>
+					<Route
+						path="/newsroom"
+						render={ routeProps => (
+							<Newsroom
+								headerText={ __( 'Tell us about your Newsroom' ) }
+								subHeaderText={ __(
+									'The description helps set the stage for the step content below'
+								) }
+								buttonText={ __( 'Continue' ) }
+								buttonAction={ {
+									href: installationComplete ? '#/configure-plugins' : '#/installation-progress',
+									onClick: () => this.updateProfile(),
+								} }
+								profile={ profile }
+								currencies={ currencies }
+								countries={ countries }
+								updateProfile={ ( key, value ) => {
+									this.setState( { profile: { ...profile, [ key ]: value } } );
+								} }
+							/>
+						) }
+					/>
+					<Route
+						path="/configure-plugins"
+						render={ routeProps => (
+							<ConfigurePlugins
+								noBackground
+								headerText={ __( 'Configure Core Plugins' ) }
+								subHeaderText={ __(
+									'You’re almost done. Please configure the following core plugins to start using Newspack.'
+								) }
+								buttonText={
+									this.isConfigurationComplete() ? __( 'Finish' ) : __( 'Start Configuration' )
+								}
+								buttonAction={
+									this.isConfigurationComplete()
+										? {
+												onClick: () =>
+													this.updateProfile().then( response =>
+														this.completeSetup().then(
+															() => ( window.location = newspack_urls.dashboard )
+														)
+													),
+										  }
+										: this.getFirstPluginToConfigure()
+								}
+								pluginInfoReady={ plugin => {
+									const { pluginInfo } = this.state;
+									this.setState( { pluginInfo: { ...pluginInfo, [ plugin.Slug ]: plugin } } );
+								} }
+							/>
+						) }
+					/>
+					<Route
+						path={ [ '/about', '/newsroom', '/configure-plugins', '/installation-progress' ] }
+						render={ routeProps => (
+							<InstallationProgress
+								hidden={ '/installation-progress' !== routeProps.location.pathname }
+								noBackground
+								headerText={ __( 'Installation...' ) }
+								subHeaderText={ __(
+									'You’re almost done. Please configure the following core plugins to start using Newspack.'
+								) }
+								buttonText={ __( 'Continue' ) }
+								buttonAction={ '#/configure-plugins' }
+								buttonDisabled={ ! installationComplete }
+								plugins={ REQUIRED_PLUGINS }
+								onStatus={ status => this.setState( { installationComplete: status.complete } ) }
+							/>
+						) }
+					/>
+					<Route
+						render={ routeProps => {
+							return ( allPaths.indexOf( routeProps.location.pathname ) === -1 ) ? <Redirect to="/" /> : null;
+						} }
 					/>
 				</HashRouter>
 			</Fragment>
--- assets/wizards/setup/views/index.js
+++ assets/wizards/setup/views/index.js
@@ -2,3 +2,4 @@ export { default as About } from './about';
 export { default as Welcome } from './welcome';
 export { default as Newsroom } from './newsroom';
 export { default as ConfigurePlugins } from './configure-plugins';
+export { default as InstallationProgress } from './installation-progress';
