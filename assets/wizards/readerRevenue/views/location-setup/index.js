dca2c0556c72e31741265759e55e1a640856a8c1
Jefferson Rabb <jeff@atavist.net>
2019-08-03 09:30:46 -0400

    Location form (no model yet).

--- assets/wizards/readerRevenue/views/location-setup/index.js
+++ assets/wizards/readerRevenue/views/location-setup/index.js
@@ -5,12 +5,13 @@
 /**
  * WordPress dependencies
  */
-import { Component } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { Component, Fragment } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
-import { withWizardScreen } from '../../../../components/src';
+import { SelectControl, TextControl, withWizardScreen } from '../../../../components/src';
 
 /**
  * Location Setup Screen Component
@@ -20,8 +21,52 @@ class LocationSetup extends Component {
 	 * Render.
 	 */
 	render() {
-		return <p>Location Setup TK</p>;
+		const { countryStateFields, currencyFields, location, onChange } = this.props;
+		const { address1, address2, city, countrystate, currency, postcode } = location;
+		return (
+			<Fragment>
+				<SelectControl
+					label={ __( 'Where is your business based?' ) }
+					value={ countrystate }
+					options={ countryStateFields }
+					onChange={ countrystate => onChange( { ...location, countrystate: value } ) }
+				/>
+				<TextControl
+					label={ __( 'Address' ) }
+					value={ address1 }
+					onChange={ address1 => onChange( { ...location, address1 } ) }
+				/>
+				<TextControl
+					label={ __( 'Address line 2' ) }
+					value={ address2 }
+					onChange={ address2 => onChange( { ...location, address2 } ) }
+				/>
+				<TextControl
+					label={ __( 'City' ) }
+					value={ city }
+					onChange={ city => onChange( { ...location, city } ) }
+				/>
+				<TextControl
+					label={ __( 'Postcode / Zip' ) }
+					value={ postcode }
+					onChange={ postcode => onChange( { ...location, postcode } ) }
+				/>
+				<SelectControl
+					label={ 'Which currency does your business use?' }
+					value={ currency }
+					options={ currencyFields }
+					onChange={ currency => onChange( { ...location, currency } ) }
+				/>
+			</Fragment>
+		);
 	}
 }
+
+LocationSetup.defaultProps = {
+	countryStateFields: [ {} ],
+	currencyFields: [ {} ],
+	location: {},
+	onChange: () => null,
+};
 
 export default withWizardScreen( LocationSetup );
