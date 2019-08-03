32413ba5024c2682a84848308786dbc12ef64c32
Jefferson Rabb <jeff@atavist.net>
2019-08-03 13:14:17 -0400

    Move more WooCommerce functionality into the configuration manager.

--- includes/configuration_managers/class-woocommerce-configuration-manager.php
+++ includes/configuration_managers/class-woocommerce-configuration-manager.php
@@ -7,7 +7,7 @@
 
 namespace Newspack;
 
-use \WC_Payment_Gateways;
+use \WC_Payment_Gateways, \WC_Install, \WP_Error;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -90,11 +90,11 @@ class WooCommerce_Configuration_Manager extends Configuration_Manager {
 	}
 
 	/**
-	 * Retrieve payment data
+	 * Retrieve Stripe data
 	 *
-	 * @return Array Array of data.
+	 * @return Array Array of Stripe data.
 	 */
-	public function payment_data() {
+	public function stripe_data() {
 		$gateways = WC_Payment_Gateways::instance()->payment_gateways();
 		if ( ! isset( $gateways['stripe'] ) ) {
 			return [
@@ -132,5 +132,64 @@ class WooCommerce_Configuration_Manager extends Configuration_Manager {
 			'postcode'     => WC()->countries->get_base_postcode(),
 			'currency'     => get_woocommerce_currency(),
 		];
+	}
+
+	/**
+	 * Update WooCommerce Location Data
+	 *
+	 * @param Array $args Address data.
+	 * @return Array The data that was updated.
+	 */
+	public function update_location( $args ) {
+		update_option( 'woocommerce_store_address', $args['address1'] );
+		update_option( 'woocommerce_store_address_2', $args['address2'] );
+		update_option( 'woocommerce_store_city', $args['city'] );
+		update_option( 'woocommerce_default_country', $args['countrystate'] );
+		update_option( 'woocommerce_store_postcode', $args['postcode'] );
+		update_option( 'woocommerce_currency', $args['currency'] );
+		return true;
+	}
+
+	/**
+	 * Update WooCommerce Stripe settings
+	 *
+	 * @param Array $args Address data.
+	 * @return Array|WP_Error The data that was updated or an error.
+	 */
+	public function update_stripe_settings( $args ) {
+		$gateways = WC_Payment_Gateways::instance()->payment_gateways();
+		if ( ! isset( $gateways['stripe'] ) ) {
+			if ( $args['enabled'] ) {
+				// Stripe is not installed and we want to use it. Install/Activate/Initialize it.
+				Plugin_Manager::activate( 'woocommerce-gateway-stripe' );
+				do_action( 'plugins_loaded' );
+				WC_Payment_Gateways::instance()->init();
+				$gateways = WC_Payment_Gateways::instance()->payment_gateways();
+			} else {
+				// Stripe is not installed and we don't want to use it. No settings needed.
+				return true;
+			}
+		}
+
+		$stripe = $gateways['stripe'];
+		$stripe->update_option( 'enabled', $args['enabled'] ? 'yes' : 'no' );
+		$stripe->update_option( 'testmode', $args['testMode'] ? 'yes' : 'no' );
+		$stripe->update_option( 'publishable_key', $args['publishableKey'] );
+		$stripe->update_option( 'secret_key', $args['secretKey'] );
+		$stripe->update_option( 'test_publishable_key', $args['testPublishableKey'] );
+		$stripe->update_option( 'test_secret_key', $args['testSecretKey'] );
+		return true;
+	}
+
+	/**
+	 * Set general settings that our users will want (e.g. no reason for product reviews on a news membership).
+	 */
+	public function set_smart_defaults() {
+		// Create Shop, My Account, etc. pages if not already created.
+		WC_Install::create_pages();
+
+		// Disable coupons and reviews.
+		update_option( 'woocommerce_enable_coupons', 'no' );
+		update_option( 'woocommerce_enable_reviews', 'no' );
 	}
 }
