6274be4ccbf93efe47d93c22d325d5b929cd3677
claudiulodro <claudiulodro@gmail.com>
2019-08-21 11:42:13 -0700

    Bump version for release

--- newspack.php
+++ newspack.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Newspack
  * Description: An advanced open-source publishing and revenue-generating platform for news organizations.
- * Version: 0.0.1-alpha
+ * Version: 1.0.0-alpha.4
  * Author: Automattic
  * Author URI: https://newspack.blog/
  * License: GPL2
