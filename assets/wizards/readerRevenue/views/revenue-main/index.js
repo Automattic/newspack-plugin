83a701b0ade86d0386ebcf2a2710091ebdcc6e16
Thomas Guillot <thomasguillot@users.noreply.github.com>
2019-12-11 11:40:47 +0000

    Revenue Main: Update heading to h2

--- assets/wizards/readerRevenue/views/revenue-main/index.js
+++ assets/wizards/readerRevenue/views/revenue-main/index.js
@@ -23,7 +23,7 @@ class RevenueMain extends Component {
 	render() {
 		return (
 			<div>
-				<h3>{ __( 'Accept donations on your site' ) }</h3>
+				<h2>{ __( 'Accept donations on your site' ) }</h2>
 				<p>{ __( 'Newspack can help you set up a donations page and accept one-time or recurring payments from your readers.' ) }</p>
 			</div>
 		);
