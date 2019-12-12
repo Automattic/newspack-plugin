67bc7bebf63c9baa15386d917a991eeb5b2f8c3a
Thomas Guillot <thomasguillot@users.noreply.github.com>
2019-12-12 14:42:14 +0000

    Performance: Add back button to the Intro Card
    * Remove the `noCard` setting
    * Add a `buttonText` and a `buttonAction` setting
    * Reorganise view with a `noBackground` Card

--- assets/wizards/performance/views/intro/index.js
+++ assets/wizards/performance/views/intro/index.js
@@ -24,58 +24,52 @@ class Intro extends Component {
 	render() {
 		const { updateSiteIcon, settings } = this.props;
 		return (
-			<div className='muriel-wizardScreen muriel-wizardScreen__no-background'>
-				<Grid>
-					<Card>
-						<h2>{ __( 'Increase user engagement' ) }</h2>
-						<p>
-							{ __( 'Engage with your audience more by letting them add the site to their home screen and use it offline.' ) }
-						</p>
-						<hr />
-						<h2>{ __( 'Automatic performance enhancements' ) }</h2>
-						<div className="newspack-performance-wizard__info-block dashicons-before dashicons-info">
-							<p>
-								{ __(
-									'Newspack utilizes Progressive Web App (PWA) to automatically optimize and configure your news site to perform better. It automatically improves:'
-								) }
-							</p>
-							<p>
-								<strong>{ __( 'Speed: ' ) }</strong>
-								{ __( 'Will work reliably, no matter the network conditions.' ) }
-								<br />
-								<strong>{ __( 'Security: ' ) }</strong>
-								{ __( 'Served through HTTPS to protect the integrity of your news site.' ) }
-								<br />
-								<strong>{ __( 'User experience:  ' ) }</strong>
-								{ __( 'Feels like a native app on the device.' ) }
-							</p>
-							<p className="newspack-plugin-description">
-								<strong>
-									{ __( 'PWA options have been automatically set up for you.' ) }
-								</strong>
-							</p>
+			<Card noBackground>
+				<h2>{ __( 'Increase user engagement' ) }</h2>
+				<p>
+					{ __(
+						'Engage with your audience more by letting them add the site to their home screen and use it offline.'
+					) }
+				</p>
+				<hr />
+				<h2>{ __( 'Automatic performance enhancements' ) }</h2>
+				<p>
+					{ __(
+						'Newspack utilizes Progressive Web App (PWA) to automatically optimize and configure your news site to perform better. It automatically improves:'
+					) }
+				</p>
+				<p>
+					<strong>{ __( 'Speed: ' ) }</strong>
+					{ __( 'Will work reliably, no matter the network conditions.' ) }
+					<br />
+					<strong>{ __( 'Security: ' ) }</strong>
+					{ __( 'Served through HTTPS to protect the integrity of your news site.' ) }
+					<br />
+					<strong>{ __( 'User experience:  ' ) }</strong>
+					{ __( 'Feels like a native app on the device.' ) }
+				</p>
+				<p className="newspack-plugin-description">
+					<em>{ __( 'PWA options have been automatically set up for you.' ) }</em>
+				</p>
+				<div className='newspack-performance-wizard__status'>
+					{ settings.configured && (
+						<div className='notice notice-success'>{ __( 'PWA is configured and working.' ) }</div>
+					) }
+					{ ! settings.configured && settings.error && (
+						<div className='notice notice-error'>
+							{ settings.error }
 						</div>
-						<div className='newspack-performance-wizard__status'>
-							{ settings.configured && (
-								<div className='notice notice-success'>{ __( 'PWA is configured and working.' ) }</div>
-							) }
-							{ ! settings.configured && settings.error && (
-								<div className='notice notice-error'>
-									{ settings.error }
-								</div>
-							) }
-						</div>
-						<hr />
-						<h2>{ __( 'Site icon' ) }</h2>
-						<p>{ __( 'Your site icon is the icon your site has when installed as an app.' ) }</p>
-						<p><em>{ __( 'Site icons should be square and at least 144 × 144 pixels.' ) }</em></p>
-						<ImageUpload
-							image={ settings.site_icon }
-							onChange={ image => updateSiteIcon( image ) }
-						/>
-					</Card>
-				</Grid>
-			</div>
+					) }
+				</div>
+				<hr />
+				<h2>{ __( 'Site icon' ) }</h2>
+				<p>{ __( 'Your site icon is the icon your site has when installed as an app.' ) }</p>
+				<ImageUpload
+					image={ settings.site_icon }
+					onChange={ image => updateSiteIcon( image ) }
+				/>
+				<p><em>{ __( 'Site icons should be square and at least 144 × 144 pixels.' ) }</em></p>
+			</Card>
 		);
 	}
 }
