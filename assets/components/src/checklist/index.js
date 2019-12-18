c2e1041b16fe488e60e2a6fa6d5a8f901f74705e
Thomas Guillot <thomasguillot@users.noreply.github.com>
2019-12-18 18:07:32 +0000

    Checklist: Remove Muriel and Dashicon

--- assets/components/src/checklist/index.js
+++ assets/components/src/checklist/index.js
@@ -1,5 +1,5 @@
 /**
- * Checklist for tracking multi-step tasks.
+ * Checklist
  */
 
 /**
@@ -7,14 +7,18 @@
  */
 import { __ } from '@wordpress/i18n';
 import { Component, Children, cloneElement } from '@wordpress/element';
-import { Dashicon } from '@wordpress/components';
+import { SVG, Path } from '@wordpress/components';
 
 /**
  * Internal dependencies.
  */
-import murielClassnames from '../../../shared/js/muriel-classnames';
 import { Button, Card, ProgressBar } from '../';
 import './style.scss';
+
+/**
+ * External dependencies.
+ */
+import classnames from 'classnames';
 
 class Checklist extends Component {
 	constructor() {
@@ -30,10 +34,20 @@ class Checklist extends Component {
 	render() {
 		const { className, children, progressBarText, ...otherProps } = this.props;
 		const { hideCompleted } = this.state;
+		const iconExpandLess = (
+			<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+				<Path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z"/>
+			</SVG>
+		);
+		const iconExpandMore = (
+			<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+				<Path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/>
+			</SVG>
+		);
 		const completedLabel = hideCompleted ? __( 'Show completed' ) : __( 'Hide completed' );
-		const completedIcon = hideCompleted ? 'arrow-down-alt2' : 'arrow-up-alt2';
-		const classes = murielClassnames(
-			'muriel-checklist',
+		const completedIcon = hideCompleted ? iconExpandMore : iconExpandLess;
+		const classes = classnames(
+			'newspack-checklist',
 			className,
 			hideCompleted && 'is-hide-completed'
 		);
@@ -41,10 +55,10 @@ class Checklist extends Component {
 			return completedCount + ( child.props.completed ? 1 : 0 );
 		}, 0 );
 		return (
-			<Card noBackground className={ classes } { ...otherProps }>
-				<div className="muriel-checklist__wrapper">
-					<div className="muriel-checklist__header">
-						<div className="muriel-checklist__header-main">
+			<Card className={ classes } { ...otherProps }>
+				<div className="newspack-checklist__wrapper">
+					<div className="newspack-checklist__header">
+						<div className="newspack-checklist__progress-bar">
 							<ProgressBar
 								completed={ completedCount }
 								total={ Children.count( children ) }
@@ -52,17 +66,17 @@ class Checklist extends Component {
 								label={ progressBarText }
 							/>
 						</div>
-						<div className="muriel-checklist__header-secondary">
-							<label htmlFor="muriel-checklist__header-action">{ completedLabel }</label>
+						<div className="newspack-checklist__header-action">
+							<label htmlFor="newspack-checklist__header-action">{ completedLabel }</label>
 							<Button
-								id="muriel-checklist__header-action"
+								id="newspack-checklist__header-action"
 								onClick={ () => this.setState( { hideCompleted: ! hideCompleted } ) }
 							>
-								<Dashicon icon={ completedIcon } />
+								{ completedIcon }
 							</Button>
 						</div>
 					</div>
-					<div className="muriel-checklist__tasks">{ children }</div>
+					<div className="newspack-checklist__tasks">{ children }</div>
 				</div>
 			</Card>
 		);
