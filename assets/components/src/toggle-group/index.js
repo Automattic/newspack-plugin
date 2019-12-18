fefa5a920be497c8c2bded40c9b81357b1cff1d3
Thomas Guillot <thomasguillot@users.noreply.github.com>
2019-12-18 08:57:43 +0000

    Toggle Group: Use Newspack's `ToggleControl`
    * Update the container class and use paragraphs instead of headings

--- assets/components/src/toggle-group/index.js
+++ assets/components/src/toggle-group/index.js
@@ -1,40 +1,38 @@
 /**
- * Toggle group
+ * Toggle Group
  */
 
 /**
  * WordPress dependencies.
  */
 import { Component } from '@wordpress/element';
-import { ToggleControl } from '@wordpress/components';
+
+/**
+ * Internal dependencies.
+ */
+import { ToggleControl } from '../';
+import './style.scss';
 
 /**
  * External dependencies.
  */
 import classnames from 'classnames';
 
-/**
- * Internal dependencies.
- */
-import murielClassnames from '../../../shared/js/muriel-classnames';
-import './style.scss';
-
-/**
- * Progress bar.
- */
 class ToggleGroup extends Component {
 	/**
 	 * Render.
 	 */
 	render() {
 		const { checked, children, description, onChange, title, className } = this.props;
-		const classes = murielClassnames( 'muriel-toggle-group', className );
+		const classes = classnames( 'newspack-toggle-group', className );
 		return (
 			<div className={ classes }>
 				<ToggleControl checked={ checked || false } onChange={ onChange } />
-				<div className="container">
-					<h1>{ title }</h1>
-					<h2>{ description }</h2>
+				<div className="newspack-toggle-group__description">
+					<p className="is-dark">
+						<strong>{ title }</strong>
+					</p>
+					<p>{ description }</p>
 					{ checked && children }
 				</div>
 			</div>
