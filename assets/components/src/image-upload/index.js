656b8e1ed7e4ab18141a415c599e2db30de580b9
Thomas Guillot <thomasguillot@users.noreply.github.com>
2019-12-24 20:01:41 +0000

    Image Upload: Rework Add image upload button

--- assets/components/src/image-upload/index.js
+++ assets/components/src/image-upload/index.js
@@ -78,39 +78,34 @@ class ImageUpload extends Component {
 	 * Render.
 	 */
 	render = () => {
-		const { className, image } = this.props;
+		const { className, image, removeText, addText } = this.props;
+		const classes = classnames( 'newspack-image-upload', className );
 		const iconAddImage = (
 			<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 				<Path d="M19 7v2.99s-1.99.01-2 0V7h-3s.01-1.99 0-2h3V2h2v3h3v2h-3zm-3 4V8h-3V5H5c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-8h-3zM5 19l3-4 2 3 3-4 4 5H5z"/>
 			</SVG>
 		);
-		const iconRemoveImage = (
-			<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-				<Path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
-			</SVG>
-		);
 		return (
-			<Fragment>
+			<div className={ classes }>
 				{ !! image && (
-				<div className={ classnames( 'newspack-image-upload', 'newspack-image-upload__has-image', className ) }>
+				<Fragment>
 					<div className="newspack-image-upload__image-preview">
 						<img src={ image.url } />
 					</div>
-					<Button onClick={ this.removeImage } isTertiary>
-						{ iconRemoveImage }
-						{ __( 'Remove image' ) }
+					<Button onClick={ this.removeImage } isTertiary isSmall>
+						{ ! removeText && ( __( 'Remove image' ) ) }
+						{ removeText && removeText }
 					</Button>
-				</div>
+				</Fragment>
 				) }
 				{ ! image && (
-					<div className={ classnames( 'newspack-image-upload', className ) }>
-						<Button onClick={ this.openModal } isTertiary>
-							{ iconAddImage }
-							{ __( 'Add an image' ) }
-						</Button>
-					</div>
+				<Button onClick={ this.openModal } className="newspack-image-upload__add-image" isTertiary isSmall>
+					{ iconAddImage }
+					{ ! addText && ( __( 'Add image' ) ) }
+					{ addText && addText }
+				</Button>
 				) }
-			</Fragment>
+			</div>
 		);
 	}
 }
