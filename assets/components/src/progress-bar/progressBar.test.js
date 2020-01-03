252cd7a0d9eee14d285a59ac44adc2cf663d69b4
Jefferson Rabb <jeff@atavist.net>
2020-01-03 11:09:17 -0500

    Fix ImageUploader, ProgressBar and FormattedHeader tests to reflect changed classes and markup.

--- assets/components/src/progress-bar/progressBar.test.js
+++ assets/components/src/progress-bar/progressBar.test.js
@@ -6,30 +6,30 @@ describe( 'ProgressBar', () => {
 	describe( 'basic rendering', () => {
 		it( 'should render a ProgressBar element with no labels', () => {
 			const bar = shallow( <ProgressBar completed="1" total="2" /> );
-			expect( bar.hasClass( 'muriel-progress-bar' ) ).toBe( true );
-			expect( bar.find( '.muriel-progress-bar__label' ) ).toHaveLength( 0 );
-			expect( bar.find( '.muriel-progress-bar__fraction' ) ).toHaveLength( 0 );
+			expect( bar.hasClass( 'newspack-progress-bar' ) ).toBe( true );
+			expect( bar.find( 'h2' ) ).toHaveLength( 0 );
+			expect( bar.find( 'p' ) ).toHaveLength( 0 );
 		} );
 
 		it( 'should render a ProgressBar element with a label', () => {
 			const bar = shallow( <ProgressBar completed="1" total="2" label="test label" /> );
-			expect( bar.hasClass( 'muriel-progress-bar' ) ).toBe( true );
-			expect( bar.find( '.muriel-progress-bar__label' ) ).toHaveLength( 1 );
-			expect( bar.find( '.muriel-progress-bar__fraction' ) ).toHaveLength( 0 );
+			expect( bar.hasClass( 'newspack-progress-bar' ) ).toBe( true );
+			expect( bar.find( 'h2' ) ).toHaveLength( 1 );
+			expect( bar.find( 'p' ) ).toHaveLength( 0 );
 		} );
 
 		it( 'should render a ProgressBar element with a fraction', () => {
 			const bar = shallow( <ProgressBar completed="1" total="2" displayFraction /> );
-			expect( bar.hasClass( 'muriel-progress-bar' ) ).toBe( true );
-			expect( bar.find( '.muriel-progress-bar__label' ) ).toHaveLength( 0 );
-			expect( bar.find( '.muriel-progress-bar__fraction' ) ).toHaveLength( 1 );
+			expect( bar.hasClass( 'newspack-progress-bar' ) ).toBe( true );
+			expect( bar.find( 'h2' ) ).toHaveLength( 0 );
+			expect( bar.find( 'p' ) ).toHaveLength( 1 );
 		} );
 
 		it( 'should render a ProgressBar element with both label and fraction', () => {
 			const bar = shallow( <ProgressBar completed="1" total="2" label="test label" displayFraction /> );
-			expect( bar.hasClass( 'muriel-progress-bar' ) ).toBe( true );
-			expect( bar.find( '.muriel-progress-bar__label' ) ).toHaveLength( 1 );
-			expect( bar.find( '.muriel-progress-bar__fraction' ) ).toHaveLength( 1 );
+			expect( bar.hasClass( 'newspack-progress-bar' ) ).toBe( true );
+			expect( bar.find( 'h2' ) ).toHaveLength( 1 );
+			expect( bar.find( 'p' ) ).toHaveLength( 1 );
 		} );
 
 		it( 'should calculate progress correctly', () => {
@@ -41,14 +41,14 @@ describe( 'ProgressBar', () => {
 
 		it( 'should handle non-numeric values in ProgressBar element', () => {
 			const bar = shallow( <ProgressBar completed="cats" total="dogs" displayFraction /> );
-			expect( bar.find( '.muriel-progress-bar__fraction' ).text() ).toBe( '0/0' );
-			expect( bar.find( '.muriel-progress-bar__bar' ).render().css( 'width' ) ).toBe( '100%' );
+			expect( bar.find( 'p' ).text() ).toBe( '0/0' );
+			expect( bar.find( '.newspack-progress-bar__bar' ).render().css( 'width' ) ).toBe( '100%' );
 		} );
 
 		it( 'should handle non-logical values in ProgressBar element', () => {
 			const bar = shallow( <ProgressBar completed="3" total="-1" displayFraction /> );
-			expect( bar.find( '.muriel-progress-bar__fraction' ).text() ).toBe( '0/0' );
-			expect( bar.find( '.muriel-progress-bar__bar' ).render().css( 'width' ) ).toBe( '100%' );
+			expect( bar.find( 'p' ).text() ).toBe( '0/0' );
+			expect( bar.find( '.newspack-progress-bar__bar' ).render().css( 'width' ) ).toBe( '100%' );
 		} );
 	} );
 } );
