3be35c94e12b46790e027577783f463e2bc6708f
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-01-03 09:10:27 +0000

    Image Upload: Use `@material-ui/icons` instead of SVG

--- assets/components/src/image-upload/index.js
+++ assets/components/src/image-upload/index.js
@@ -7,8 +7,13 @@
  */
 import { Component, Fragment } from '@wordpress/element';
 import { data } from '@wordpress/data';
-import { SVG, Path } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
+
+/**
+ * Material UI dependencies.
+ */
+import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 /**
  * Internal dependencies.
@@ -80,16 +85,6 @@ class ImageUpload extends Component {
 	render = () => {
 		const { className, image, removeText, addText } = this.props;
 		const classes = classnames( 'newspack-image-upload', className );
-		const iconAddImage = (
-			<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-				<Path d="M19 7v2.99s-1.99.01-2 0V7h-3s.01-1.99 0-2h3V2h2v3h3v2h-3zm-3 4V8h-3V5H5c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-8h-3zM5 19l3-4 2 3 3-4 4 5H5z"/>
-			</SVG>
-		);
-		const iconRemoveImage = (
-			<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-				<Path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
-			</SVG>
-		);
 		return (
 			<div className={ classes }>
 				{ !! image && (
@@ -97,8 +92,8 @@ class ImageUpload extends Component {
 					<div className="newspack-image-upload__image-preview">
 						<img src={ image.url } />
 					</div>
-					<Button onClick={ this.removeImage } isTertiary isSmall>
-						{ iconRemoveImage }
+					<Button onClick={ this.removeImage } className="newspack-image-upload__remove-image" isTertiary isSmall>
+						<DeleteIcon />
 						{ ! removeText && ( __( 'Remove image' ) ) }
 						{ removeText && removeText }
 					</Button>
@@ -106,7 +101,7 @@ class ImageUpload extends Component {
 				) }
 				{ ! image && (
 				<Button onClick={ this.openModal } className="newspack-image-upload__add-image" isTertiary isSmall>
-					{ iconAddImage }
+					<AddPhotoAlternateIcon />
 					{ ! addText && ( __( 'Add image' ) ) }
 					{ addText && addText }
 				</Button>
