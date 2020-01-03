414c72a87d6d0075bd06186242dcecb5a5916c74
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-01-03 11:45:27 +0000

    With Wizards: Remove unused WordPress component

--- assets/components/src/with-wizard/index.js
+++ assets/components/src/with-wizard/index.js
@@ -6,7 +6,6 @@
  * WordPress dependencies.
  */
 import { Component, createRef, Fragment } from '@wordpress/element';
-import { Spinner } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import apiFetch from '@wordpress/api-fetch';
 
