7a97a7b2caca9ff5e2189d7bb91f36d0695be333
Jefferson Rabb <jeff@atavist.net>
2020-01-03 13:07:47 -0500

    Setup Wizard: Theme Style Setup Screen, initial commit.

--- assets/wizards/setup/views/theme-style-selection/index.js
+++ assets/wizards/setup/views/theme-style-selection/index.js
@@ -11,14 +11,13 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import { Button, ButtonGroup, withWizardScreen } from '../../../../components/src';
-import './style.scss';
-import ScreenshotStyle0 from './images/screenshot-style-0.png';
-import ScreenshotStyle1 from './images/screenshot-style-1.png';
-import ScreenshotStyle2 from './images/screenshot-style-2.png';
-import ScreenshotStyle3 from './images/screenshot-style-3.png';
-import ScreenshotStyle4 from './images/screenshot-style-4.png';
-import ScreenshotStyle5 from './images/screenshot-style-5.png';
+import { StyleCard, StyleCardGroup, withWizardScreen } from '../../../../components/src';
+import DefaultImg from './images/default.png';
+import ScottImg from './images/scott.png';
+import NelsonImg from './images/nelson.png';
+import KatharineImg from './images/katharine.png';
+import SachaImg from './images/sacha.png';
+import JosephImg from './images/joseph.png';
 
 /**
  * Theme Style Selection Screen.
@@ -30,62 +29,48 @@ class ThemeStyleSelection extends Component {
 	render() {
 		const { updateThemeStyle, themeStyle } = this.props;
 		return (
-			<ButtonGroup>
-				<Button
+			<StyleCardGroup>
+				<StyleCard
+					cardTitle={ __( 'Default', 'newspack-plugin' ) }
+					image={ DefaultImg }
+					url="//newspack.blog"
+					isActive={ themeStyle === 'default' }
 					onClick={ () => updateThemeStyle( 'default' ) }
-					isPrimary={ themeStyle === 'default' }
-				>
-					<img
-						alt={ __( 'Default', 'newspack-plugin' ) }
-						src={ ScreenshotStyle0 }
-					/>
-				</Button>
-				<Button
+				/>
+				<StyleCard
+					cardTitle="Scott"
+					image={ ScottImg }
+					isActive={ themeStyle === 'style-1' }
 					onClick={ () => updateThemeStyle( 'style-1' ) }
-					isPrimary={ themeStyle === 'style-1' }
-				>
-					<img
-						alt={ __( 'Style 1', 'newspack-plugin' ) }
-						src={ ScreenshotStyle1 }
-					/>
-				</Button>
-				<Button
+				/>
+				<StyleCard
+					cardTitle="Nelson"
+					image={ NelsonImg }
+					url="//elsoberano.org"
+					isActive={ themeStyle === 'style-2' }
 					onClick={ () => updateThemeStyle( 'style-2' ) }
-					isPrimary={ themeStyle === 'style-2' }
-				>
-					<img
-						alt={ __( 'Style 2', 'newspack-plugin' ) }
-						src={ ScreenshotStyle2 }
-					/>
-				</Button>
-				<Button
+				/>
+				<StyleCard
+					cardTitle="Katharine"
+					image={ KatharineImg }
+					url="//thelensnola.org"
+					isActive={ themeStyle === 'style-3' }
 					onClick={ () => updateThemeStyle( 'style-3' ) }
-					isPrimary={ themeStyle === 'style-3' }
-				>
-					<img
-						alt={ __( 'Style 3', 'newspack-plugin' ) }
-						src={ ScreenshotStyle3 }
-					/>
-				</Button>
-				<Button
+				/>
+				<StyleCard
+					cardTitle="Sacha"
+					image={ SachaImg }
+					isActive={ themeStyle === 'style-4' }
 					onClick={ () => updateThemeStyle( 'style-4' ) }
-					isPrimary={ themeStyle === 'style-4' }
-				>
-					<img
-						alt={ __( 'Style 4', 'newspack-plugin' ) }
-						src={ ScreenshotStyle4 }
-					/>
-				</Button>
-				<Button
+				/>
+				<StyleCard
+					cardTitle="Joseph"
+					image={ JosephImg }
+					url="//oklahomawatch.org"
+					isActive={ themeStyle === 'style-5' }
 					onClick={ () => updateThemeStyle( 'style-5' ) }
-					isPrimary={ themeStyle === 'style-5' }
-				>
-					<img
-						alt={ __( 'Style 5', 'newspack-plugin' ) }
-						src={ ScreenshotStyle5 }
-					/>
-				</Button>
-			</ButtonGroup>
+				/>
+			</StyleCardGroup>
 		);
 	}
 }
