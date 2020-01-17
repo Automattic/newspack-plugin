e929d1f26e455cec1691897207f78934ae88b448
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-01-17 11:02:00 +0000

    create new `FormTokenField` component using `BaseComponent`

--- assets/components/src/index.js
+++ assets/components/src/index.js
@@ -4,6 +4,7 @@ export { default as Card } from './card';
 export { default as CategoryAutocomplete } from './category-autocomplete';
 export { default as CheckboxControl } from './checkbox-control';
 export { default as Checklist } from './checklist';
+export { default as FormTokenField } from './form-token-field';
 export { default as FormattedHeader } from './formatted-header';
 export { default as Handoff } from './handoff';
 export { default as InfoButton } from './info-button';
