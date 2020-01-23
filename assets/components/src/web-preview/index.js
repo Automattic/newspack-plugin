d6ef2facefff912f29ae3570ab45e35dfaddd48a
Adam Borowski <adam@adamboro.com>
2020-01-23 11:57:09 +0100

    feat(WebPreview): render in portal
    this way there is more control over layering

--- assets/components/src/web-preview/index.js
+++ assets/components/src/web-preview/index.js
@@ -1,7 +1,7 @@
 /**
  * WordPress dependencies.
  */
-import { Component, Fragment } from '@wordpress/element';
+import { Component, Fragment, createPortal } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -30,11 +30,33 @@ class WebPreview extends Component {
 	state = {
 		device: 'desktop',
 		loaded: false,
-		previewVisibility: false,
+		isPreviewVisible: false,
+		modalDOMElement: null,
 	};
 
+	/**
+	 * Create a div and append it to the body, so that the modal is on the top layer
+	 */
+	componentDidMount() {
+		const modalDOMElement = document.createElement( 'div' );
+		document.body.appendChild( modalDOMElement );
+		this.setState( { modalDOMElement } );
+	}
+
+	/**
+	 * Cleanup
+	 */
+	componentWillUnmount() {
+		if ( this.state.modalDOMElement ) {
+			document.body.removeChild( this.state.modalDOMElement );
+		}
+	}
+
+	/**
+	 * Add or remove applicable body classes
+	 */
 	componentDidUpdate() {
-		if ( this.state.previewVisibility === true ) {
+		if ( this.state.isPreviewVisible === true ) {
 			document.body.classList.add( 'newspack-web-preview__open' );
 		} else {
 			document.body.classList.remove( 'newspack-web-preview__open' );
@@ -42,83 +64,118 @@ class WebPreview extends Component {
 	}
 
 	/**
-	 * Render.
+	 * Create JSX for the modal
 	 */
-	render() {
-		const { label, url, isPrimary, isSecondary, isTertiary, isLink, isLarge, isSmall } = this.props;
-		const { device, loaded, previewVisibility } = this.state;
+	getWebPreviewModal = () => {
+		const { url } = this.props;
+		const { device, loaded, modalDOMElement, isPreviewVisible } = this.state;
+
+		if ( ! modalDOMElement || ! isPreviewVisible ) {
+			return null;
+		}
+
 		const classes = classnames(
 			'newspack-web-preview__container',
 			device,
 			loaded && 'newspack-web-preview__is-loaded'
 		);
-		return (
-			<Fragment>
-				<Button
-					isPrimary={ isPrimary }
-					isSecondary={ isSecondary }
-					isTertiary={ isTertiary }
-					isLink={ isLink }
-					isLarge={ isLarge }
-					isSmall={ isSmall }
-					onClick={ () => this.setState( { previewVisibility: true } ) }
-				>
-					{ label }
-				</Button>
-				{ !! previewVisibility && (
-					<div className={ classes }>
-						<div className="newspack-web-preview__interior">
-							<div className="newspack-web-preview__toolbar">
-								<div className="newspack-web-preview__toolbar-left">
-									<Button
-										onClick={ () => this.setState( { device: 'desktop' } ) }
-										isPrimary={ 'desktop' === device }
-										isSecondary={ 'desktop' !== device }
-										className="is-desktop"
-									>
-										<DesktopWindowsIcon />
-										<span className="screen-reader-text">{ __( 'Preview desktop size' ) }</span>
-									</Button>
-									<Button
-										onClick={ () => this.setState( { device: 'tablet' } ) }
-										isPrimary={ 'tablet' === device }
-										isSecondary={ 'tablet' !== device }
-										className="is-tablet"
-									>
-										<TabletAndroidIcon />
-										<span className="screen-reader-text">{ __( 'Preview tablet size' ) }</span>
-									</Button>
-									<Button
-										onClick={ () => this.setState( { device: 'phone' } ) }
-										isPrimary={ 'phone' === device }
-										isSecondary={ 'phone' !== device }
-										className="is-phone"
-									>
-										<PhoneAndroidIcon />
-										<span className="screen-reader-text">{ __( 'Preview phone size' ) }</span>
-									</Button>
-								</div>
-								<div className="newspack-web-preview__toolbar-right">
-									<Button
-										onClick={ () => this.setState( { previewVisibility: false, loaded: false } ) }
-									>
-										<CloseIcon />
-										<span className="screen-reader-text">{ __( 'Close preview' ) }</span>
-									</Button>
-								</div>
-							</div>
-							<div className="newspack-web-preview__content">
-								{ ! loaded &&
-									<div className="newspack-web-preview__is-waiting">
-										<Waiting isLeft />
-										{ __( 'Loading...' ) }
-									</div>
-								}
-								<iframe src={ url } onLoad={ () => this.setState( { loaded: true } ) } />
-							</div>
+
+		return createPortal(
+			<div className={ classes }>
+				<div className="newspack-web-preview__interior">
+					<div className="newspack-web-preview__toolbar">
+						<div className="newspack-web-preview__toolbar-left">
+							<Button
+								onClick={ () => this.setState( { device: 'desktop' } ) }
+								isPrimary={ 'desktop' === device }
+								isSecondary={ 'desktop' !== device }
+								className="is-desktop"
+							>
+								<DesktopWindowsIcon />
+								<span className="screen-reader-text">{ __( 'Preview desktop size' ) }</span>
+							</Button>
+							<Button
+								onClick={ () => this.setState( { device: 'tablet' } ) }
+								isPrimary={ 'tablet' === device }
+								isSecondary={ 'tablet' !== device }
+								className="is-tablet"
+							>
+								<TabletAndroidIcon />
+								<span className="screen-reader-text">{ __( 'Preview tablet size' ) }</span>
+							</Button>
+							<Button
+								onClick={ () => this.setState( { device: 'phone' } ) }
+								isPrimary={ 'phone' === device }
+								isSecondary={ 'phone' !== device }
+								className="is-phone"
+							>
+								<PhoneAndroidIcon />
+								<span className="screen-reader-text">{ __( 'Preview phone size' ) }</span>
+							</Button>
+						</div>
+						<div className="newspack-web-preview__toolbar-right">
+							<Button onClick={ () => this.setState( { isPreviewVisible: false, loaded: false } ) }>
+								<CloseIcon />
+								<span className="screen-reader-text">{ __( 'Close preview' ) }</span>
+							</Button>
 						</div>
 					</div>
+					<div className="newspack-web-preview__content">
+						{ ! loaded && (
+							<div className="newspack-web-preview__is-waiting">
+								<Waiting isLeft />
+								{ __( 'Loading...' ) }
+							</div>
+						) }
+						<iframe src={ url } onLoad={ () => this.setState( { loaded: true } ) } />
+					</div>
+				</div>
+			</div>,
+			modalDOMElement
+		);
+	};
+
+	showPreview = () => {
+		this.setState( { isPreviewVisible: true } );
+	};
+
+	/**
+	 * Render.
+	 */
+	render() {
+		const {
+			label,
+			isPrimary,
+			isSecondary,
+			isTertiary,
+			isLink,
+			isLarge,
+			isSmall,
+			/**
+			 * Inversion of control - let the caller render
+			 * the button that will trigger the modal
+			 */
+			renderButton,
+		} = this.props;
+
+		return (
+			<Fragment>
+				{ renderButton ? (
+					renderButton( { showPreview: this.showPreview } )
+				) : (
+					<Button
+						isPrimary={ isPrimary }
+						isSecondary={ isSecondary }
+						isTertiary={ isTertiary }
+						isLink={ isLink }
+						isLarge={ isLarge }
+						isSmall={ isSmall }
+						onClick={ this.showPreview }
+					>
+						{ label }
+					</Button>
 				) }
+				{ this.getWebPreviewModal() }
 			</Fragment>
 		);
 	}
