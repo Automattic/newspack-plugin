45dc944b8e5f1fd488174552d463fb31edf1a6cd
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-02-06 15:38:05 +0000

    revenue main: remove memberships actioncard and add new description to donations actioncard

--- assets/wizards/readerRevenue/views/revenue-main/index.js
+++ assets/wizards/readerRevenue/views/revenue-main/index.js
@@ -23,18 +23,11 @@ class RevenueMain extends Component {
 	render() {
 		return (
 			<Fragment>
-				<p>{ __( 'Newspack can help you set up a donations page and accept one-time or recurring payments from your readers.' ) }</p>
 				<ActionCard
 					title={ __( 'Donations' ) }
-					description={ __( 'Suggest donations on your website.' ) }
+					description={ __( 'Set up a donations page and accept one-time or recurring payments from your readers.' ) }
 					actionText={ __( 'Configure' ) }
 					href="/wp-admin/admin.php?page=newspack-reader-revenue-wizard#/donations"
-				/>
-				<ActionCard
-					title={ __( 'Memberships' ) }
-					description={ __( 'Add a memberships page to your website.' ) }
-					actionText={ __( 'Configure' ) }
-					href="/wp-admin/admin.php?page=newspack-reader-revenue-wizard#/configure-landing-page"
 				/>
 				<PluginToggle
 					plugins={ {
