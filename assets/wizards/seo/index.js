8491cde8343ec94975b50f109a0cdfb2197f092f
Jefferson Rabb <jeff@atavist.net>
2020-02-09 11:36:14 -0500

    feat(seo): stubs of new screens.

--- assets/wizards/seo/index.js
+++ assets/wizards/seo/index.js
@@ -18,7 +18,7 @@ import HeaderIcon from '@material-ui/icons/Search';
  */
 import { withWizard } from '../../components/src';
 import Router from '../../components/src/proxied-imports/router';
-import { Intro } from './views';
+import { Environment, Separator, Social, Tools } from './views';
 
 const { HashRouter, Redirect, Route, Switch } = Router;
 
@@ -28,6 +28,27 @@ class SEOWizard extends Component {
 	 */
 	render() {
 		const { pluginRequirements } = this.props;
+		const tabbedNavigation = [
+			{
+				label: __( 'Environment', 'newspack' ),
+				path: '/',
+				exact: true,
+			},
+			{
+				label: __( 'separator', 'newspack' ),
+				path: '/separator',
+				exact: true,
+			},
+			{
+				label: __( 'Tools', 'newspack' ),
+				path: '/tools',
+				exact: true,
+			},
+			{
+				label: __( 'Social', 'newspack' ),
+				path: '/social',
+			},
+		];
 		return (
 			<Fragment>
 				<HashRouter hashType="slash">
@@ -37,10 +58,47 @@ class SEOWizard extends Component {
 							path="/"
 							exact
 							render={ () => (
-								<Intro
+								<Environment
 									headerIcon={ <HeaderIcon /> }
 									headerText={ __( 'SEO' ) }
 									subHeaderText={ __( 'Search engine and social optimization' ) }
+									tabbedNavigation={ tabbedNavigation }
+								/>
+							) }
+						/>
+						<Route
+							path="/separator"
+							exact
+							render={ () => (
+								<Separator
+									headerIcon={ <HeaderIcon /> }
+									headerText={ __( 'SEO' ) }
+									subHeaderText={ __( 'Search engine and social optimization' ) }
+									tabbedNavigation={ tabbedNavigation }
+								/>
+							) }
+						/>
+						<Route
+							path="/social"
+							exact
+							render={ () => (
+								<Social
+									headerIcon={ <HeaderIcon /> }
+									headerText={ __( 'SEO' ) }
+									subHeaderText={ __( 'Search engine and social optimization' ) }
+									tabbedNavigation={ tabbedNavigation }
+								/>
+							) }
+						/>
+						<Route
+							path="/tools"
+							exact
+							render={ () => (
+								<Tools
+									headerIcon={ <HeaderIcon /> }
+									headerText={ __( 'SEO' ) }
+									subHeaderText={ __( 'Search engine and social optimization' ) }
+									tabbedNavigation={ tabbedNavigation }
 								/>
 							) }
 						/>
