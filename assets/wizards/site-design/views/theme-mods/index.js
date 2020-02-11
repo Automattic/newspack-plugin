6aa50000061489f4bafaf8b15e401fd06cf3bb4f
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-02-11 18:05:07 +0000

    Site Design Theme Mods: Use Newspack's RadioControl component

--- assets/wizards/site-design/views/theme-mods/index.js
+++ assets/wizards/site-design/views/theme-mods/index.js
@@ -3,13 +3,13 @@
  */
 import { Component, Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
-import { RadioControl } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import {
 	ImageUpload,
+	RadioControl,
 	TextControl,
 	ToggleControl,
 	ToggleGroup,
