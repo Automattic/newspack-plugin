208cd6b8e9475ee8c2ee617a61fbf017029f1661
Jefferson Rabb <jefferson.rabb@automattic.com>
2020-02-17 15:01:01 -0500

    SEO Wizard/Yoast Configuration Manager (#435)
    * feat(seo): stubs of new screens.
    
    * chore: missing files for seo screen stubs.
    
    * feat: basics of SEO wizard data model.
    
    * feat: Yoast configuration manager. wire up all SEO wizard fields.
    
    * SEO Social: Add support for Pinterest
    
    * Add new ButtonGroup component
    
    * SEO Separator: Use Newspack's ButtonGroup
    
    * SEO Separator: Add &bull option
    
    * SEO: Add custom style to environment and add translatable strings
    
    * fix: SEO wizard, identical update/save paradigm throughout.
    
    * With Wizard Screen: Allow secondary button to support `Handoff`
    
    * SEO: Add secondary button handoff to Yoast
    
    * fix: unclosed quote.
    
    * feat: add support for passing editLink through to a Handoff button rendered by withWizardScreen.
    
    * feat: add editLink to the secondary button on SEO wizard Social screen.
    
    * SEO: Add specific handoff page to Separator and Tools
    
    * SEO Separator: Add full stop to sentence
    
    * Text Control: Reset help text
    
    * SEO Tools: Update help text with link using RawHTML
    
    * Toggle Control: Reset margin on the help text
    
    * SEO Environnment: Add help text
    
    * Toggle Control: Create alternative style via isDark prop
    
    * SEO Environment: Use isDark on the ToggleControl
    
    * fix: wp-html-entities as a dependency for SEO wizard.
    
    * feat: preview site title with selected separator.
    
    * fix: camelCase for initial state.
    
    * fix: switch to isPressed and isSecondary for separator Button states.
    
    * fix: missing import.
    
    * fix: typo.
    
    * fix: add installed/activated checks in Yoast configuration manager get/set methods.
    
    * Button: Add style to `isPressed`, same as `isPrimary`
    
    * Button Group: Fix display style
    
    * Card: Add style for Preview
    
    * SEO Separator: Use Preview Card instead of a disabled Text Control
    
    Co-authored-by: Thomas Guillot <thomasguillot@users.noreply.github.com>

---
+++ assets/wizards/seo/views/separator/index.js
@@ -0,0 +1,85 @@
+/**
+ * WordPress dependencies
+ */
+import { Component, Fragment } from '@wordpress/element';
+import { decodeEntities } from '@wordpress/html-entities';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import { Button, ButtonGroup, Card, withWizardScreen } from '../../../../components/src';
+
+const SEPARATORS = {
+	'sc-dash': '-',
+	'sc-ndash': '&ndash;',
+	'sc-mdash': '&mdash;',
+	'sc-bull': '&bull;',
+	'sc-star': '*',
+	'sc-pipe': '|',
+};
+
+/**
+ * SEO Separator screen.
+ */
+class Separator extends Component {
+	/**
+	 * Generate sample page title using selected separator.
+	 */
+	exampleTitle = titleSeparator => {
+		const separator = SEPARATORS[ titleSeparator ];
+		const blogName =
+			window && window.newspack_urls
+				? window.newspack_urls.bloginfo.name
+				: __( 'Site Name', 'newspack' );
+		return `${ __( 'Homepage', 'newspack' ) } ${ separator } ${ blogName }`;
+	};
+
+	/**
+	 * Render.
+	 */
+	render() {
+		const { data, onChange } = this.props;
+		const { titleSeparator } = data;
+		return (
+			<Fragment>
+				<h2>{ __( 'Title separator', 'newspack' ) }</h2>
+				<p>
+					{ __(
+						"Choose the symbol to use as your title separator. This will display, for instance, between your post title and site name. Symbols are shown in the size they'll appear in the search results.",
+						'newspack'
+					) }
+				</p>
+				<ButtonGroup>
+					{ Object.keys( SEPARATORS ).map( key => {
+						const value = decodeEntities( SEPARATORS[ key ] );
+						return (
+							<Button
+								key={ key }
+								onClick={ () => onChange( { titleSeparator: key } ) }
+								isPressed={ key === titleSeparator }
+								isSecondary={ key !== titleSeparator }
+							>
+								{ value }
+							</Button>
+						);
+					} ) }
+				</ButtonGroup>
+				<Card noBackground className="newspack-card-preview">
+					<span className="newspack-card-preview__label">
+						{ __( 'Preview', 'newspack' ) }
+					</span>
+					<span className="newspack-card-preview__content">
+						{ decodeEntities( this.exampleTitle( titleSeparator ) ) }
+					</span>
+				</Card>
+			</Fragment>
+		);
+	}
+}
+Separator.defaultProps = {
+	data: {},
+	onChange: () => null,
+};
+
+export default withWizardScreen( Separator );
