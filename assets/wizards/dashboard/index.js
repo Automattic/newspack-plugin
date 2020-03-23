c898f6146e5b98d55768c9b4b1d344f26bf7340a
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-03-23 16:51:46 +0000

    feat: add grid view option to the dashboard

--- assets/wizards/dashboard/index.js
+++ assets/wizards/dashboard/index.js
@@ -1,35 +1,63 @@
 import '../../shared/js/public-path';
 
-/* global newspack_dashboard */
-
 /**
- * WordPress dependencies
+ * WordPress dependencies.
  */
 import { Component, Fragment, render } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 
 /**
- * Internal dependencies
+ * Material UI dependencies.
  */
-import { Grid, NewspackLogo } from '../../components/src';
+import ViewStreamIcon from '@material-ui/icons/ViewStream';
+import ViewModuleIcon from '@material-ui/icons/ViewModule';
+
+/**
+ * Internal dependencies.
+ */
+import { Button, Card, Grid, NewspackLogo } from '../../components/src';
 import DashboardCard from './views/dashboardCard';
 import './style.scss';
 
 /**
- * The Newspack dashboard.
+ * Newspack Dashboard.
  */
 class Dashboard extends Component {
+	state = {
+		view: 'list',
+	};
+
 	/**
 	 * Render.
 	 */
 	render() {
 		const { items } = this.props;
+		const { view } = this.state;
 
 		return (
 			<Fragment>
 				<div className="newspack-logo-wrapper">
 					<NewspackLogo />
 				</div>
-				<Grid>
+				<Grid className={ "view-" + view } isWide={ view === 'grid' && true }>
+					<Card noBackground className="newspack-dashboard-card__views">
+						<Button
+							onClick={ () => this.setState( { view: 'list' } ) }
+							isPrimary={ 'list' === view }
+							isSecondary={ 'list' !== view }
+						>
+							<ViewStreamIcon />
+							<span className="screen-reader-text">{ __( 'List view' ) }</span>
+						</Button>
+						<Button
+							onClick={ () => this.setState( { view: 'grid' } ) }
+							isPrimary={ 'grid' === view }
+							isSecondary={ 'grid' !== view }
+						>
+							<ViewModuleIcon />
+							<span className="screen-reader-text">{ __( 'Grid view' ) }</span>
+						</Button>
+					</Card>
 					{ items.map( card => (
 						<DashboardCard { ...card } key={ card.slug } />
 					) ) }
