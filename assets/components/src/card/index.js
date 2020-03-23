718a5af5113d8abe193511d33c6965f51f6540fc
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-03-23 16:53:10 +0000

    feat: card component add specific class when only using buttons

--- assets/components/src/card/index.js
+++ assets/components/src/card/index.js
@@ -22,11 +22,12 @@ class Card extends Component {
 	 * Render
 	 */
 	render() {
-		const { className, noBackground, ...otherProps } = this.props;
+		const { className, noBackground, isButtons, ...otherProps } = this.props;
 		const classes = classNames(
 			'newspack-card',
 			className,
-			noBackground && 'newspack-card__no-background'
+			noBackground && 'newspack-card__no-background',
+			isButtons && 'newspack-card__buttons-card'
 		);
 		return <div className={ classes } { ...otherProps } />;
 	}
