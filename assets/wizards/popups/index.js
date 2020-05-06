f53a52a8344aaa5368ed89a946186bceeba345ff
Adam Borowski <adam@adamboro.com>
2020-05-06 11:49:38 +0200

    fix: add query string fo previews

--- assets/wizards/popups/index.js
+++ assets/wizards/popups/index.js
@@ -11,9 +11,10 @@ import { Component, render, createElement } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
- * Material UI dependencies.
+ * External dependencies.
  */
 import HeaderIcon from '@material-ui/icons/NewReleases';
+import { stringify } from 'qs';
 
 /**
  * Internal dependencies.
@@ -171,7 +172,7 @@ class PopupsWizard extends Component {
 				  window.newspack_popups_wizard_data &&
 				  window.newspack_popups_wizard_data.preview_post
 				: '/';
-		return `${ previewURL }?newspack_popups_preview_id=${ id }`;
+		return `${ previewURL }?${ stringify( { ...options, newspack_popups_preview_id: id } ) }`;
 	};
 
 	render() {
