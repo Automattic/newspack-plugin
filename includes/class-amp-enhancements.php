823a0bc0dc211f987ce74a3ce95f62ecfdc3f53e
claudiulodro <claudiulodro@gmail.com>
2020-08-05 12:19:45 -0700

    fix: improve performance of WP GDPR Cookie Notice on AMP

--- includes/class-amp-enhancements.php
+++ includes/class-amp-enhancements.php
@@ -7,6 +7,10 @@
 
 namespace Newspack;
 
+use Felix_Arntz\WP_GDPR_Cookie_Notice\Shortcodes\WordPress_Shortcode_Parser;
+use Felix_Arntz\WP_GDPR_Cookie_Notice\Settings\Plugin_Option_Reader;
+use Felix_Arntz\WP_GDPR_Cookie_Notice\Cookie_Notice\Cookie_Notice_AMP_Markup;
+
 defined( 'ABSPATH' ) || exit;
 
 /**
@@ -18,6 +22,10 @@ class AMP_Enhancements {
 	 * Initialize hooks and filters.
 	 */
 	public static function init() {
+		// WP GDPR Cookie Notice plugin enhancements.
+		if ( is_plugin_active( 'wp-gdpr-cookie-notice/wp-gdpr-cookie-notice.php' ) ) {
+			include_once 'amp-enhancements/class-wp-gdpr-cookie-notice-performance.php';
+		}
 	}
 }
 AMP_Enhancements::init();
