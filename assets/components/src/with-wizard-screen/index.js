c11d55d810382693e35cd450970311de6c347802
Adam Borowski <adam@adamboro.com>
2020-11-18 13:10:12 +0100

    refactor: withWizardScreen

--- assets/components/src/with-wizard-screen/index.js
+++ assets/components/src/with-wizard-screen/index.js
@@ -1,9 +1,4 @@
 /**
- * WordPress dependencies
- */
-import { Component, Fragment } from '@wordpress/element';
-
-/**
  * Internal dependencies
  */
 import {
@@ -27,106 +22,83 @@ import classnames from 'classnames';
  * Higher-Order Component to provide plugin management and error handling to Newspack Wizards.
  */
 export default function withWizardScreen( WrappedComponent ) {
-	return class WrappedWithWizardScreen extends Component {
-		render() {
-			const {
-				className,
-				buttonText,
-				buttonAction,
-				buttonDisabled,
-				headerIcon,
-				headerText,
-				subHeaderText,
-				noBackground,
-				noCard,
-				isWide,
-				tabbedNavigation,
-				secondaryNavigation,
-				footer,
-				notice,
-				secondaryButtonText,
-				secondaryButtonAction,
-				secondaryButtonStyle,
-				hidden,
-			} = this.props;
-			const classes = classnames(
-				'newspack-wizard',
-				className,
-				hidden ? 'newspack-wizard__is-hidden' : ''
-			);
-			const content = <WrappedComponent { ...this.props } />;
-			const retrievedButtonProps = buttonProps( buttonAction );
-			const retrievedSecondaryButtonProps = buttonProps( secondaryButtonAction );
-			return (
-				<Fragment>
-					{ ! hidden && (
-						<Grid>
-							<Card noBackground>
-								{ headerText && (
-									<FormattedHeader
-										headerIcon={ headerIcon }
-										headerText={ headerText }
-										subHeaderText={ subHeaderText }
-									/>
-								) }
-							</Card>
-							{ tabbedNavigation && (
-								<Card noBackground>
-									<TabbedNavigation items={ tabbedNavigation } />
-									{ secondaryNavigation && <SecondaryNavigation items={ secondaryNavigation } /> }
-								</Card>
-							) }
-						</Grid>
-					) }
-					{ !! noCard && content }
-					{ ! noCard && (
-						<Grid isWide={ isWide }>
-							<Card className={ classes } noBackground={ noBackground }>
-								{ content }
-								{ ! hidden && (
-									<div className="newspack-buttons-card">
-										{ buttonText && buttonAction && !! retrievedButtonProps.plugin && (
-											<Handoff isPrimary { ...retrievedButtonProps }>
-												{ buttonText }
-											</Handoff>
-										) }
-										{ notice }
-										{ buttonText && buttonAction && ! retrievedButtonProps.plugin && (
-											<Button
-												isPrimary={ ! buttonDisabled }
-												isDefault={ !! buttonDisabled }
-												disabled={ buttonDisabled }
-												{ ...retrievedButtonProps }
-											>
-												{ buttonText }
-											</Button>
-										) }
-										{ footer }
-										{ secondaryButtonText &&
-											secondaryButtonAction &&
-											!! retrievedSecondaryButtonProps.plugin && (
-												<Handoff isDefault { ...retrievedSecondaryButtonProps }>
-													{ secondaryButtonText }
-												</Handoff>
-											) }
-										{ secondaryButtonText &&
-											secondaryButtonAction &&
-											! retrievedSecondaryButtonProps.plugin && (
-												<Button
-													{ ...secondaryButtonStyle }
-													isDefault
-													{ ...retrievedSecondaryButtonProps }
-												>
-													{ secondaryButtonText }
-												</Button>
-											) }
-									</div>
-								) }
-							</Card>
-						</Grid>
-					) }
-				</Fragment>
-			);
+	const WrappedWithWizardScreen = props => {
+		const {
+			className,
+			buttonText,
+			buttonAction,
+			buttonDisabled,
+			headerIcon,
+			headerText,
+			subHeaderText,
+			noBackground,
+			isWide,
+			tabbedNavigation,
+			secondaryNavigation,
+			secondaryButtonText,
+			secondaryButtonAction,
+			hidden,
+		} = props;
+		if ( hidden ) {
+			return null;
 		}
+		const content = <WrappedComponent { ...props } />;
+		const retrievedButtonProps = buttonProps( buttonAction );
+		const retrievedSecondaryButtonProps = buttonProps( secondaryButtonAction );
+		const SecondaryCTAComponent = retrievedSecondaryButtonProps.plugin ? Handoff : Button;
+		return (
+			<>
+				<Grid>
+					<Card noBackground>
+						{ headerText && (
+							<FormattedHeader
+								headerIcon={ headerIcon }
+								headerText={ headerText }
+								subHeaderText={ subHeaderText }
+							/>
+						) }
+					</Card>
+					{ tabbedNavigation && (
+						<Card noBackground>
+							<TabbedNavigation items={ tabbedNavigation } />
+							{ secondaryNavigation && <SecondaryNavigation items={ secondaryNavigation } /> }
+						</Card>
+					) }
+				</Grid>
+
+				<Grid isWide={ isWide }>
+					<Card
+						className={ classnames( 'newspack-wizard', className ) }
+						noBackground={ noBackground }
+					>
+						{ content }
+						<div className="newspack-buttons-card">
+							{ buttonText &&
+								buttonAction &&
+								( retrievedButtonProps.plugin ? (
+									<Handoff isPrimary { ...retrievedButtonProps }>
+										{ buttonText }
+									</Handoff>
+								) : (
+									<Button
+										isPrimary={ ! buttonDisabled }
+										isSecondary={ !! buttonDisabled }
+										disabled={ buttonDisabled }
+										{ ...retrievedButtonProps }
+									>
+										{ buttonText }
+									</Button>
+								) ) }
+							{ secondaryButtonText && secondaryButtonAction && (
+								<SecondaryCTAComponent isSecondary { ...retrievedSecondaryButtonProps }>
+									{ secondaryButtonText }
+								</SecondaryCTAComponent>
+							) }
+						</div>
+					</Card>
+				</Grid>
+			</>
+		);
 	};
+	return WrappedWithWizardScreen;
 }
