4f291db873013dc9b86005fa8c43d1b9aa5777a6
Thomas Guillot <thomasguillot@users.noreply.github.com>
2020-12-18 08:31:58 +0000

    feat: update grid component and wizards using it (#747)
    * feat: update grid component to display content in multiple columns
    
    * feat: remove stylecardgroup and use grid instead
    
    * feat: remove grid from components demo
    
    * feat: remove iswide prop since not used anymore
    
    * feat: simplify dashboard, update grid, remove view buttons
    
    * feat: simplify logo
    
    * style: reorganise grid-template-columns

--- assets/components/src/grid/index.js
+++ assets/components/src/grid/index.js
@@ -15,17 +15,27 @@ import './style.scss';
 /**
  * External dependencies
  */
-import classNames from 'classnames';
+import classnames from 'classnames';
 
 class Grid extends Component {
 	/**
 	 * Render
 	 */
 	render() {
-		const { className, isWide, ...otherProps } = this.props;
-		const classes = classNames( 'newspack-grid', isWide && 'newspack-grid__wide', className );
+		const { className, columns, gutter, ...otherProps } = this.props;
+		const classes = classnames(
+			'newspack-grid',
+			columns && 'newspack-grid__columns-' + columns,
+			gutter && 'newspack-grid__gutter-' + gutter,
+			className
+		);
 		return <div className={ classes } { ...otherProps } />;
 	}
 }
+
+Grid.defaultProps = {
+	columns: 2,
+	gutter: 64,
+};
 
 export default Grid;
