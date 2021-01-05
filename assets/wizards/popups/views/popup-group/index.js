f3a23d9cbb6a9c3038575f1baad006e529176c47
Jefferson Rabb <jefferson.rabb@automattic.com>
2021-01-05 18:52:36 -0500

    feat: campaign group filter with preview (#771)
    * refactor: stateless components
    
    * fix: linting
    
    * feat: ui for picking campaign groups for campaigns
    
    * feat: consolidate campaigns into one tab
    
    * feat: campaign placement in description
    
    * chore: remove unused lodash functions
    
    * feat: campaign group filter
    
    * fix: better label text
    
    * feat: segmentation webview
    
    * fix: sanitize campaign group values
    
    * feat: preview segment from campaigns view
    
    * fix: linting errors
    
    * feat: improved preview ui
    
    * fix: broken preview functionality
    
    * feat: replace filter with group select
    
    * chore: remove unused action card sections component
    
    * feat: revised ui for previewing campaign groups
    
    * feat: add style to the segmentation preview functionnality
    
    * fix: switch preview button back to tertiary
    
    * feat: temporarily revert taxonomy slug approach
    
    * fix: linting errors
    
    * fix: remove duplicate add buttons
    
    * fix: remove console log statement
    
    Co-authored-by: Thomas Guillot <info@thomasguillot.com>

--- assets/wizards/popups/views/popup-group/index.js
+++ assets/wizards/popups/views/popup-group/index.js
@@ -8,6 +8,7 @@
 import apiFetch from '@wordpress/api-fetch';
 import { useEffect, useState, Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { ESCAPE } from '@wordpress/keycodes';
 
 /**
  * External dependencies.
@@ -17,8 +18,9 @@ import { find } from 'lodash';
 /**
  * Internal dependencies
  */
-import { withWizardScreen, Button, SelectControl } from '../../../../components/src';
+import { withWizardScreen, Button, Popover, SelectControl } from '../../../../components/src';
 import PopupActionCard from '../../components/popup-action-card';
+import SegmentationPreview from '../../components/segmentation-preview';
 import { isOverlay } from '../../utils';
 import './style.scss';
 
@@ -68,6 +70,8 @@ const PopupGroup = ( {
 } ) => {
 	const [ campaignGroup, setCampaignGroup ] = useState( -1 );
 	const [ campaignGroups, setCampaignGroups ] = useState( -1 );
+	const [ segmentId, setSegmentId ] = useState();
+	const [ previewPopoverIsVisible, setPreviewPopoverIsVisible ] = useState();
 
 	useEffect( () => {
 		apiFetch( {
@@ -104,29 +108,77 @@ const PopupGroup = ( {
 	return (
 		<Fragment>
 			<div className="newspack-campaigns__popup-group__filter-group-wrapper">
-				<SelectControl
-					options={
-						-1 === campaignGroups
-							? [
-									{
-										value: -1,
-										label: __( 'Loading...', 'newspack' ),
-									},
-							  ]
-							: [
-									{ value: -1, label: __( 'All Campaigns', 'newspack' ) },
-									...campaignGroups.map( term => ( {
-										value: term.id,
-										label: term.name,
-									} ) ),
-							  ]
-					}
-					value={ campaignGroup }
-					onChange={ value => setCampaignGroup( +value ) }
-					label={ __( 'Groups', 'newspack' ) }
-					labelPosition="side"
-					disabled={ -1 === campaignGroups }
-				/>
+				<div className="newspack-campaigns__popup-group__filter-group-actions">
+					<SelectControl
+						options={
+							-1 === campaignGroups
+								? [
+										{
+											value: -1,
+											label: __( 'Loading...', 'newspack' ),
+										},
+								  ]
+								: [
+										{ value: -1, label: __( 'All Campaigns', 'newspack' ) },
+										...campaignGroups.map( term => ( {
+											value: term.id,
+											label: term.name,
+										} ) ),
+								  ]
+						}
+						value={ campaignGroup }
+						onChange={ value => setCampaignGroup( +value ) }
+						label={ __( 'Groups', 'newspack' ) }
+						labelPosition="side"
+						disabled={ -1 === campaignGroups }
+					/>
+					{ campaignGroup > 0 && (
+						<SegmentationPreview
+							campaignGroups={ campaignGroup }
+							segment={ segmentId }
+							renderButton={ ( { showPreview } ) => (
+								<div className="newspack-campaigns__popup-group__filter-group-segmentation">
+									<Button
+										isTertiary
+										isSmall
+										onClick={ () => setPreviewPopoverIsVisible( ! previewPopoverIsVisible ) }
+									>
+										{ __( 'Preview', 'newspack' ) }
+									</Button>
+									{ previewPopoverIsVisible && (
+										<Popover
+											className="has-select-border"
+											position="bottom right"
+											onFocusOutside={ () => setPreviewPopoverIsVisible( false ) }
+											onKeyDown={ event =>
+												ESCAPE === event.keyCode && setPreviewPopoverIsVisible( false )
+											}
+										>
+											<SelectControl
+												options={ [
+													{ value: '', label: __( 'Default (no segment)', 'newspack' ) },
+													...segments.map( s => ( { value: s.id, label: s.name } ) ),
+												] }
+												value={ segmentId }
+												onChange={ setSegmentId }
+												label={ __( 'Segment to preview', 'newspack' ) }
+											/>
+											<Button
+												isLink
+												onClick={ () => {
+													showPreview();
+													setPreviewPopoverIsVisible( false );
+												} }
+											>
+												{ __( 'Preview', 'newspack' ) }
+											</Button>
+										</Popover>
+									) }
+								</div>
+							) }
+						/>
+					) }
+				</div>
 				<Button isPrimary isSmall href="/wp-admin/post-new.php?post_type=newspack_popups_cpt">
 					{ __( 'Add New', 'newspack' ) }
 				</Button>
@@ -146,12 +198,12 @@ const PopupGroup = ( {
 					publishPopup={ publishPopup }
 				/>
 			) ) }
-			{ campaignsToDisplay.length < 1 &&
-				-1 === campaignGroup &&
-				__( 'No Campaigns have been created yet.', 'newspack' ) }
-			{ campaignsToDisplay.length < 1 &&
-				campaignGroup > 0 &&
-				__( 'There are no Campaigns in this group.', 'newspack' ) }
+			{ campaignsToDisplay.length < 1 && -1 === campaignGroup && (
+				<p>{ __( 'No Campaigns have been created yet.', 'newspack' ) }</p>
+			) }
+			{ campaignsToDisplay.length < 1 && campaignGroup > 0 && (
+				<p>{ __( 'There are no Campaigns in this group.', 'newspack' ) }</p>
+			) }
 		</Fragment>
 	);
 };
