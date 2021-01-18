5fb21aad9173531c4235176f031a3ea059461276
Jefferson Rabb <jeff@atavist.net>
2021-01-18 00:02:11 -0500

    refactor: simplify code

--- assets/components/src/starter-content-installer/index.js
+++ assets/components/src/starter-content-installer/index.js
@@ -23,46 +23,53 @@ const StarterContentInstaller = ( {
 	inProgress,
 	postCount = 6,
 } ) => {
-	const [ requestStack, setRequestStack ] = useState( [] );
 	const [ progress, setProgress ] = useState( 0 );
-
-	useEffect( () => {
-		if ( ! progress ) {
-			return;
-		}
-		const endpoint = requestStack[ progress ];
-		apiFetch( {
-			path: '/newspack/v1/wizard/newspack-setup-wizard/starter-content/' + endpoint,
-			method: 'POST',
-		} ).then( () => {
-			if ( progress < requestStack.length ) {
-				setProgress( progress + 1 );
-			} else {
-				onComplete();
-			}
-		} );
-	}, [ progress, requestStack ] );
-
-	const install = () => {
-		const stack = [ 'categories' ];
-		for ( let x = 0; x < postCount; x++ ) {
-			stack.push( `post/${ x }` );
-		}
-		stack.push( 'homepage' );
-		stack.push( 'theme' );
-		setRequestStack( stack );
-		setProgress( 1 );
+	const [ total, setTotal ] = useState( 0 );
+	const increment = () => {
+		setProgress( progress + 1 );
 	};
+	const install = () => {
+		// there are 12 categories in starter content, this will result in one post in each for e2e
+		const starterPostsCount = newspack_aux_data.is_e2e ? 12 : 40;
+		setProgress( 1 );
+		setTotal( starterPostsCount + 3 );
 
-	if ( ! progress ) {
-		return (
-			<Button isPrimary onClick={ () => install() }>
-				{ __( 'Install', 'newspack' ) }
-			</Button>
-		);
-	}
+		apiFetch( {
+			path: `/newspack/v1/wizard/newspack-setup-wizard/starter-content/categories`,
+			method: 'post',
+		} ).then( increment );
 
-	return <ProgressBar completed={ progress } total={ requestStack.length } displayFraction />;
+		const postsPromises = [];
+		for ( let x = 0; x < starterPostsCount; x++ ) {
+			postsPromises.push(
+				apiFetch( {
+					path: `/newspack/v1/wizard/newspack-setup-wizard/starter-content/post/${ x }`,
+					method: 'post',
+				} )
+					.then( increment )
+					.catch( increment )
+			);
+		}
+
+		Promise.all( postsPromises );
+
+		apiFetch( {
+			path: `/newspack/v1/wizard/newspack-setup-wizard/starter-content/homepage`,
+			method: 'post',
+		} ).then( increment );
+
+		apiFetch( {
+			path: `/newspack/v1/wizard/newspack-setup-wizard/starter-content/theme`,
+			method: 'post',
+		} ).then( increment );
+	};
+	return progress ? (
+		<ProgressBar completed={ progress } total={ total } displayFraction />
+	) : (
+		<Button isPrimary onClick={ () => install() }>
+			{ __( 'Install', 'newspack' ) }
+		</Button>
+	);
 };
 
 export default StarterContentInstaller;
