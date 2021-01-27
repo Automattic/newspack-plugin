164cc002055107d601c7f510ad1361e152e050d4
Thomas Guillot <info@thomasguillot.com>
2021-01-27 11:24:03 +0000

    feat: popup group use card

--- assets/wizards/popups/views/popup-group/index.js
+++ assets/wizards/popups/views/popup-group/index.js
@@ -69,32 +69,32 @@ const getCardClassName = ( { options, sitewide_default: sitewideDefault, status 
 
 const iconInline = (
 	<SVG xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-		<Path d="M21 11.01L3 11v2h18zM3 16h12v2H3zM21 6H3v2.01L21 8z"/>
+		<Path d="M21 11.01L3 11v2h18zM3 16h12v2H3zM21 6H3v2.01L21 8z" />
 	</SVG>
 );
 const iconCenterOverlay = (
 	<SVG xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-		<Path d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM7 9h10v6H7V9z"/>
+		<Path d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM7 9h10v6H7V9z" />
 	</SVG>
 );
 const iconTopOverlay = (
 	<SVG xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-		<Path d="M3 21h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2zM3 5h18v14H3V5zm16 4H5V6h14v3z"/>
+		<Path d="M3 21h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2zM3 5h18v14H3V5zm16 4H5V6h14v3z" />
 	</SVG>
 );
 const iconBottomOverlay = (
 	<SVG xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-		<Path d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM5 15h14v3H5z"/>
+		<Path d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H3V5h18v14zM5 15h14v3H5z" />
 	</SVG>
 );
 const iconAboveHeader = (
 	<SVG xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-		<Path d="M3 21h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2zM3 8h18v11H3V8z"/>
+		<Path d="M3 21h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2zM3 8h18v11H3V8z" />
 	</SVG>
 );
 const iconManualPlacement = (
 	<SVG xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
-		<Path d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM3 8h11.5v4.5H3V8zm0 6.5h11.5V19H3v-4.5zM21 19h-4.5V8H21v11z"/>
+		<Path d="M21 3H3c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h18c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM3 8h11.5v4.5H3V8zm0 6.5h11.5V19H3v-4.5zM21 19h-4.5V8H21v11z" />
 	</SVG>
 );
 
@@ -113,8 +113,8 @@ const Segment = ( {
 	const [ addNewPopoverIsVisible, setAddNewPopoverIsVisible ] = useState();
 	const { label, id, items } = segment;
 	return (
-		<Fragment>
-			<h2 className="newspack-campaigns__popup-group__subhead">
+		<Card isSmall className="newspack-campaigns__popup-group__card">
+			<h3 className="newspack-campaigns__popup-group__card__segment">
 				{ label }
 				{ items.length > 0 ? (
 					<SegmentationPreview
@@ -128,7 +128,7 @@ const Segment = ( {
 						) }
 					/>
 				) : null }
-			</h2>
+			</h3>
 			{ items.map( item => (
 				<PopupActionCard
 					className={ getCardClassName( item ) }
@@ -205,7 +205,7 @@ const Segment = ( {
 					) }
 				</div>
 			) }
-		</Fragment>
+		</Card>
 	);
 };
 /**
@@ -353,7 +353,9 @@ const PopupGroup = ( {
 							<Card buttonsCard noBorder>
 								<Button
 									isSecondary
-									onClick={ () => { setAddNewPopoverIsVisible( false ); } }
+									onClick={ () => {
+										setAddNewPopoverIsVisible( false );
+									} }
 								>
 									{ __( 'Cancel', 'newspack' ) }
 								</Button>
