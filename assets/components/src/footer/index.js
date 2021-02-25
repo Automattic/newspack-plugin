20fd6a98a041d10f4fe77f48082d6ac6ff5812ee
Thomas Guillot <info@thomasguillot.com>
2021-02-25 09:56:46 +0000

    fix: avoid using class-based component

--- assets/components/src/footer/index.js
+++ assets/components/src/footer/index.js
@@ -6,7 +6,6 @@
  * WordPress dependencies.
  */
 import { __ } from '@wordpress/i18n';
-import { Component } from '@wordpress/element';
 import { ExternalLink } from '@wordpress/components';
 
 /**
@@ -15,86 +14,80 @@ import { ExternalLink } from '@wordpress/components';
 import { PatronsLogo } from '../';
 import './style.scss';
 
-class Footer extends Component {
-	/**
-	 * Render
-	 */
-	render() {
-		const componentsDemo = window && window.newspack_urls && window.newspack_urls.components_demo;
-		const setupWizard = window && window.newspack_urls && window.newspack_urls.setup_wizard;
-		const resetUrl = window && window.newspack_urls && window.newspack_urls.reset_url;
-		const resetWpcomUrl = window && window.newspack_urls && window.newspack_urls.reset_wpcom_url;
-		const pluginVersion = window && window.newspack_urls && window.newspack_urls.plugin_version;
-		const removeStarterContent =
-			window && window.newspack_urls && window.newspack_urls.remove_starter_content;
-		const footerElements = [
-			{
-				label: pluginVersion.label,
-				url: pluginVersion.url,
-			},
-			{
-				label: __( 'About', 'newspack' ),
-				url: 'https://newspack.pub/',
-				external: true,
-			},
-			{
-				label: __( 'Documentation', 'newspack' ),
-				url: 'https://newspack.pub/support/',
-				external: true,
-			},
-		];
-		if ( componentsDemo ) {
-			footerElements.push( {
-				label: __( 'Components Demo', 'newspack' ),
-				url: componentsDemo,
-			} );
-		}
-		if ( setupWizard ) {
-			footerElements.push( {
-				label: __( 'Setup Wizard', 'newspack' ),
-				url: setupWizard,
-			} );
-		}
-		if ( resetUrl ) {
-			footerElements.push( {
-				label: __( 'Reset Newspack', 'newspack' ),
-				url: resetUrl,
-			} );
-		}
-		if ( resetWpcomUrl ) {
-			footerElements.push( {
-				label: __( 'Reset WordPress.com Authentication', 'newspack' ),
-				url: resetWpcomUrl,
-			} );
-		}
-		if ( removeStarterContent ) {
-			footerElements.push( {
-				label: __( 'Remove Starter Content', 'newspack' ),
-				url: removeStarterContent,
-			} );
-		}
-		const { simple } = this.props;
-		return (
-			<div className="newspack-footer">
-				<PatronsLogo />
-				{ ! simple && (
-					<div className="newspack-footer__inner">
-						<ul>
-							{ footerElements.map( ( { url, label, external }, index ) => (
-								<li key={ index }>
-									{ external ? (
-										<ExternalLink href={ url }>{ label }</ExternalLink>
-									) : (
-										<a href={ url }>{ label }</a>
-									) }
-								</li>
-							) ) }
-						</ul>
-					</div>
-				) }
-			</div>
-		);
+const Footer = ( { simple } ) => {
+	const componentsDemo = window && window.newspack_urls && window.newspack_urls.components_demo;
+	const setupWizard = window && window.newspack_urls && window.newspack_urls.setup_wizard;
+	const resetUrl = window && window.newspack_urls && window.newspack_urls.reset_url;
+	const resetWpcomUrl = window && window.newspack_urls && window.newspack_urls.reset_wpcom_url;
+	const pluginVersion = window && window.newspack_urls && window.newspack_urls.plugin_version;
+	const removeStarterContent =
+		window && window.newspack_urls && window.newspack_urls.remove_starter_content;
+	const footerElements = [
+		{
+			label: pluginVersion.label,
+			url: pluginVersion.url,
+		},
+		{
+			label: __( 'About', 'newspack' ),
+			url: 'https://newspack.pub/',
+			external: true,
+		},
+		{
+			label: __( 'Documentation', 'newspack' ),
+			url: 'https://newspack.pub/support/',
+			external: true,
+		},
+	];
+	if ( componentsDemo ) {
+		footerElements.push( {
+			label: __( 'Components Demo', 'newspack' ),
+			url: componentsDemo,
+		} );
 	}
+	if ( setupWizard ) {
+		footerElements.push( {
+			label: __( 'Setup Wizard', 'newspack' ),
+			url: setupWizard,
+		} );
+	}
+	if ( resetUrl ) {
+		footerElements.push( {
+			label: __( 'Reset Newspack', 'newspack' ),
+			url: resetUrl,
+		} );
+	}
+	if ( resetWpcomUrl ) {
+		footerElements.push( {
+			label: __( 'Reset WordPress.com Authentication', 'newspack' ),
+			url: resetWpcomUrl,
+		} );
+	}
+	if ( removeStarterContent ) {
+		footerElements.push( {
+			label: __( 'Remove Starter Content', 'newspack' ),
+			url: removeStarterContent,
+		} );
+	}
+	return (
+		<div className="newspack-footer">
+			<PatronsLogo />
+			{ ! simple && (
+				<div className="newspack-footer__inner">
+					<ul>
+						{ footerElements.map( ( { url, label, external }, index ) => (
+							<li key={ index }>
+								{ external ? (
+									<ExternalLink href={ url }>{ label }</ExternalLink>
+								) : (
+									<a href={ url }>{ label }</a>
+								) }
+							</li>
+						) ) }
+					</ul>
+				</div>
+			) }
+		</div>
+	);
 }
 
 export default Footer;
