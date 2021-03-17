254f08b14f2f2d85bd559ca904f89e816c96d86f
Adam Boro <adam@adamboro.com>
2021-03-17 09:56:41 +0100

    feat(setup): design step (#892)
    - feat: update ColorPicker component
    
    - feat: handle optgroups in SelectControl
    
    - feat(SelectControl): handle buttons group
    
    - feat: theme colors settings in Onboarding & Wizard
    
    - feat: typography settings
    
    - feat: footer, header color settings
    
    - feat: header, footer settings
    
    - feat: remove header settings from the Settings view
    
    - feat: header, footer logos settings
    
    - feat: ButtonGroup styling
    
    - feat: title & tagline mods
    
    - feat: save & preview button
    
    - chore: phpcs
    
    - chore: style lint
    
    - chore: js lint
    
    - fix: typo
    
    - style: update site preview button
    
    - feat: add more display fonts
    
    - feat: adjust grid gutter
    
    - feat(color-picker): update style
    
    - feat: fonts improvements
    
    - fix(color-picker): padding on the expander
    
    - feat: update groupedselectcontrol to look like selectcontrol
    
    - fix(web-preview): adjust buttons padding
    
    - style(button-group): adjust after and focus state to is-pressed
    
    - feat: update size and style button-groups
    
    - feat(site-design): update imageupload style
    
    - style(image-upload): add background to upload button
    
    - fix(site-design): add icon-only class to logo size selectcontrol
    
    - feat: reorganise site-design to use grid
    
    - feat(site-design): update copyright help text
    
    - feat(setup): update subHeaderText for design step
    
    - feat: title & tagline dependent on custom logo
    
    - feat: handle custom vs default theme colors
    
    - feat: update header logo sizes options
    
    - fix: colors
    
    - feat: force custom header color
    
    - feat: default footer copyright value
    
    - feat: remove info icon
    
    - feat: remove info icon
    
    - style: remove unneeded css
    
    Co-authored-by: Thomas Guillot <info@thomasguillot.com>

---
+++ assets/components/src/select-control/GroupedSelectControl.js
@@ -0,0 +1,71 @@
+/**
+ * WordPress dependencies
+ */
+import { BaseControl } from '@wordpress/components';
+import { Icon, chevronDown } from '@wordpress/icons';
+
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+import { find, some } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { hooks } from '..';
+
+/**
+ * SelectControl with optgroup support
+ */
+export default function GroupedSelectControl( {
+	help,
+	label,
+	onChange,
+	optgroups = [],
+	className,
+	hideLabelFromVision,
+	...props
+} ) {
+	const onChangeValue = event => {
+		const { value } = event.target;
+		const optgroup = find( optgroups, group => some( group.options, [ 'value', value ] ) );
+		onChange( value, optgroup );
+	};
+	const id = hooks.useUniqueId( 'group-select' );
+
+	return (
+		<BaseControl
+			label={ label }
+			hideLabelFromVision={ hideLabelFromVision }
+			id={ id }
+			help={ help }
+			className={ classnames( className, 'components-select-control' ) }
+		>
+			<select
+				id={ id }
+				className="components-select-control__input"
+				onChange={ onChangeValue }
+				aria-describedby={ !! help ? `${ id }__help` : undefined }
+				{ ...props }
+			>
+				{ optgroups.map( ( { label: optgroupLabel, options }, optgroupIndex ) => (
+					<optgroup label={ optgroupLabel } key={ optgroupIndex }>
+						{ options.map( ( option, optionIndex ) => (
+							<option
+								key={ `${ option.label }-${ option.value }-${ optionIndex }` }
+								value={ option.value }
+								disabled={ option.disabled }
+							>
+								{ option.label }
+							</option>
+						) ) }
+					</optgroup>
+				) ) }
+			</select>
+			<div className="components-select-control__arrow-wrapper">
+				<Icon icon={ chevronDown } size={ 18 } />
+			</div>
+		</BaseControl>
+	);
+}
