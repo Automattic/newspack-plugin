7a8af1cf9f52cba46ccc0ace0932a75315d3249f
Derrick Koo <derrick.koo@automattic.com>
2021-03-22 14:00:01 -0600

    chore: remove unused var for JS lint

--- assets/wizards/popups/components/segment-group/SegmentGroup.test.js
+++ assets/wizards/popups/components/segment-group/SegmentGroup.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitForElement, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 import SegmentGroup from './index';
