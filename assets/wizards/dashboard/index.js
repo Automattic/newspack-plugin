d17e3d319d53177dd88ea1c59f6a9dca497708bc
Thomas Guillot <info@thomasguillot.com>
2021-04-16 16:41:05 +0100

    feat: move auth elements to the grid and globalnotices above the header

--- assets/wizards/dashboard/index.js
+++ assets/wizards/dashboard/index.js
@@ -13,13 +13,27 @@ import qs from 'qs';
 import { useEffect, useState, Fragment, render } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 import { __ } from '@wordpress/i18n';
+import { Icon, plugins } from '@wordpress/icons';
 
 /**
  * Internal dependencies.
  */
-import { GlobalNotices, Button, Waiting, Footer, Grid, NewspackLogo } from '../../components/src';
+import {
+	GlobalNotices,
+	Button,
+	Card,
+	Waiting,
+	Footer,
+	Grid,
+	NewspackLogo,
+} from '../../components/src';
 import DashboardCard from './views/dashboardCard';
 import './style.scss';
+
+/**
+ * External dependencies.
+ */
+import classnames from 'classnames';
 
 const Dashboard = ( { items } ) => {
 	const params = qs.parse( window.location.search );
@@ -75,43 +89,48 @@ const Dashboard = ( { items } ) => {
 
 	return (
 		<Fragment>
+			<GlobalNotices />
 			<div className="newspack-wizard__header">
 				<div className="newspack-wizard__header__inner">
 					<NewspackLogo centered height={ 72 } />
 				</div>
 			</div>
-			<div className="mw6 mr-auto ml-auto">
-				<GlobalNotices />
-			</div>
-			{ authCode && (
-				<div className="pt4 flex justify-around">
-					<Waiting />
-				</div>
-			) }
-			{ displayAuth ? (
-				<div className="pt4 ph3 flex justify-around">
-					{ userBasicInfo ? (
-						<span>
-							{ __( 'Authorized Google as', 'newspack' ) } <strong>{ userBasicInfo.email }</strong>.
-						</span>
-					) : (
-						<div>
-							<p>
-								{ __( 'Click on the button below to authorise Newspack with Google.', 'newspack' ) }
-							</p>
-							<br />
-							<Button isPrimary onClick={ goToAuthPage }>
-								{ __( 'Authorise', 'newspack' ) }
-							</Button>
-						</div>
-					) }
-				</div>
-			) : null }
 			<div className="newspack-wizard newspack-wizard__content">
 				<Grid columns={ 3 } gutter={ 32 }>
 					{ items.map( card => (
 						<DashboardCard { ...card } key={ card.slug } />
 					) ) }
+					{ authCode && (
+						<div className="flex justify-around items-center">
+							<Waiting />
+						</div>
+					) }
+					{ displayAuth ? (
+						<Card className={ classnames( 'newspack-dashboard-card', 'google-oauth2' ) }>
+							{ userBasicInfo ? (
+								<div className="newspack-dashboard-card__contents">
+									<Icon icon={ plugins } />
+									<div className="newspack-dashboard-card__header">
+										<h2>{ __( 'Google OAuth2' ) }</h2>
+										<p>
+											{ __( 'Authorized Google as', 'newspack' ) }{' '}
+											<strong>{ userBasicInfo.email }</strong>
+										</p>
+									</div>
+								</div>
+							) : (
+								<Button onClick={ goToAuthPage }>
+									<div className="newspack-dashboard-card__contents">
+										<Icon icon={ plugins } />
+										<div className="newspack-dashboard-card__header">
+											<h2>{ __( 'Google OAuth2' ) }</h2>
+											<p>{ __( 'Authorise Newspack with Google', 'newspack' ) }</p>
+										</div>
+									</div>
+								</Button>
+							) }
+						</Card>
+					) : null }
 				</Grid>
 			</div>
 			<Footer />
