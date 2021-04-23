0d7e35621788f76bba25282ef4e3e8bf4401d30e
Derrick Koo <derrick.koo@automattic.com>
2021-04-23 16:00:53 -0600

    feat: integrate Newspack Scheduled Post Checker into main plugin

--- includes/class-newspack.php
+++ includes/class-newspack.php
@@ -112,6 +112,9 @@ final class Newspack {
 		}
 
 		include_once NEWSPACK_ABSPATH . 'includes/configuration_managers/class-configuration-managers.php';
+
+		// Scheduled post checker CRON job.
+		include_once NEWSPACK_ABSPATH . 'includes/scheduled-post-checker/scheduled-post-checker.php';
 	}
 
 	/**
