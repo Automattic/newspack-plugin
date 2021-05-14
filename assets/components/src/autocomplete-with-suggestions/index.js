74b51b99c8b317038edef2cccccb920737531c78
Derrick Koo <derrick.koo@automattic.com>
2021-05-14 11:05:49 -0600

    refactor: make component functional

--- assets/components/src/autocomplete-with-suggestions/index.js
+++ assets/components/src/autocomplete-with-suggestions/index.js
@@ -3,7 +3,7 @@
  */
 import { __, sprintf } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
-import { Component } from '@wordpress/element';
+import { useEffect, useState } from '@wordpress/element';
 
 /**
  * External dependencies.
@@ -11,24 +11,29 @@ import { Component } from '@wordpress/element';
 import { AutocompleteTokenField } from '../';
 import './style.scss';
 
-class AutocompleteWithSuggestions extends Component {
-	constructor( props ) {
-		super( props );
-		this.state = {
-			suggestions: [],
-		};
-	}
+const AutocompleteWithSuggestions = ( {
+	fetchSavedPosts = false,
+	fetchSuggestions = false,
+	onChange = false,
+	help = __( 'Begin typing search term, click autocomplete result to select.', 'newspack' ),
+	label = __( 'Search', 'newspack' ),
+	postTypeLabel = __( 'post', 'newspack' ),
+	selectedPost = 0,
+} ) => {
+	const [ suggestions, setSuggestions ] = useState( [] );
 
 	/**
 	 * Fetch recent posts to show as suggestions.
 	 */
-	componentDidMount() {
-		this.props.fetchSuggestions().then( suggestions => {
-			if ( 0 < suggestions.length ) {
-				this.setState( { suggestions } );
-			}
-		} );
-	}
+	useEffect( () => {
+		if ( fetchSuggestions ) {
+			fetchSuggestions().then( suggestions => {
+				if ( 0 < suggestions.length ) {
+					setSuggestions( suggestions );
+				}
+			} );
+		}
+	}, [] );
 
 	/**
 	 * Render a single suggestion object that can be clicked to select it immediately.
@@ -36,63 +41,43 @@ class AutocompleteWithSuggestions extends Component {
 	 * @param {Object} suggestion Suggestion object with value and label keys.
 	 * @param {number} index Index of this suggestion in the array.
 	 */
-	renderSuggestion( suggestion, index ) {
-		const { onChange } = this.props;
+	const renderSuggestion = ( suggestion, index ) => {
 		return (
 			<Button isLink key={ index } onClick={ () => onChange( [ suggestion ] ) }>
 				{ suggestion.label }
 			</Button>
 		);
-	}
+	};
 
 	/**
 	 * Render a list of suggestions that can be clicked to select instead of searching by title.
 	 */
-	renderSuggestions() {
-		const { postTypeLabel } = this.props;
-		const { suggestions } = this.state;
-
+	const renderSuggestions = () => {
 		return (
 			<>
 				<p className="newspack-autocomplete-with-suggestions__label">
 					{ __( 'Or, select a recent ', 'newspack' ) + sprintf( ' %s:', postTypeLabel ) }
 				</p>
 				<div className="newspack-autocomplete-with-suggestions__search-suggestions">
-					{ suggestions.map( this.renderSuggestion.bind( this ) ) }
+					{ suggestions.map( renderSuggestion.bind( this ) ) }
 				</div>
 			</>
 		);
-	}
-
-	render = () => {
-		const { fetchSuggestions, fetchSavedPosts, label, help, selectedPost, onChange } = this.props;
-		const { suggestions } = this.state;
-
-		return (
-			<div className="newspack-autocomplete-with-suggestions">
-				<AutocompleteTokenField
-					tokens={ [ selectedPost ] }
-					onChange={ onChange }
-					fetchSuggestions={ fetchSuggestions }
-					fetchSavedInfo={ postIDs => fetchSavedPosts( postIDs ) }
-					label={ label }
-					help={ help }
-				/>
-				{ 0 < suggestions.length && this.renderSuggestions() }
-			</div>
-		);
 	};
-}
 
-AutocompleteWithSuggestions.defaultProps = {
-	fetchSavedPosts: () => false,
-	fetchSuggestions: () => false,
-	onChange: () => false,
-	help: __( 'Begin typing search term, click autocomplete result to select.', 'newspack' ),
-	label: __( 'Search', 'newspack' ),
-	postType: 'post',
-	postTypeLabel: __( 'post', 'newspack' ),
-	selectedPost: 0,
+	return (
+		<div className="newspack-autocomplete-with-suggestions">
+			<AutocompleteTokenField
+				tokens={ [ selectedPost ] }
+				onChange={ onChange }
+				fetchSuggestions={ fetchSuggestions }
+				fetchSavedInfo={ postIDs => fetchSavedPosts( postIDs ) }
+				label={ label }
+				help={ help }
+			/>
+			{ 0 < suggestions.length && renderSuggestions() }
+		</div>
+	);
 };
 
 export default AutocompleteWithSuggestions;
