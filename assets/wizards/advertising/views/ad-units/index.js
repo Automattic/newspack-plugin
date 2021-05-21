bf5fa15121cd1ffccc938cbe8fc7684369583271
Derrick Koo <derrick.koo@automattic.com>
2021-05-21 14:50:16 -0600

    fix: prevent external links from ad dashboard UI

--- assets/wizards/advertising/views/ad-units/index.js
+++ assets/wizards/advertising/views/ad-units/index.js
@@ -5,46 +5,46 @@
 /**
  * WordPress dependencies
  */
-import { Component, Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import { ActionCard, withWizardScreen } from '../../../../components/src';
+import { ActionCard, Router, withWizardScreen } from '../../../../components/src';
+
+/**
+ * Router component for managing single-page app nav.
+ */
+const { useHistory } = Router;
 
 /**
  * Advertising management screen.
  */
-class AdUnits extends Component {
-	/**
-	 * Render.
-	 */
-	render() {
-		const { adUnits, onDelete, service } = this.props;
+const AdUnits = ( { adUnits, onDelete, service } ) => {
+	const history = useHistory();
 
-		return (
-			<Fragment>
-				<p>
-					{ __(
-						'Set up multiple ad units to use on your homepage, articles and other places throughout your site. You can place ads through our Newspack Ad Block in the Editor.'
-					) }
-				</p>
-				{ Object.values( adUnits ).map( ( { id, name } ) => {
-					return (
-						<ActionCard
-							key={ id }
-							title={ name }
-							actionText={ __( 'Edit' ) }
-							href={ `#${ service }/${ id }` }
-							secondaryActionText={ __( 'Delete' ) }
-							onSecondaryActionClick={ () => onDelete( id ) }
-						/>
-					);
-				} ) }
-			</Fragment>
-		);
-	}
-}
+	return (
+		<>
+			<p>
+				{ __(
+					'Set up multiple ad units to use on your homepage, articles and other places throughout your site. You can place ads through our Newspack Ad Block in the Editor.'
+				) }
+			</p>
+			{ Object.values( adUnits ).map( ( { id, name } ) => {
+				return (
+					<ActionCard
+						key={ id }
+						title={ name }
+						actionText={ __( 'Edit' ) }
+						titleLink={ `#${ service }/${ id }` }
+						onClick={ () => history.push( `${ service }/${ id }` ) }
+						secondaryActionText={ __( 'Delete' ) }
+						onSecondaryActionClick={ () => onDelete( id ) }
+					/>
+				);
+			} ) }
+		</>
+	);
+};
 
 export default withWizardScreen( AdUnits );
--- assets/wizards/advertising/views/services/index.js
+++ assets/wizards/advertising/views/services/index.js
@@ -5,77 +5,78 @@
 /**
  * WordPress dependencies
  */
-import { Component, Fragment } from '@wordpress/element';
 import { ExternalLink } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import { ActionCard, withWizardScreen } from '../../../../components/src';
+import { ActionCard, Router, withWizardScreen } from '../../../../components/src';
+
+/**
+ * Router component for managing single-page app nav.
+ */
+const { useHistory } = Router;
 
 /**
  * Advertising management screen.
  */
-class Services extends Component {
-	/**
-	 * Render.
-	 */
-	render() {
-		const { services, toggleService } = this.props;
-		const { wordads, google_adsense, google_ad_manager } = services;
-		return (
-			<Fragment>
-				<p>
-					{ __( 'Please enable and configure the ad providers you’d like to use to get started.' ) }
-				</p>
-				<ActionCard
-					title={ __( 'WordAds from WordPress.com' ) }
-					badge={ __( 'Jetpack Premium' ) }
-					description={ __(
-						'A managed ad optimization platform where the top 50 ad networks (DSPs and exchanges) compete for your traffic, with flexible placement options, and support from WordPress.com.'
-					) }
-					actionText={ wordads && wordads.enabled && __( 'Configure' ) }
-					toggle
-					toggleChecked={ wordads && wordads.enabled }
-					toggleOnChange={ value => toggleService( 'wordads', value ) }
-					href={ wordads && '#/ad-placements' }
-					notification={
-						wordads.upgrade_required && [
-							__( 'Upgrade Jetpack to enable WordAds.' ) + '\u00A0',
-							<ExternalLink href="/wp-admin/admin.php?page=jetpack#/plans" key="jetpack-link">
-								{ __( 'Click to upgrade' ) }
-							</ExternalLink>,
-						]
-					}
-					notificationLevel={ 'info' }
-				/>
-				<ActionCard
-					title={ __( 'Google AdSense' ) }
-					description={ __(
-						'A simple way to place adverts on your news site automatically based on where they best perform.'
-					) }
-					actionText={ google_adsense && google_adsense.enabled && __( 'Configure' ) }
-					toggle
-					toggleChecked={ google_adsense && google_adsense.enabled }
-					toggleOnChange={ value => toggleService( 'google_adsense', value ) }
-					handoff="google-site-kit"
-					editLink="admin.php?page=googlesitekit-module-adsense"
-				/>
-				<ActionCard
-					title={ __( 'Google Ad Manager' ) }
-					description={ __(
-						'An advanced ad inventory creation and management platform, allowing you to be specific about ad placements.'
-					) }
-					actionText={ google_ad_manager && google_ad_manager.enabled && __( 'Configure' ) }
-					toggle
-					toggleChecked={ google_ad_manager && google_ad_manager.enabled }
-					toggleOnChange={ value => toggleService( 'google_ad_manager', value ) }
-					href={ google_ad_manager && '#/google_ad_manager' }
-				/>
-			</Fragment>
-		);
-	}
-}
+const Services = ( { services, toggleService } ) => {
+	const { wordads, google_adsense, google_ad_manager } = services;
+	const history = useHistory();
+
+	return (
+		<>
+			<p>
+				{ __( 'Please enable and configure the ad providers you’d like to use to get started.' ) }
+			</p>
+			<ActionCard
+				title={ __( 'WordAds from WordPress.com' ) }
+				badge={ __( 'Jetpack Premium' ) }
+				description={ __(
+					'A managed ad optimization platform where the top 50 ad networks (DSPs and exchanges) compete for your traffic, with flexible placement options, and support from WordPress.com.'
+				) }
+				actionText={ wordads && wordads.enabled && __( 'Configure' ) }
+				toggle
+				toggleChecked={ wordads && wordads.enabled }
+				toggleOnChange={ value => toggleService( 'wordads', value ) }
+				href={ wordads && '#/ad-placements' }
+				notification={
+					wordads.upgrade_required && [
+						__( 'Upgrade Jetpack to enable WordAds.' ) + '\u00A0',
+						<ExternalLink href="/wp-admin/admin.php?page=jetpack#/plans" key="jetpack-link">
+							{ __( 'Click to upgrade' ) }
+						</ExternalLink>,
+					]
+				}
+				notificationLevel={ 'info' }
+			/>
+			<ActionCard
+				title={ __( 'Google AdSense' ) }
+				description={ __(
+					'A simple way to place adverts on your news site automatically based on where they best perform.'
+				) }
+				actionText={ google_adsense && google_adsense.enabled && __( 'Configure' ) }
+				toggle
+				toggleChecked={ google_adsense && google_adsense.enabled }
+				toggleOnChange={ value => toggleService( 'google_adsense', value ) }
+				handoff="google-site-kit"
+				editLink="admin.php?page=googlesitekit-module-adsense"
+			/>
+			<ActionCard
+				title={ __( 'Google Ad Manager' ) }
+				description={ __(
+					'An advanced ad inventory creation and management platform, allowing you to be specific about ad placements.'
+				) }
+				actionText={ google_ad_manager && google_ad_manager.enabled && __( 'Configure' ) }
+				toggle
+				toggleChecked={ google_ad_manager && google_ad_manager.enabled }
+				toggleOnChange={ value => toggleService( 'google_ad_manager', value ) }
+				titleLink={ google_ad_manager ? '#/google_ad_manager' : null }
+				onClick={ () => google_ad_manager && history.push( '/google_ad_manager' ) }
+			/>
+		</>
+	);
+};
 
 export default withWizardScreen( Services );
