30887205515c0f08c7634a908ac1e52226346d30
Thomas Guillot <info@thomasguillot.com>
2021-06-08 16:58:30 +0100

    feat: campaign analytics update design

--- assets/wizards/popups/views/analytics/Chart.js
+++ assets/wizards/popups/views/analytics/Chart.js
@@ -16,15 +16,16 @@ import {
 const Chart = ( { data } ) => (
 	<div className="newspack-campaigns-wizard-analytics__chart">
 		<ResponsiveContainer width={ '100%' } height={ 300 }>
-			<AreaChart data={ data } margin={ { top: 5, right: 20, bottom: 5, left: -15 } }>
+			<AreaChart data={ data } margin={ { top: 8, right: 16, bottom: 8, left: -32 } }>
 				<Area stackId="1" type="monotoneX" dataKey="value" stroke="#36f" fill="#3366ff1c" />
-				<CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+				<CartesianGrid stroke="#ddd" strokeDasharray="4 4" />
 				<XAxis
 					dataKey="date"
 					tickFormatter={ date => format( new Date( date ), 'MMM d' ) }
-					tickMargin={ 10 }
+					tickMargin={ 8 }
+					stroke="#1e1e1e"
 				/>
-				<YAxis allowDecimals={ false } tickFormatter={ humanNumber } />
+				<YAxis allowDecimals={ false } tickFormatter={ humanNumber } stroke="#1e1e1e" />
 				<Tooltip formatter={ value => humanNumber( value ) } />
 			</AreaChart>
 		</ResponsiveContainer>
