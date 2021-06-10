0230e3bce7f00cbab0d8d83da0241883b31f9360
Thomas Guillot <info@thomasguillot.com>
2021-06-10 16:20:45 +0100

    fix: lint

--- assets/wizards/site-design/views/main/index.js
+++ assets/wizards/site-design/views/main/index.js
@@ -93,7 +93,9 @@ const Main = ( {
 			<Card noBorder>
 				<TextControl
 					label={ label + ' - ' + __( 'Font provider import code or URL', 'newspack' ) }
-					placeholder={ 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap' }
+					placeholder={
+						'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap'
+					}
 					value={
 						isHeadings ? mods.custom_font_import_code : mods.custom_font_import_code_alternate
 					}
