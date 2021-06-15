ecd0179dbf4b17e7610ef4549e5dddfbec114e2b
Adam Boro <adam@adamboro.com>
2021-06-15 11:53:41 +0200

    feat: handle GAM ad units (#936)
    Experimental: NEWSPACK_GOOGLE_OAUTH_ENABLED, NEWSPACK_ADS_USE_GAM env variables have to be set, and the user added as the test user to (yet unpublished) Google Cloud Platform app. 
    
    see: https://github.com/Automattic/newspack-ads/pull/124

--- assets/wizards/advertising/views/index.js
+++ assets/wizards/advertising/views/index.js
@@ -2,4 +2,3 @@ export { default as Services } from './services';
 export { default as Placements } from './placements';
 export { default as AdUnits } from './ad-units';
 export { default as AdUnit } from './ad-unit';
-export { default as HeaderCode } from './header-code';
