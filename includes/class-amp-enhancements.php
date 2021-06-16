40d181a2cb20c8ed059b320bfa83df4affa8e880
Adam Boro <adam@adamboro.com>
2021-06-16 15:43:05 +0200

    feat(amp-plus): selectively allow JS on AMP pages (#990)
    Behind `NEWSPACK_AMP_PLUS_ENABLED` feature flag.
    
    Co-authored-by: Derrick Koo <derrick.koo@automattic.com>

--- includes/class-amp-enhancements.php
+++ includes/class-amp-enhancements.php
@@ -29,34 +29,57 @@ class AMP_Enhancements {
 				return $gtag_opt;
 			}
 		);
-		add_filter( 'amp_content_sanitizers', [ __CLASS__, 'amp_content_sanitizers' ] );
+
+		// Check if AMP Plugin is in Standard mode.
+		// In Transitional/Reader modes, AMP pages with kept invalid markup get redirected to the non-AMP version, which is not desired.
+		if ( function_exists( 'amp_is_canonical' ) && amp_is_canonical() ) {
+			add_filter( 'amp_validation_error_sanitized', [ __CLASS__, 'amp_validation_error_sanitized' ], 10, 2 );
+			add_filter( 'amp_validation_error_default_sanitized', [ __CLASS__, 'amp_validation_error_sanitized' ], 10, 2 );
+		}
 	}
 
 	/**
 	 * AMP plus mode.
 	 *
-	 * @param  string $context The context for which AMP plus should be assessed.
 	 * @return bool Should AMP plus be applied.
 	 */
-	public static function should_use_amp_plus( $context = null ) {
+	public static function should_use_amp_plus() {
 		$should = false;
-		if ( isset( $_GET['ampplus'] ) && defined( 'NEWSPACK_AMP_PLUS_CONFIG' ) && is_array( NEWSPACK_AMP_PLUS_CONFIG ) && in_array( $context, NEWSPACK_AMP_PLUS_CONFIG ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		if ( self::is_amp_plus_configured() ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
 			$should = true;
 		}
-		return apply_filters( 'should_use_amp_plus', $should, $context );
+		// Check if AMP Plugin is in Standard mode.
+		// In Transitional/Reader modes, AMP pages with kept invalid markup get redirected to the non-AMP version, which is not desired.
+		if ( function_exists( 'amp_is_canonical' ) && false === amp_is_canonical() ) {
+			$should = false;
+		}
+		return apply_filters( 'should_use_amp_plus', $should );
 	}
 
 	/**
-	 * Allow certain scripts to be included in AMP pages.
+	 * Is AMP plus mode configured.
 	 *
-	 * @param array $sanitizers The array of sanitizers, 'MyClassName' => [] // array of constructor params for class.
+	 * @return bool Is AMP plus mode configured.
 	 */
-	public static function amp_content_sanitizers( $sanitizers ) {
-		if ( self::should_use_amp_plus( 'gam' ) ) {
-			require_once NEWSPACK_ABSPATH . 'includes/amp-sanitizers/class-amp-sanitizer-gam.php';
-			$sanitizers['AMP_Sanitizer_GAM'] = [];
+	public static function is_amp_plus_configured() {
+		return defined( 'NEWSPACK_AMP_PLUS_ENABLED' ) && NEWSPACK_AMP_PLUS_ENABLED;
+	}
+
+	/**
+	 * Sanitize AMP Plugin errors to reject some errors.
+	 *
+	 * @param bool|null $is_sanitized If null, the error will be handled. If false, rejected.
+	 * @param object    $error The AMP sanitisation error.
+	 */
+	public static function amp_validation_error_sanitized( $is_sanitized, $error ) {
+		if ( false === self::should_use_amp_plus() ) {
+			return $is_sanitized;
 		}
-		return $sanitizers;
+		// Explicitly allowed scripts - with a 'data-amp-plus-allowed' attribute.
+		if ( isset( $error, $error['node_attributes'], $error['node_attributes']['data-amp-plus-allowed'] ) ) {
+			return false;
+		}
+		return $is_sanitized;
 	}
 }
 AMP_Enhancements::init();
