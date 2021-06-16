bbf746deeb7a90365da29f536239b4f128078480
Adam Borowski <adam@adamboro.com>
2021-06-16 12:33:12 +0200

    feat: remove organic-profile-block from supported plugins
    Newspack Blocks is featuring an Author Profile block now.

--- includes/class-plugin-manager.php
+++ includes/class-plugin-manager.php
@@ -271,14 +271,6 @@ class Plugin_Manager {
 				'Description' => 'The Newspack theme.',
 				'Author'      => 'Newspack',
 			],
-			'organic-profile-block'         => [
-				'Name'        => 'Organic Profile Block',
-				'Description' => "The Profile Block is created for the Gutenberg content editor. It displays a profile section with an image, name, subtitle, bio and personal social media links. It's perfect for author biographies, personal profiles, or staff pages.",
-				'Author'      => 'Organic Themes',
-				'PluginURI'   => 'https://organicthemes.com/',
-				'AuthorURI'   => 'https://organicthemes.com/',
-				'Download'    => 'wporg',
-			],
 			'wp-parsely'                    => [
 				'Name'        => 'Parse.ly',
 				'Description' => 'This plugin makes it a snap to add Parse.ly tracking code to your WordPress blog.',
