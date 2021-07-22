3a13d73bc270273ffa497197d0604d7b833e24e2
Adam Borowski <adam@adamboro.com>
2021-07-22 14:03:11 +0200

    feat(components): add DateRangePicker component

---
+++ assets/components/src/date-range-picker/index.js
@@ -0,0 +1,30 @@
+/**
+ * External dependencies.
+ */
+import { default as ReactDateRangePicker } from 'react-daterange-picker';
+import origMoment from 'moment';
+import { extendMoment } from 'moment-range';
+import 'react-daterange-picker/dist/css/react-calendar.css';
+
+/**
+ * Internal dependencies.
+ */
+import './style.scss';
+
+const moment = extendMoment( origMoment );
+
+const DateRangePicker = ( { start, end, onChange } ) => {
+	return (
+		<ReactDateRangePicker
+			value={ moment.range( moment( start ), moment( end ) ) }
+			onSelect={ range => {
+				onChange( {
+					start: range.start._d,
+					end: range.end._d,
+				} );
+			} }
+		/>
+	);
+};
+
+export default DateRangePicker;
