5cc4ad26fae640e9a37228ac9174dbf58d2f97f5
Noah Allen <noahtallen@gmail.com>
2021-09-22 16:48:10 -0700

    Update jest

--- assets/setupTests.js
+++ assets/setupTests.js
@@ -3,3 +3,18 @@ import '@testing-library/jest-dom';
 window.newspack_aux_data = {
 	is_debug_mode: false,
 };
+
+// matchMedia does not exist in JSDOM, see https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty( window, 'matchMedia', {
+	writable: true,
+	value: jest.fn().mockImplementation( query => ( {
+		matches: false,
+		media: query,
+		onchange: null,
+		addListener: jest.fn(), // deprecated
+		removeListener: jest.fn(), // deprecated
+		addEventListener: jest.fn(),
+		removeEventListener: jest.fn(),
+		dispatchEvent: jest.fn(),
+	} ) ),
+} );
