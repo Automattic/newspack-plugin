85836798df08a682093c801345ce85c4dce6c14e
Miguel Peixe <miguel@peixe.co>
2021-11-01 12:23:05 -0300

    fix: proper integration with select control

--- assets/wizards/advertising/views/settings/settings-section/index.js
+++ assets/wizards/advertising/views/settings/settings-section/index.js
@@ -20,14 +20,42 @@ import {
 	SelectControl,
 } from '../../../../../components/src';
 
-const SettingsSection = props => {
-	const section = props.settings.find( setting => ! setting.key || setting.key === 'active' );
+const SettingsSection = ( { settings, onChange, onUpdate } ) => {
+	const section = settings.find( setting => ! setting.key || setting.key === 'active' );
 	if ( ! section ) {
 		return null;
 	}
-	const activation = props.settings.find( setting => setting.key === 'active' );
-	const settings = props.settings.filter( setting => setting.key && setting.key !== 'active' );
+	const activation = settings.find( setting => setting.key === 'active' );
+	const fields = settings.filter( setting => setting.key && setting.key !== 'active' );
+	const getControlComponent = setting => {
+		if ( Array.isArray( setting.options ) && setting.options.length ) {
+			return SelectControl;
+		}
+		switch ( setting.type ) {
+			case 'checkbox':
+			case 'boolean':
+				return CheckboxControl;
+			default:
+				return TextControl;
+		}
+	};
+	const getControlType = setting => {
+		switch ( setting.type ) {
+			case 'int':
+			case 'integer':
+			case 'float':
+			case 'number':
+				return 'number';
+			case 'string':
+			case 'text':
+				return 'text';
+			default:
+				return null;
+		}
+	};
 	const getControlProps = setting => ( {
+		name: `${ setting.section }_${ setting.key }`,
+		type: getControlType( setting ),
 		label: setting.description,
 		help: setting.help || null,
 		options:
@@ -38,7 +66,7 @@ const SettingsSection = props => {
 		value: setting.value,
 		checked: setting.type === 'boolean' ? !! setting.value : null,
 		onChange: value => {
-			props.onChange( setting.key, value );
+			onChange( setting.key, value );
 		},
 	} );
 	return (
@@ -48,44 +76,21 @@ const SettingsSection = props => {
 			description={ section.help }
 			toggleChecked={ activation ? activation.value : null }
 			hasGreyHeader={ !! activation }
-			toggleOnChange={ value => props.onUpdate( { [ activation.key ]: value } ) }
+			toggleOnChange={ value => onUpdate( { [ activation.key ]: value } ) }
 		>
 			{ ( ! activation || activation.value ) && (
 				<Fragment>
-					<Grid columns={ settings.length === 3 ? 3 : 2 } gutter={ 32 }>
-						{ settings.map( setting => {
-							switch ( setting.type ) {
-								case 'int':
-								case 'float':
-									return (
-										<TextControl
-											key={ setting.key }
-											type="number"
-											{ ...getControlProps( setting ) }
-										/>
-									);
-								case 'string':
-								case 'text':
-									return (
-										<TextControl
-											key={ setting.key }
-											type="text"
-											{ ...getControlProps( setting ) }
-										/>
-									);
-								case 'select':
-									return <SelectControl key={ setting.key } { ...getControlProps( setting ) } />;
-								case 'boolean':
-									return <CheckboxControl key={ setting.key } { ...getControlProps( setting ) } />;
-							}
-							return null;
+					<Grid columns={ fields.length === 3 ? 3 : 2 } gutter={ 32 }>
+						{ fields.map( setting => {
+							const Control = getControlComponent( setting ); // eslint-disable-line @wordpress/no-unused-vars-before-return, no-unused-vars
+							return <Control key={ setting.key } { ...getControlProps( setting ) } />;
 						} ) }
 					</Grid>
 					<div className="newspack-buttons-card" style={ { margin: '32px 0 0' } }>
 						<Button
 							isPrimary
 							onClick={ () => {
-								props.onUpdate();
+								onUpdate();
 							} }
 						>
 							{ __( 'Save settings', 'newspack' ) }
