8c9c3f4b3516fed56305056b2799243f35c6063b
Adam Borowski <adam@adamboro.com>
2021-11-05 18:36:34 +0100

    refactor: load_common_assets method

--- includes/class-newspack.php
+++ includes/class-newspack.php
@@ -255,5 +255,28 @@ final class Newspack {
 	public static function is_debug_mode() {
 		return defined( 'WP_NEWSPACK_DEBUG' ) && WP_NEWSPACK_DEBUG;
 	}
+
+	/**
+	 * Load the common assets.
+	 */
+	public static function load_common_assets() {
+		wp_register_script(
+			'newspack_commons',
+			self::plugin_url() . '/dist/commons.js',
+			[],
+			filemtime( dirname( NEWSPACK_PLUGIN_FILE ) . '/dist/commons.js' ),
+			true
+		);
+		wp_enqueue_script( 'newspack_commons' );
+
+		wp_register_style(
+			'newspack-commons',
+			self::plugin_url() . '/dist/commons.css',
+			[ 'wp-components' ],
+			filemtime( dirname( NEWSPACK_PLUGIN_FILE ) . '/dist/commons.css' )
+		);
+		wp_style_add_data( 'newspack-commons', 'rtl', 'replace' );
+		wp_enqueue_style( 'newspack-commons' );
+	}
 }
 Newspack::instance();
