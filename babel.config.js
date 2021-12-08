3d819a1efa72341c7745146431416cb60c448f9b
Adam Borowski <adam@adamboro.com>
2021-12-08 10:22:27 +0100

    chore: use test script of newspack-scripts

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = ( { env } ) => {
 	const isTest = env( 'test' );
 	return {
-		plugins: [ ...( isTest ? [] : [ 'babel-plugin-jsx-remove-data-test-id' ] ) ],
 		presets: [
 			...( isTest ? [ '@babel/preset-env' ] : [] ),
 			'@automattic/calypso-build/babel/default',
