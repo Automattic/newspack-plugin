39dd2aa06e1f3da7686d9159b97f1edb8247d54a
Derrick Koo <dkoo@users.noreply.github.com>
2022-01-12 09:13:56 -0700

    refactor(salesforce): simplify Salesforce API requests (#1429)
    * refactor(salesforce): simplify Salesforce API requests
    
    * refactor: move Salesforce webhooks handler methods to Salesforce class
    
    * docs: correct API type in docblock comment
    
    * fix: get_salesforce_settings method must be public, not private
    
    * refactor: use WC_Order class to parse order details
    
    * fix: rename unused $data variable
    
    * chore: standardize format of endpoint strings to include / prefix
    
    * refactor: change salesforce API prefixes; move API handlers to class
    
    * feat: add afterFetch/afterUpdate callbacks; allow password input
    
    * refactor: use PluginSettings component to handle UI
    
    * chore: move copy to clipboard link
    
    * fix: use \rest_ensure_response() to return WP_Errors for API requests
    
    * fix: handle legacy API namespace for webhook handler
    
    * fix: salesforce settings should not be public

--- assets/wizards/readerRevenue/views/salesforce/index.js
+++ assets/wizards/readerRevenue/views/salesforce/index.js
@@ -7,7 +7,7 @@ import { parse } from 'qs';
  * WordPress dependencies.
  */
 import { __ } from '@wordpress/i18n';
-import { ExternalLink } from '@wordpress/components';
+import { ClipboardButton, ExternalLink } from '@wordpress/components';
 import { useState, useEffect } from '@wordpress/element';
 import { useDispatch } from '@wordpress/data';
 import { addQueryArgs } from '@wordpress/url';
@@ -15,19 +15,18 @@ import { addQueryArgs } from '@wordpress/url';
 /**
  * Internal dependencies.
  */
-import { Card, Grid, Notice, TextControl, Wizard, Button } from '../../../../components/src';
+import { PluginSettings, Notice, Wizard } from '../../../../components/src';
 import { READER_REVENUE_WIZARD_SLUG } from '../../constants';
 
 const Salesforce = () => {
+	const { salesforce_redirect_url: redirectUrl } = window?.newspack_reader_revenue || {};
+	const [ hasCopied, setHasCopied ] = useState( false );
 	const { salesforce_settings: salesforceData = {} } = Wizard.useWizardData( {} );
+	const [ isConnected, setIsConnected ] = useState( salesforceData.refresh_token );
 	const [ error, setError ] = useState( null );
 
-	const isConnected = !! salesforceData.refresh_token;
-
-	const { updateWizardSettings, saveWizardSettings, wizardApiFetch } = useDispatch(
-		Wizard.STORE_NAMESPACE
-	);
-	const saveSettings = value =>
+	const { saveWizardSettings, wizardApiFetch } = useDispatch( Wizard.STORE_NAMESPACE );
+	const saveAllSettings = value =>
 		saveWizardSettings( {
 			slug: READER_REVENUE_WIZARD_SLUG,
 			section: 'salesforce',
@@ -46,22 +45,22 @@ const Salesforce = () => {
 	 * @param {string} authorizationCode Auth code fetched from Salesforce.
 	 * @return {void}
 	 */
-	const getTokens = async ( authorizationCode, redirectURI ) => {
+	const getTokens = async authorizationCode => {
 		try {
 			// Get the tokens.
 			const response = await wizardApiFetch( {
-				path: '/newspack/v1/wizard/salesforce/tokens',
+				path: '/newspack/salesforce/v1/tokens',
 				method: 'POST',
 				data: {
 					code: authorizationCode,
-					redirect_uri: redirectURI,
+					redirect_uri: redirectUrl,
 				},
 			} );
 
 			const { access_token, client_id, client_secret, instance_url, refresh_token } = response;
 
 			if ( access_token && refresh_token ) {
-				saveSettings( {
+				saveAllSettings( {
 					access_token,
 					client_id,
 					client_secret,
@@ -69,6 +68,7 @@ const Salesforce = () => {
 					refresh_token,
 				} );
 			}
+			setIsConnected( true );
 		} catch ( e ) {
 			setError(
 				__(
@@ -86,7 +86,7 @@ const Salesforce = () => {
 	 */
 	const checkConnectionStatus = async () => {
 		const response = await wizardApiFetch( {
-			path: '/newspack/v1/wizard/salesforce/connection-status',
+			path: '/newspack/salesforce/v1/connection-status',
 			method: 'POST',
 			isQuietFetch: true,
 		} );
@@ -99,16 +99,9 @@ const Salesforce = () => {
 		const query = parse( window.location.search );
 		const authorizationCode = query.code;
 		if ( authorizationCode ) {
-			const redirectURI =
-				window.location.origin +
-				window.location.pathname +
-				'?page=' +
-				query[ '?page' ] +
-				window.location.hash;
-
 			// Remove `code` param from URL without adding history.
-			window.history.replaceState( {}, '', redirectURI );
-			getTokens( authorizationCode, redirectURI );
+			window.history.replaceState( {}, '', redirectUrl );
+			getTokens( authorizationCode, redirectUrl );
 		}
 	}, [] );
 
@@ -120,144 +113,75 @@ const Salesforce = () => {
 		}
 	}, [ isConnected ] );
 
-	const onButtonClick = async () => {
-		// If Salesforce is already connected, button should reset settings.
-		if ( isConnected ) {
-			saveSettings( {
-				client_id: '',
-				client_secret: '',
-				access_token: '',
-				refresh_token: '',
-				instance_url: '',
-			} );
-			return;
-		}
-
-		// Otherwise, attempt to establish a connection with Salesforce.
-		const { client_id, client_secret } = salesforceData;
-
-		if ( client_id && client_secret ) {
-			const loginUrl = addQueryArgs( 'https://login.salesforce.com/services/oauth2/authorize', {
-				response_type: 'code',
-				client_id: encodeURIComponent( client_id ),
-				client_secret: encodeURIComponent( client_secret ),
-				redirect_uri: encodeURI( window.location.href ),
-			} );
-
-			saveSettings( {
-				client_id,
-				client_secret,
-			} );
-
-			// Validate credentials before redirecting.
-			const valid = await wizardApiFetch( {
-				path: '/newspack/v1/wizard/salesforce/validate',
-				method: 'POST',
-				data: {
-					client_id,
-					client_secret,
-					redirect_uri: window.location.href,
-				},
-				isQuietFetch: true,
-			} );
-
-			if ( valid ) {
-				return window.location.assign( loginUrl );
-			}
-			setError( __( 'The credentials are invalid.', 'newspack' ) );
-		}
-	};
-
-	const changeHandler = key => value =>
-		updateWizardSettings( {
-			slug: READER_REVENUE_WIZARD_SLUG,
-			path: [ 'salesforce_settings', key ],
-			value,
-		} );
-
 	return (
 		<>
-			<Grid>
-				<Card noBorder>
-					{ error && <Notice noticeText={ error } isWarning /> }
-
-					{ isConnected && ! error && (
-						<Notice
-							noticeText={ __( 'Your site is connected to Salesforce.', 'newspack' ) }
-							isSuccess
-						/>
-					) }
-
-					{ ! isConnected && ! error && (
-						<>
-							<p>
-								{ __(
-									'To connect with Salesforce, create or choose a Connected App for this site in your Salesforce dashboard. Make sure to paste the the full URL for this page into the “Callback URL” field in the Connected App’s settings. ',
-									'newspack'
-								) }
-								<ExternalLink href="https://help.salesforce.com/articleView?id=connected_app_create.htm">
-									{ __( 'Learn how to create a Connected App', 'newspack' ) }
-								</ExternalLink>
-							</p>
-
-							<p>
-								{ __(
-									'Enter your Consumer Key and Secret, then click “Connect” to authorize access to your Salesforce account.',
-									'newspack'
-								) }
-							</p>
-						</>
-					) }
-
-					{ isConnected && (
-						<p>
-							{ __(
-								'To reconnect your site in case of issues, or to connect to a different Salesforce account, click “Reset". You will need to re-enter your Consumer Key and Secret before you can re-connect to Salesforce.',
-								'newspack'
-							) }
-						</p>
-					) }
-				</Card>
-
-				<Card noBorder>
-					{ error && (
-						<Notice
-							noticeText={ __(
-								'We couldn’t connect to Salesforce. Please verify that you entered the correct Consumer Key and Secret and try again. If you just created your Connected App or edited the Callback URL settings, it may take up to an hour before we can establish a connection.',
-								'newspack'
-							) }
-							isError
-						/>
-					) }
-					<TextControl
-						disabled={ isConnected }
-						label={
-							( isConnected ? __( 'Your', 'newspack' ) : __( 'Enter your', 'newspack' ) ) +
-							__( ' Salesforce Consumer Key', 'newspack' )
+			<PluginSettings
+				afterUpdate={ settings => {
+					let clientId, clientSecret;
+					( settings?.salesforce || [] ).forEach( setting => {
+						if ( 'newspack_salesforce_client_id' === setting.key ) {
+							clientId = setting.value;
+						} else if ( 'newspack_salesforce_client_secret' === setting.key ) {
+							clientSecret = setting.value;
 						}
-						value={ salesforceData.client_id }
-						onChange={ changeHandler( 'client_id' ) }
-					/>
-					<TextControl
-						disabled={ isConnected }
-						label={
-							( isConnected ? __( 'Your', 'newspack' ) : __( 'Enter your', 'newspack' ) ) +
-							__( ' Salesforce Consumer Secret', 'newspack' )
-						}
-						value={ salesforceData.client_secret }
-						onChange={ changeHandler( 'client_secret' ) }
-					/>
-				</Card>
-			</Grid>
-			<div className="newspack-buttons-card">
-				<Button
-					isPrimary
-					disabled={ ! salesforceData.client_id || ! salesforceData.client_secret }
-					onClick={ onButtonClick }
-				>
-					{ isConnected ? __( 'Reset', 'newspack' ) : __( 'Connect', 'newspack' ) }
-				</Button>
-			</div>
+					} );
+
+					if ( clientId && clientSecret && redirectUrl ) {
+						const loginUrl = addQueryArgs(
+							'https://login.salesforce.com/services/oauth2/authorize',
+							{
+								response_type: 'code',
+								client_id: encodeURIComponent( clientId ),
+								client_secret: encodeURIComponent( clientSecret ),
+								redirect_uri: encodeURI( redirectUrl ),
+							}
+						);
+
+						window.location.assign( loginUrl );
+					} else {
+						setIsConnected( false );
+					}
+				} }
+				pluginSlug="newspack/salesforce"
+				title={ __( 'Salesforce Settings', 'newspack' ) }
+				description={ () => (
+					<>
+						{ error && <Notice noticeText={ error } isWarning /> }
+
+						{ isConnected && ! error && (
+							<Notice
+								noticeText={ __( 'Your site is connected to Salesforce.', 'newspack' ) }
+								isSuccess
+							/>
+						) }
+
+						{ __(
+							'Establish a connection to sync WooCommerce order data to Salesforce. To connect with Salesforce, create or choose a Connected App for this site in your Salesforce dashboard. Make sure to paste the full URL for this page (',
+							'newspack'
+						) }
+
+						<ClipboardButton
+							className="newspack-button is-link"
+							text={ redirectUrl }
+							onCopy={ () => setHasCopied( true ) }
+							onFinishCopy={ () => setHasCopied( false ) }
+						>
+							{ hasCopied
+								? __( 'copied to clipboard!', 'newspack' )
+								: __( 'copy to clipboard', 'newspack' ) }
+						</ClipboardButton>
+
+						{ __(
+							') into the “Callback URL” field in the Connected App’s settings. ',
+							'newspack'
+						) }
+
+						<ExternalLink href="https://help.salesforce.com/articleView?id=connected_app_create.htm">
+							{ __( 'Learn how to create a Connected App', 'newspack' ) }
+						</ExternalLink>
+					</>
+				) }
+			/>
 		</>
 	);
 };
