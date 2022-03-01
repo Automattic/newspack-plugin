1a8650002cd7fbe155fdcbf28af0c1c88a085316
Derrick Koo <derrick.koo@automattic.com>
2022-03-01 11:55:56 -0700

    Revert "Merge branch 'master' into feat/restrict-media-library-users"
    This reverts commit 76d7eefbc568edcbb61ace0d1f921542d0638fbc, reversing
    changes made to 19b0bbf2b125973782a82ac82f9380b585e8d088.

--- assets/wizards/handoff-banner/index.js
+++ assets/wizards/handoff-banner/index.js
@@ -7,7 +7,7 @@ import '../../shared/js/public-path';
 /**
  * WordPress dependencies.
  */
-import { createElement, render, useState } from '@wordpress/element';
+import { Component, createElement, render } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -16,28 +16,42 @@ import { __ } from '@wordpress/i18n';
 import { Button } from '../../components/src';
 import './style.scss';
 
-const HandoffBanner = ( {
-	bodyText = __( 'Return to Newspack after completing configuration', 'newspack' ),
-	primaryButtonText = __( 'Back to Newspack', 'newspack' ),
-	dismissButtonText = __( 'Dismiss', 'newspack' ),
-	primaryButtonURL = '/wp-admin/admin.php?page=newspack',
-} ) => {
-	const [ visibility, setVisibility ] = useState( true );
-	return (
-		visibility && (
-			<div className="newspack-handoff-banner">
-				<div className="newspack-handoff-banner__text">{ bodyText }</div>
-				<div className="newspack-handoff-banner__buttons">
-					<Button isPrimary isSmall onClick={ () => setVisibility( false ) }>
-						{ dismissButtonText }
-					</Button>
-					<Button isSecondary isSmall href={ primaryButtonURL }>
-						{ primaryButtonText }
-					</Button>
+class HandoffBanner extends Component {
+	constructor() {
+		super( ...arguments );
+		this.state = {
+			visibility: true,
+		};
+	}
+	/**
+	 * Render.
+	 */
+	render() {
+		const { bodyText, primaryButtonText, dismissButtonText, primaryButtonURL } = this.props;
+		const { visibility } = this.state;
+		return (
+			visibility && (
+				<div className="newspack-handoff-banner">
+					<div className="newspack-handoff-banner__text">{ bodyText }</div>
+					<div className="newspack-handoff-banner__buttons">
+						<Button isPrimary isSmall onClick={ () => this.setState( { visibility: false } ) }>
+							{ dismissButtonText }
+						</Button>
+						<Button isSecondary isSmall href={ primaryButtonURL }>
+							{ primaryButtonText }
+						</Button>
+					</div>
 				</div>
-			</div>
-		)
-	);
+			)
+		);
+	}
+}
+
+HandoffBanner.defaultProps = {
+	primaryButtonText: __( 'Back to Newspack', 'newspack' ),
+	dismissButtonText: __( 'Dismiss', 'newspack' ),
+	primaryButtonURL: '/wp-admin/admin.php?page=newspack',
+	bodyText: __( 'Return to Newspack after completing configuration', 'newspack' ),
 };
 
 const el = document.getElementById( 'newspack-handoff-banner' );
