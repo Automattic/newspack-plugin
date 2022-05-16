d28c8fda7059873a9c42c6baa42781090c9012aa
Miguel Peixe <miguel@peixe.co>
2022-05-15 22:28:47 -0300

    fix: typo

--- assets/blocks/index.js
+++ assets/blocks/index.js
@@ -5,6 +5,6 @@
  */
 import registerReaderRegistrationBlock from './reader-registration';
 
-if ( newspack_blocks?.enabled ) {
+if ( newspack_blocks?.reader_registration_enabled ) {
 	registerReaderRegistrationBlock();
 }
