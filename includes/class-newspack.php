a42b40096ef4946fef16a99c9bcc5f989313be82
Adam Borowski <adam@adamboro.com>
2022-06-01 16:31:42 +0200

    feat: add settings wizard backend

--- includes/class-newspack.php
+++ includes/class-newspack.php
@@ -97,6 +97,7 @@ final class Newspack {
 		include_once NEWSPACK_ABSPATH . 'includes/wizards/class-components-demo.php';
 
 		/* Unified Wizards */
+		include_once NEWSPACK_ABSPATH . 'includes/wizards/class-settings.php';
 		include_once NEWSPACK_ABSPATH . 'includes/wizards/class-advertising-wizard.php';
 		include_once NEWSPACK_ABSPATH . 'includes/wizards/class-analytics-wizard.php';
 		include_once NEWSPACK_ABSPATH . 'includes/wizards/class-engagement-wizard.php';
