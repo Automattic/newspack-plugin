a1ec5e52b11b0d0e706e35615851b0021a17445b
Miguel Peixe <miguel.peixe@automattic.com>
2022-07-27 15:23:59 -0300

    feat: fields

--- assets/wizards/engagement/views/reader-activation/index.js
+++ assets/wizards/engagement/views/reader-activation/index.js
@@ -2,25 +2,71 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { ToggleControl } from '@wordpress/components';
+import { CheckboxControl } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
-import { SectionHeader, withWizardScreen } from '../../../../components/src';
+import {
+	Grid,
+	Card,
+	Button,
+	SectionHeader,
+	TextControl,
+	withWizardScreen,
+} from '../../../../components/src';
 
 export default withWizardScreen( () => (
 	<>
-		<SectionHeader title={ __( 'Reader Activation', 'newspack' ) } />
-		<p>
-			Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse luctus auctor justo,
-			non malesuada purus convallis id. Quisque ut justo ultricies, convallis ligula non, ornare
-			est. Suspendisse ante dui, mollis quis neque sit amet, rhoncus luctus quam.
-		</p>
-		<ToggleControl
-			label={ __( 'Enable Reader Activation', 'newspack' ) }
-			checked={ true }
-			onChange={ () => {} }
-		/>
+		<Card noBorder>
+			<SectionHeader
+				title={ __( 'Reader Activation', 'newspack' ) }
+				description={ __( 'Configure a set of features for reader activation.', 'newspack' ) }
+			/>
+			<CheckboxControl
+				label={ __( 'Enable Reader Activation', 'newspack' ) }
+				help={ __( 'Whether to enable reader activation features for your site.', 'newspack' ) }
+				checked={ true }
+				onChange={ () => {} }
+			/>
+			<hr />
+			<CheckboxControl
+				label={ __( 'Enable Sign In/Account link', 'newspack' ) }
+				help={ __(
+					'Display an always-visible sign in/account menu link in the header. This link triggers a modal for signing in or registration.',
+					'newspack'
+				) }
+				checked={ true }
+				onChange={ () => {} }
+			/>
+			<TextControl
+				label={ __( 'Newsletter subscription text on registration', 'newspack' ) }
+				help={ __(
+					'The text to display while subscribing to newsletters on the registration modal.',
+					'newspack'
+				) }
+				value={ 'Subscribe to our newsletters.' }
+				onChange={ () => {} }
+			/>
+			<Grid columns={ 2 } gutter={ 16 }>
+				<TextControl
+					label={ __( 'Terms & Conditions Text', 'newspack' ) }
+					help={ __( 'Terms and conditions text to display on registration.', 'newspack' ) }
+					value={ 'By signing up, you agree to our Terms and Conditions.' }
+					onChange={ () => {} }
+				/>
+				<TextControl
+					label={ __( 'Terms & Conditions URL', 'newspack' ) }
+					help={ __( 'URL to the page containing the terms and conditions.', 'newspack' ) }
+					value={ '' }
+					onChange={ () => {} }
+				/>
+			</Grid>
+		</Card>
+		<div className="newspack-buttons-card">
+			<Button isPrimary onClick={ () => {} } disabled={ false }>
+				{ __( 'Save Changes', 'newspack' ) }
+			</Button>
+		</div>
 	</>
 ) );
