87b9be9f8f26c61bc9e793318e0870b9fb5d309c
Miguel Peixe <miguel.peixe@automattic.com>
2022-08-03 14:54:48 -0300

    fix(registration-block): render on preview (#1844)
    Co-authored-by: Derrick Koo <dkoo@users.noreply.github.com>

--- assets/blocks/reader-registration/index.php
+++ assets/blocks/reader-registration/index.php
@@ -113,8 +113,12 @@ function render_block( $attrs, $content ) {
 
 	// phpcs:disable WordPress.Security.NonceVerification.Recommended
 	if (
-		\is_user_logged_in() ||
-		( isset( $_GET['newspack_reader'] ) && absint( $_GET['newspack_reader'] ) )
+		! \is_preview() &&
+		( ! method_exists( '\Newspack_Popups', 'is_preview_request' ) || ! \Newspack_Popups::is_preview_request() ) &&
+		(
+			\is_user_logged_in() ||
+			( isset( $_GET['newspack_reader'] ) && absint( $_GET['newspack_reader'] ) )
+		)
 	) {
 		$registered = true;
 		$message    = $success_message;
