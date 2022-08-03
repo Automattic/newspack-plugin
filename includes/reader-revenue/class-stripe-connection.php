722724cc49b3aac35b81a3fc0da2f62a317c3cd1
Adam Borowski <adam@adamboro.com>
2022-08-03 15:36:55 +0200

    fix(reader-activation): add metadata to reader registered on donation

--- includes/reader-revenue/class-stripe-connection.php
+++ includes/reader-revenue/class-stripe-connection.php
@@ -804,7 +804,7 @@ class Stripe_Connection {
 			$payment_method_id = $config['payment_method_id'];
 
 			if ( ! isset( $client_metadata['userId'] ) && Reader_Activation::is_enabled() ) {
-				$user_id = Reader_Activation::register_reader( $email_address, $full_name, true, false );
+				$user_id = Reader_Activation::register_reader( $email_address, $full_name, true, $client_metadata );
 				if ( ! \is_wp_error( $user_id ) && false !== $user_id ) {
 					$client_metadata['userId'] = $user_id;
 				}
