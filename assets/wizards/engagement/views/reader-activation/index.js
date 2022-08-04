a9e8c9cb7ab5a13580d5cd1d678b7a7e5d732901
Thomas Guillot <thomasguillot@users.noreply.github.com>
2022-08-04 12:52:48 +0100

    feat: reorganise reader activation wizard with ActionCard

--- assets/wizards/engagement/views/reader-activation/index.js
+++ assets/wizards/engagement/views/reader-activation/index.js
@@ -10,11 +10,10 @@ import { useEffect, useState } from '@wordpress/element';
  * Internal dependencies
  */
 import {
-	Notice,
-	Grid,
-	Card,
+	ActionCard,
 	Button,
-	SectionHeader,
+	Grid,
+	Notice,
 	TextControl,
 	withWizardScreen,
 } from '../../../../components/src';
@@ -68,66 +67,70 @@ export default withWizardScreen( () => {
 					isError
 				/>
 			) }
-			<Card noBorder>
-				<SectionHeader
-					title={ __( 'Reader Activation', 'newspack' ) }
-					description={ __( 'Configure a set of features for reader activation.', 'newspack' ) }
-				/>
-				<CheckboxControl
-					label={ __( 'Enable Reader Activation', 'newspack' ) }
-					help={ __( 'Whether to enable reader activation features for your site.', 'newspack' ) }
-					checked={ !! config.enabled }
-					onChange={ value => updateConfig( 'enabled', value ) }
-				/>
-				<hr />
-				<CheckboxControl
-					label={ __( 'Enable Sign In/Account link', 'newspack' ) }
-					help={ __(
-						'Display an account link in the site header. It will allow readers to register and access their account.',
-						'newspack'
-					) }
-					checked={ !! config.enabled_account_link }
-					onChange={ value => updateConfig( 'enabled_account_link', value ) }
-				/>
-				<TextControl
-					label={ __( 'Newsletter subscription text on registration', 'newspack' ) }
-					help={ __(
-						'The text to display while subscribing to newsletters on the registration modal.',
-						'newspack'
-					) }
-					value={ config.newsletters_label }
-					onChange={ value => updateConfig( 'newsletters_label', value ) }
-				/>
-				<Grid columns={ 2 } gutter={ 16 }>
-					<TextControl
-						label={ __( 'Terms & Conditions Text', 'newspack' ) }
-						help={ __( 'Terms and conditions text to display on registration.', 'newspack' ) }
-						value={ config.terms_text }
-						onChange={ value => updateConfig( 'terms_text', value ) }
-					/>
-					<TextControl
-						label={ __( 'Terms & Conditions URL', 'newspack' ) }
-						help={ __( 'URL to the page containing the terms and conditions.', 'newspack' ) }
-						value={ config.terms_url }
-						onChange={ value => updateConfig( 'terms_url', value ) }
-					/>
-				</Grid>
-			</Card>
-			{ isActiveCampaign && (
-				<ActiveCampaign
-					value={ { masterList: config.active_campaign_master_list } }
-					onChange={ ( key, value ) => {
-						if ( key === 'masterList' ) {
-							updateConfig( 'active_campaign_master_list', value );
-						}
-					} }
-				/>
-			) }
-			<div className="newspack-buttons-card">
-				<Button isPrimary onClick={ saveConfig } disabled={ inFlight }>
-					{ __( 'Save Changes', 'newspack' ) }
-				</Button>
-			</div>
+			<ActionCard
+				isMedium
+				title={ __( 'Reader Activation', 'newspack' ) }
+				description={ __( 'Configure a set of features for reader activation.', 'newspack' ) }
+				toggleChecked={ !! config.enabled }
+				toggleOnChange={ value => updateConfig( 'enabled', value ) }
+				hasGreyHeader={ !! config.enabled }
+				disabled={ inFlight }
+				actionContent={
+					<Button variant="primary" disabled={ inFlight } onClick={ saveConfig }>
+						{ __( 'Save Settings', 'newspack' ) }
+					</Button>
+				}
+			>
+				{ !! config.enabled && (
+					<Grid columns={ 1 }>
+						<CheckboxControl
+							label={ __( 'Enable Sign In/Account link', 'newspack' ) }
+							help={ __(
+								'Display an account link in the site header. It will allow readers to register and access their account.',
+								'newspack'
+							) }
+							checked={ !! config.enabled_account_link }
+							onChange={ value => updateConfig( 'enabled_account_link', value ) }
+						/>
+						<TextControl
+							label={ __( 'Newsletter subscription text on registration', 'newspack' ) }
+							help={ __(
+								'The text to display while subscribing to newsletters on the registration modal.',
+								'newspack'
+							) }
+							value={ config.newsletters_label }
+							onChange={ value => updateConfig( 'newsletters_label', value ) }
+						/>
+						<Grid>
+							<TextControl
+								label={ __( 'Terms & Conditions Text', 'newspack' ) }
+								help={ __( 'Terms and conditions text to display on registration.', 'newspack' ) }
+								value={ config.terms_text }
+								onChange={ value => updateConfig( 'terms_text', value ) }
+							/>
+							<TextControl
+								label={ __( 'Terms & Conditions URL', 'newspack' ) }
+								help={ __( 'URL to the page containing the terms and conditions.', 'newspack' ) }
+								value={ config.terms_url }
+								onChange={ value => updateConfig( 'terms_url', value ) }
+							/>
+						</Grid>
+						{ isActiveCampaign && (
+							<>
+								<hr />
+								<ActiveCampaign
+									value={ { masterList: config.active_campaign_master_list } }
+									onChange={ ( key, value ) => {
+										if ( key === 'masterList' ) {
+											updateConfig( 'active_campaign_master_list', value );
+										}
+									} }
+								/>
+							</>
+						) }
+					</Grid>
+				) }
+			</ActionCard>
 		</>
 	);
 } );
