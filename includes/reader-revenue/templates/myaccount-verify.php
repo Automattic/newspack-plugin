723a5cd2373cc8f9a8b5827f3cc22d6c3d230b45
Derrick Koo <derrick.koo@automattic.com>
2022-08-10 15:44:07 -0600

    refactor: use add_query_arg to build verification URLs

--- includes/reader-revenue/templates/myaccount-verify.php
+++ includes/reader-revenue/templates/myaccount-verify.php
@@ -8,7 +8,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-do_action( 'woocommerce_before_edit_account_form' );
+\do_action( 'woocommerce_before_edit_account_form' );
 
 global $newspack_reset_password_arg;
 global $newspack_send_magic_link_arg;
@@ -28,10 +28,23 @@ if ( isset( $_GET['is_error'] ) ) { // phpcs:ignore WordPress.Security.NonceVeri
 if ( $message ) :
 	?>
 	<div class="newspack-wc-message <?php echo $is_error ? 'newspack-wc-message--error' : ''; ?>">
-		<p><?php echo esc_html( $message ); ?></p>
+		<p><?php echo \esc_html( $message ); ?></p>
 	</div>
 	<?php
 endif;
+
+$magic_link_args                                     = [];
+$magic_link_args[ $newspack_send_magic_link_arg ]    = wp_create_nonce( $newspack_send_magic_link_arg );
+$magic_link_url                                      = \add_query_arg(
+	$magic_link_args,
+	\home_url()
+);
+$reset_password_args                                 = [];
+$reset_password_args[ $newspack_reset_password_arg ] = wp_create_nonce( $newspack_reset_password_arg );
+$reset_password_url                                  = \add_query_arg(
+	$reset_password_args,
+	\home_url()
+);
 ?>
 
 <div class="newspack-verify-account-message">
@@ -39,13 +52,13 @@ endif;
 		<?php esc_html_e( 'You must verify your account before you can manage account settings. Verify with a link or by setting a password.', 'newspack' ); ?>
 	</p>
 	<p>
-		<a class="woocommerce-Button button" href="<?php echo '?' . esc_attr( $newspack_send_magic_link_arg ) . '=' . esc_attr( wp_create_nonce( $newspack_send_magic_link_arg ) ); ?>">
+		<a class="woocommerce-Button button" href="<?php echo esc_url( $magic_link_url ); ?>">
 			<?php esc_html_e( 'Send me a link', 'newspack' ); ?>
 		</a>
-		<a class="woocommerce-Button button" href="<?php echo '?' . esc_attr( $newspack_reset_password_arg ) . '=' . esc_attr( wp_create_nonce( $newspack_reset_password_arg ) ); ?>">
+		<a class="woocommerce-Button button" href="<?php echo esc_url( $reset_password_url ); ?>">
 			<?php esc_html_e( 'Set a new password', 'newspack' ); ?>
 		</a>
 	</p>
 </div>
 
-<?php do_action( 'woocommerce_after_edit_account_form' ); ?>
+<?php \do_action( 'woocommerce_after_edit_account_form' ); ?>
