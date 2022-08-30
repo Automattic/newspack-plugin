886cc87a97d186cc979d8d7acd26c59511faada8
Adam Borowski <adam@adamboro.com>
2022-08-30 15:55:58 +0200

    feat: update billing portal copy

--- includes/reader-revenue/templates/myaccount-billing.php
+++ includes/reader-revenue/templates/myaccount-billing.php
@@ -22,7 +22,7 @@ else :
 	?>
 
 	<a href="<?php echo esc_url( $stripe_billing_portal_url ); ?>">
-		<?php echo esc_html__( 'Update payment details, recurring payments, and view billing history', 'newspack' ); ?>
+		<?php echo esc_html__( 'Update payment method, cancel recurring payments, and view billing history', 'newspack' ); ?>
 	</a>
 
 	<?php
