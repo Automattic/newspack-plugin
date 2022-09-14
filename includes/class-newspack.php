43411ee46d4c4aa2b38857cc9793c46c91adb1b4
Adam Borowski <adam@adamboro.com>
2022-09-14 15:02:24 +0200

    feat(plugins): handle missing organic-profile-block plugin

--- includes/class-newspack.php
+++ includes/class-newspack.php
@@ -136,6 +136,7 @@ final class Newspack {
 		include_once NEWSPACK_ABSPATH . 'includes/plugins/google-site-kit/class-googlesitekit.php';
 		include_once NEWSPACK_ABSPATH . 'includes/plugins/class-newspack-newsletters.php';
 		include_once NEWSPACK_ABSPATH . 'includes/plugins/class-mailchimp-for-woocommerce.php';
+		include_once NEWSPACK_ABSPATH . 'includes/plugins/class-organic-profile-block.php';
 
 		include_once NEWSPACK_ABSPATH . 'includes/class-patches.php';
 
