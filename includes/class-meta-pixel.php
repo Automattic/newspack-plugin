50a48221daa864f6a4df0a8ef66fd08395101b2f
Leo Germani <leogermani@automattic.com>
2022-09-16 12:45:46 -0300

    feat: change pixel script

--- includes/class-meta-pixel.php
+++ includes/class-meta-pixel.php
@@ -154,7 +154,7 @@ class Meta_Pixel {
 	 * @return string
 	 */
 	public static function get_img_snippet() {
-		return '<img height="1" width="1" style="display: none;" src="https://www.facebook.com/tr?id=$PIXEL_ID$&ev=PageView&noscript=1&cd%5Bpage_title%5D=Homepage&cd%5Bpost_type%5D=page&cd%5Bpost_id%5D=125&cd%5Bplugin%5D=PixelYourSite&cd%5Buser_role%5D=guest&cd%5Bevent_url%5D=leogermani.jurassic.tube%2F" alt="">';
+		return '<img height="1" width="1" style="display: none;" src="https://www.facebook.com/tr?id=__PIXEL_ID__&ev=PageView&noscript=1&cd%5Bpage_title%5D=Homepage&cd%5Bpost_type%5D=page&cd%5Bpost_id%5D=125&cd%5Bplugin%5D=PixelYourSite&cd%5Buser_role%5D=guest&cd%5Bevent_url%5D=leogermani.jurassic.tube%2F" alt="">';
 	}
 
 	/**
@@ -164,11 +164,20 @@ class Meta_Pixel {
 	 */
 	public static function get_script_snippet() {
 		// phpcs:ignore WordPressVIPMinimum.Security.Mustache.OutputNotation
-		return '<script type="text/javascript" id="pys-js-extra">
-		/* <![CDATA[ */
-		var pysOptions = {"staticEvents":{"facebook":{"init_event":[{"delay":0,"type":"static","name":"PageView","pixelIds":["$PIXEL_ID$"],"eventID":"b69f94f7-5424-467e-a856-124844a132e9","params":{"page_title":"Homepage","post_type":"page","post_id":125,"plugin":"PixelYourSite","user_role":"guest","event_url":"leogermani.jurassic.tube\/"},"e_id":"init_event","ids":[],"hasTimeWindow":false,"timeWindow":0,"woo_order":"","edd_order":""}]}},"dynamicEvents":{"woo_add_to_cart_on_button_click":{"facebook":{"delay":0,"type":"dyn","name":"AddToCart","pixelIds":["$PIXEL_ID$"],"eventID":"78439eb7-a9fc-43cd-926e-670d06c4c8f2","params":{"page_title":"Homepage","post_type":"page","post_id":125,"plugin":"PixelYourSite","user_role":"guest","event_url":"leogermani.jurassic.tube\/"},"e_id":"woo_add_to_cart_on_button_click","ids":[],"hasTimeWindow":false,"timeWindow":0,"woo_order":"","edd_order":""}}},"triggerEvents":[],"triggerEventTypes":[],"facebook":{"pixelIds":["$PIXEL_ID$"],"advancedMatching":[],"removeMetadata":false,"contentParams":{"post_type":"page","post_id":125,"content_name":"Homepage"},"commentEventEnabled":true,"wooVariableAsSimple":false,"downloadEnabled":true,"formEventEnabled":true,"ajaxForServerEvent":true,"serverApiEnabled":false,"wooCRSendFromServer":false},"debug":"","siteUrl":"https:\/\/leogermani.jurassic.tube","ajaxUrl":"https:\/\/leogermani.jurassic.tube\/wp-admin\/admin-ajax.php","enable_remove_download_url_param":"1","cookie_duration":"7","last_visit_duration":"60","gdpr":{"ajax_enabled":false,"all_disabled_by_api":false,"facebook_disabled_by_api":false,"analytics_disabled_by_api":false,"google_ads_disabled_by_api":false,"pinterest_disabled_by_api":false,"bing_disabled_by_api":false,"facebook_prior_consent_enabled":true,"analytics_prior_consent_enabled":true,"google_ads_prior_consent_enabled":null,"pinterest_prior_consent_enabled":true,"bing_prior_consent_enabled":true,"cookiebot_integration_enabled":false,"cookiebot_facebook_consent_category":"marketing","cookiebot_analytics_consent_category":"statistics","cookiebot_google_ads_consent_category":null,"cookiebot_pinterest_consent_category":"marketing","cookiebot_bing_consent_category":"marketing","consent_magic_integration_enabled":false,"real_cookie_banner_integration_enabled":false,"cookie_notice_integration_enabled":false,"cookie_law_info_integration_enabled":false},"woo":{"enabled":true,"addToCartOnButtonEnabled":true,"addToCartOnButtonValueEnabled":true,"addToCartOnButtonValueOption":"price","singleProductId":null,"removeFromCartSelector":"form.woocommerce-cart-form .remove","addToCartCatchMethod":"add_cart_js"},"edd":{"enabled":false}};
-		/* ]]> */
-		</script>';
+		return "
+		<script>
+		!function(f,b,e,v,n,t,s)
+		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];
+		s.parentNode.insertBefore(t,s)}(window, document,'script',
+		'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '__PIXEL_ID__');
+		fbq('track', 'PageView');
+		</script>
+		";
 	}
 
 	/**
@@ -184,7 +193,7 @@ class Meta_Pixel {
 		if ( empty( $pixel_id ) ) {
 			return;
 		}
-		echo str_replace( '$PIXEL_ID$', intval( $pixel_id ), self::get_script_snippet() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo str_replace( '__PIXEL_ID__', intval( $pixel_id ), self::get_script_snippet() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 
 	/**
@@ -198,7 +207,7 @@ class Meta_Pixel {
 			return;
 		}
 		// If AMP plugin is enabled, it will convert the image into a <amp-pixel> tag.
-		echo '<noscript>' . str_replace( '$PIXEL_ID$', intval( $pixel_id ), self::get_img_snippet() ) . '</noscript>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo '<noscript>' . str_replace( '__PIXEL_ID__', intval( $pixel_id ), self::get_img_snippet() ) . '</noscript>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
 
