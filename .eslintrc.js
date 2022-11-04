8ee749b0495cfbb935a26fda8e3e74848f716c85
Adam Borowski <adam@adamboro.com>
2022-11-04 14:21:19 +0100

    chore: lint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,8 @@ module.exports = {
 		newspack_urls: 'readonly',
 		newspack_aux_data: 'readonly',
 	},
+	rules: {
+		'no-console': 'off',
+	},
 	ignorePatterns: [ 'dist/', 'node_modules/', 'assets/components/node_modules' ],
 };
