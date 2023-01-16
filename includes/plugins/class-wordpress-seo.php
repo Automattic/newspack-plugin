47bc43daa41f0da0ccdea4718eef2e2a00dd8e70
Adam Borowski <adam@adamboro.com>
2023-01-16 13:04:25 +0100

    fix: wordpress-seo & cap author archives indexing
    Closes #1506

--- includes/plugins/class-wordpress-seo.php
+++ includes/plugins/class-wordpress-seo.php
@@ -19,6 +19,7 @@ class WordPress_SEO {
 	public static function init() {
 		add_filter( 'wpseo_enhanced_slack_data', [ __CLASS__, 'use_cap_for_slack_preview' ] );
 		add_filter( 'wpseo_opengraph_url', [ __CLASS__, 'http_ogurls' ] );
+		add_filter( 'wpseo_robots_array', [ __CLASS__, 'allow_indexing_guest_author_archive' ], 10, 2 );
 	}
 
 	/**
@@ -47,6 +48,44 @@ class WordPress_SEO {
 			$og_url = str_replace( 'https:', 'http:', $og_url );
 		}
 		return $og_url;
+	}
+
+	/**
+	 * CoAuthors Plus and Yoast are incompatible where the author archives for guest authors are output as noindex.
+	 * This filter will determine if we're on an author archive and reset the robots.txt string properly.
+	 *
+	 * See https://github.com/Yoast/wordpress-seo/issues/9147.
+	 *
+	 * @param string                 $robots       The meta robots directives to be echoed.
+	 * @param Indexable_Presentation $presentation The presentation of an indexable.
+	 */
+	public static function allow_indexing_guest_author_archive( $robots, $presentation ) {
+		if ( ! is_author() ) {
+			return $robots;
+		}
+
+		if ( ! is_a( $presentation, '\Yoast\WP\SEO\Presentations\Indexable_Author_Archive_Presentation' ) ) {
+			return $robots;
+		}
+
+		$post_type = get_post_type( get_queried_object_id() );
+		if ( 'guest-author' !== $post_type ) {
+			return $robots;
+		}
+
+		/*
+		 * If this is a guest author archive and hasn't manually been set to noindex,
+		 * make sure the robots.txt string is set properly.
+		 */
+		if ( empty( $presentation->model->is_robots_noindex ) || 0 === intval( $presentation->model->is_robots_noindex ) ) {
+			if ( ! is_array( $robots ) ) {
+				$robots = [];
+			}
+			$robots['index']  = 'index';
+			$robots['follow'] = 'follow';
+		}
+
+		return $robots;
 	}
 }
 WordPress_SEO::init();
