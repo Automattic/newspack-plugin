9588152e4f4e8b31e9841a8cb424a9122fb9f02a
Miguel Peixe <miguel.peixe@automattic.com>
2023-01-18 14:55:38 -0300

    feat: use option to store merge field ids

--- includes/data-events/connectors/class-mailchimp.php
+++ includes/data-events/connectors/class-mailchimp.php
@@ -79,28 +79,41 @@ class Mailchimp {
 		];
 		if ( ! empty( $data ) ) {
 			$merge_fields    = [];
+			$fields_ids      = \get_option( 'newspack_data_mailchimp_fields', [] );
 			$existing_fields = Mailchimp_API::get( "lists/$audience_id/merge-fields?count=1000" );
 			foreach ( $existing_fields['merge_fields'] as $field ) {
-				if ( isset( $data[ $field['name'] ] ) ) {
-					$merge_fields[ $field['tag'] ] = $data[ $field['name'] ];
+				$field_name = '';
+				if ( isset( $fields_ids[ $field['merge_id'] ] ) ) {
+					// Match locally stored merge field ID.
+					$field_name = $fields_ids[ $field['merge_id'] ];
+				} elseif ( isset( $data[ $field['name'] ] ) ) {
+					// Match by merge field name.
+					$field_name                       = $field['name'];
+					$fields_ids[ $field['merge_id'] ] = $field_name;
+				}
+				// If field name is found, add it to the payload.
+				if ( ! empty( $field_name ) ) {
+					$merge_fields[ $field['tag'] ] = $data[ $field_name ];
 					unset( $data[ $field['name'] ] );
 				}
 			}
-			$remaining_fields = array_keys( $data );
 			// Create remaining fields.
+			$remaining_fields = array_keys( $data );
 			foreach ( $remaining_fields as $field_name ) {
-				$created_field                         = Mailchimp_API::post(
+				$created_field                            = Mailchimp_API::post(
 					"lists/$audience_id/merge-fields",
 					[
 						'name' => $field_name,
 						'type' => self::get_merge_field_type( $data[ $field_name ] ),
 					]
 				);
-				$merge_fields[ $created_field['tag'] ] = $data[ $field_name ];
+				$merge_fields[ $created_field['tag'] ]    = $data[ $field_name ];
+				$fields_ids[ $created_field['merge_id'] ] = $field_name;
 			}
 			if ( ! empty( $merge_fields ) ) {
 				$payload['merge_fields'] = $merge_fields;
 			}
+			\update_option( 'newspack_data_mailchimp_fields', $fields_ids );
 		}
 		Mailchimp_API::put(
 			"lists/$audience_id/members/$hash",
