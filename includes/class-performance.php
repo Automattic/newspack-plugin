35b304f5779d0fa5484193ee3a5dc249a9a935ec
Adam Borowski <adam@adamboro.com>
2023-01-19 15:50:34 +0100

    feat(performance): minify all inline CSS

--- includes/class-performance.php
+++ includes/class-performance.php
@@ -35,6 +35,9 @@ class Performance {
 	public static function init() {
 		add_filter( 'newspack_blocks_homepage_posts_block_attributes', [ __CLASS__, 'optimise_homepage_posts_block' ] );
 		add_action( 'newspack_theme_before_page_content', [ __CLASS__, 'mark_page_content_rendering_start' ] );
+
+		add_action( 'template_redirect', [ __CLASS__, 'process_output_html' ] );
+		add_filter( 'newspack_output_html', [ __CLASS__, 'minify_inline_style_tags' ] );
 	}
 
 	/**
@@ -60,6 +63,62 @@ class Performance {
 			self::$current_homepage_posts_block++;
 		}
 		return $attributes;
+	}
+
+	/**
+	 * Process the HTML output.
+	 */
+	public static function process_output_html() {
+		if ( defined( 'NEWSPACK_DISABLE_HTML_PERF_PROCESSING' ) && NEWSPACK_DISABLE_HTML_PERF_PROCESSING ) {
+			return;
+		}
+		if ( ! has_filter( 'newspack_output_html' ) ) {
+			return;
+		}
+		if ( is_embed() || is_feed() || is_preview() || is_customize_preview() ) {
+			return;
+		}
+		ob_start(
+			function( $html ) {
+				if ( stripos( $html, '<html' ) === false || stripos( $html, '</body>' ) === false || stripos( $html, '<xsl:stylesheet' ) !== false ) {
+					return $html;
+				}
+				return (string) apply_filters( 'newspack_output_html', $html );
+			}
+		);
+	}
+
+	/**
+	 * Minify a CSS string.
+	 *
+	 * @param string $css CSS string.
+	 */
+	private static function minify_css( $css ) {
+		// Remove comments.
+		$css = preg_replace( '/\/\*(?:(?!\*\/).)*\*\//is', '', $css );
+		// Remove whitespace.
+		$css = preg_replace(
+			[ '/(,|\{|\}|;|\*\/)\s+/', '/\s+(,|\{|\}|;|\*\/)/' ],
+			'\1',
+			$css
+		);
+		return trim( $css );
+	}
+
+	/**
+	 * Minify inline CSS.
+	 *
+	 * @param string $html HTML string.
+	 */
+	public static function minify_inline_style_tags( $html ) {
+		return preg_replace_callback(
+			'/(<style[^>]*>)(.*?)(<\/style>)/si',
+			function( $matches ) {
+				return $matches[1] . self::minify_css( $matches[2] ) . $matches[3];
+			},
+			$html
+		);
+
 	}
 }
 Performance::init();
