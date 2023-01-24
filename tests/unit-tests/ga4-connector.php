60a385e8e837b4efb2b15b6916ab244d7d08275c
Leo Germani <leogermani@automattic.com>
2023-01-24 10:55:26 -0300

    tests: add more tests

--- tests/unit-tests/ga4-connector.php
+++ tests/unit-tests/ga4-connector.php
@@ -104,4 +104,158 @@ class Newspack_Test_GA4_Connector extends WP_UnitTestCase {
 			$this->assertFalse( Event::validate_name( 'asd123' . $char ) );
 		}
 	}
+
+	/**
+	 * Data provider for test_validate_param_name
+	 */
+	public function validate_param_value_data() {
+		return [
+			[
+				'dsl2390ijd2m, #asd',
+				true,
+			],
+			[
+				123123232,
+				true,
+			],
+			[
+				'iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0',
+				true,
+			],
+			[
+				'iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0iiiiiiiii0i',
+				false,
+			],
+			[
+				[ 123 ],
+				false,
+			],
+			[
+				(object) [ 'asd' => 123 ],
+				false,
+			],
+			[
+				true,
+				false,
+			],
+		];
+	}
+
+	/**
+	 * Tests the validate_param_value method
+	 *
+	 * @param string $value The param value.
+	 * @param bool   $expected The expected result.
+	 * @dataProvider validate_param_value_data
+	 */
+	public function test_validate_param_value( $value, $expected ) {
+		$this->assertEquals( $expected, Event::validate_param_value( $value ) );
+	}
+
+	/**
+	 * Tests the validate_params method
+	 *
+	 * @param array $params The parameters array.
+	 * @param bool  $expected The expected result.
+	 * @dataProvider validate_params_data
+	 */
+	public function test_validate_params( $params, $expected ) {
+		$this->assertEquals( $expected, Event::validate_params( $params ) );
+	}
+
+	/**
+	 * Data provider for test_validate_params
+	 */
+	public function validate_params_data() {
+		return [
+			[
+				[
+					'param1' => 'value1',
+					'param2' => 'value2',
+					'param3' => 'value3',
+					'param4' => 'value4',
+				],
+				true,
+			],
+			[
+				[
+					'invalid' => false,
+					'param2'  => 'value2',
+					'param3'  => 'value3',
+					'param4'  => 'value4',
+				],
+				false,
+			],
+			[
+				[
+					'_invalid' => 'value1',
+					'param2'   => 'value2',
+					'param3'   => 'value3',
+					'param4'   => 'value4',
+				],
+				false,
+			],
+			[
+				[
+					'param1'  => 'value1',
+					'param2'  => 'value2',
+					'param3'  => 'value3',
+					'param4'  => 'value4',
+					'param5'  => 'value5',
+					'param6'  => 'value6',
+					'param7'  => 'value7',
+					'param8'  => 'value8',
+					'param9'  => 'value9',
+					'param10' => 'value10',
+					'param11' => 'value11',
+					'param12' => 'value12',
+					'param13' => 'value13',
+					'param14' => 'value14',
+					'param15' => 'value15',
+					'param16' => 'value16',
+					'param17' => 'value17',
+					'param18' => 'value18',
+					'param19' => 'value19',
+					'param20' => 'value20',
+					'param21' => 'value21',
+					'param22' => 'value22',
+					'param23' => 'value23',
+					'param24' => 'value24',
+					'param25' => 'value25',
+					'param26' => 'value26',
+				],
+				false,
+			],
+			[
+				[
+					'param1'  => 'value1',
+					'param2'  => 'value2',
+					'param3'  => 'value3',
+					'param4'  => 'value4',
+					'param5'  => 'value5',
+					'param6'  => 'value6',
+					'param7'  => 'value7',
+					'param8'  => 'value8',
+					'param9'  => 'value9',
+					'param10' => 'value10',
+					'param11' => 'value11',
+					'param12' => 'value12',
+					'param13' => 'value13',
+					'param14' => 'value14',
+					'param15' => 'value15',
+					'param16' => 'value16',
+					'param17' => 'value17',
+					'param18' => 'value18',
+					'param19' => 'value19',
+					'param20' => 'value20',
+					'param21' => 'value21',
+					'param22' => 'value22',
+					'param23' => 'value23',
+					'param24' => 'value24',
+					'param25' => 'value25',
+				],
+				true,
+			],
+		];
+	}
 }
