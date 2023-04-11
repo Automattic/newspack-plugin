79003de09bcaa45222e9d635141debcb27c580c9
Miguel Peixe <miguel.peixe@automattic.com>
2023-04-11 19:05:12 -0300

    fix: title placeholder

--- includes/plugins/wc-memberships/block-patterns/registration-wall.php
+++ includes/plugins/wc-memberships/block-patterns/registration-wall.php
@@ -9,7 +9,7 @@
 <!-- wp:group {"templateLock":"all","lock":{"move":true,"remove":true},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group">
 <!-- wp:heading -->
-<h2 class="wp-block-heading"></h2>
+<h2 class="wp-block-heading"><?php _e( 'Become a member to continue reading', 'newspack' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:newspack/reader-registration -->
