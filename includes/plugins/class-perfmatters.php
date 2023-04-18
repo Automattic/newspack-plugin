028df2a72bdbba432b7c6cee39eb316d978f4cf9
Miguel Peixe <miguel.peixe@automattic.com>
2023-04-18 13:30:39 -0300

    fix: include JP's social logos css

--- includes/plugins/class-perfmatters.php
+++ includes/plugins/class-perfmatters.php
@@ -82,6 +82,7 @@ class Perfmatters {
 			'plugins/newspack-blocks', // Newspack Blocks.
 			'plugins/newspack-newsletters', // Newspack Newsletters.
 			'plugins/jetpack/modules/sharedaddy', // Jetpack's share buttons.
+			'plugins/jetpack/_inc/social-logos', // Jetpack's social logos CSS.
 			'/themes/newspack-', // Any Newspack theme stylesheet.
 			'wp-includes',
 		];
