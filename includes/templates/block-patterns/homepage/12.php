f092388c34bf7efad62f1194373c3212d72dbe9c
Laurel Fulford <laurel.fulford@automattic.com>
2023-05-05 13:57:21 -0700

    fix: remove unneeded blocks from patterns

--- includes/templates/block-patterns/homepage/12.php
+++ includes/templates/block-patterns/homepage/12.php
@@ -6,8 +6,7 @@
  */
 
 return array(
-	'content' => '<!-- wp:group {"className":"newspack-pattern homepage-posts__style-1"} -->
-<div class="wp-block-group newspack-pattern homepage-posts__style-1"><div class="wp-block-group__inner-container"><!-- wp:columns -->
+	'content' => '<!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"width":"66.66%"} -->
 <div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:newspack-blocks/homepage-articles {"showExcerpt":false,"minHeight":60,"showAvatar":false,"postsToShow":1,"mediaPosition":"behind","typeScale":5} /-->
 
@@ -19,11 +18,9 @@ return array(
 
 <!-- wp:newspack-blocks/homepage-articles {"showExcerpt":false,"minHeight":30,"showAvatar":false,"postsToShow":1,"mediaPosition":"behind","typeScale":2} /--></div>
 <!-- /wp:column --></div>
-<!-- /wp:columns --></div></div>
-<!-- /wp:group -->
+<!-- /wp:columns -->
 
-<!-- wp:group {"className":"newspack-pattern homepage-posts__style-7"} -->
-<div class="wp-block-group newspack-pattern homepage-posts__style-7"><div class="wp-block-group__inner-container"><!-- wp:columns {"className":"is-style-borders"} -->
+<!-- wp:columns {"className":"is-style-borders"} -->
 <div class="wp-block-columns is-style-borders"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:newspack-blocks/homepage-articles {"excerptLength":26,"imageShape":"square","showAvatar":false,"postsToShow":1,"typeScale":6} /--></div>
 <!-- /wp:column -->
@@ -31,13 +28,11 @@ return array(
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:newspack-blocks/homepage-articles {"className":"is-style-borders","excerptLength":28,"showImage":false,"showAuthor":false,"showAvatar":false,"postsToShow":5,"mediaPosition":"left","typeScale":3,"imageScale":1} /--></div>
 <!-- /wp:column --></div>
-<!-- /wp:columns --></div></div>
-<!-- /wp:group -->
+<!-- /wp:columns -->
 
 ' . $donate_block . '
 
-<!-- wp:group {"className":"newspack-pattern homepage-posts__style-3"} -->
-<div class="wp-block-group newspack-pattern homepage-posts__style-3"><div class="wp-block-group__inner-container"><!-- wp:columns {"className":"is-style-borders"} -->
+<!-- wp:columns {"className":"is-style-borders"} -->
 <div class="wp-block-columns is-style-borders"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:newspack-blocks/homepage-articles {"showImage":false,"showAvatar":false,"postsToShow":2,"typeScale":3} /--></div>
 <!-- /wp:column -->
@@ -49,8 +44,7 @@ return array(
 <!-- wp:column -->
 <div class="wp-block-column"><!-- wp:newspack-blocks/homepage-articles {"showImage":false,"showAvatar":false,"postsToShow":2,"typeScale":3} /--></div>
 <!-- /wp:column --></div>
-<!-- /wp:columns --></div></div>
-<!-- /wp:group -->
+<!-- /wp:columns -->
 
 <!-- wp:group {"align":"full","backgroundColor":"dark-gray","textColor":"white","className":"newspack-pattern homepage-posts__style-16"} -->
 <div class="wp-block-group alignfull newspack-pattern homepage-posts__style-16 has-white-color has-dark-gray-background-color has-text-color has-background"><div class="wp-block-group__inner-container"><!-- wp:spacer {"height":20} -->
@@ -76,8 +70,7 @@ return array(
 <!-- /wp:spacer --></div></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"className":"newspack-pattern homepage-posts__style-17"} -->
-<div class="wp-block-group newspack-pattern homepage-posts__style-17"><div class="wp-block-group__inner-container"><!-- wp:columns -->
+<!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column {"width":"66.66%"} -->
 <div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:newspack-blocks/homepage-articles {"excerptLength":30,"showAvatar":false,"postsToShow":5,"mediaPosition":"left","typeScale":3,"imageScale":1} /--></div>
 <!-- /wp:column -->
@@ -87,7 +80,6 @@ return array(
 
 <!-- wp:newspack-blocks/homepage-articles {"showExcerpt":false,"showImage":false,"showAvatar":false,"postsToShow":5,"typeScale":3} /--></div>
 <!-- /wp:column --></div>
-<!-- /wp:columns --></div></div>
-<!-- /wp:group -->',
+<!-- /wp:columns -->',
 	'image'   => '<svg viewBox="0 0 142 102" xmlns="http://www.w3.org/2000/svg"><path d="m74.9444 93.6806h54.2366v2.9583h-54.2366z"/><path d="m74.9444 98.1181h67.0556v1.9719h-67.0556z"/><path d="m74.9444 101.2h65.8226v.8h-65.8226z"/><g clip-rule="evenodd" fill-rule="evenodd"><path d="m0 0h92.0781v56.7014h-92.0781zm3.69792 34.1441h67.30208v6.0399h-67.30208zm26.99478 7.026h-26.99478v6.04h26.99478zm-26.99478 7.6424h7.51908v1.9722h-7.51908zm20.70828 0h-10.3541v1.9722h10.3541z"/><path d="m96.0226 0h45.9774v56.7014h-45.9774zm3.6979 42.033h9.8615v5.1771h-9.8615zm33.5275-5.9167h-33.5275v5.1771h33.5275zm-33.5275 12.6962h7.5195v1.9722h-7.5195zm20.7085 0h-10.354v1.9722h10.354z"/><path d="m142 60.6458h-45.9774v28.3507h45.9774zm-15.531 16.1476h-26.7485v3.2049h26.7485zm-26.7485 4.684h6.5325v1.7257h-6.5325zm18.1195 0h-9.121v1.7257h9.121z"/><path d="m92.0781 60.6458h-92.0781v28.3507h92.0781zm-39.0746 14.6684h-49.30558v4.0677h49.30558zm-49.30558 5.7934h7.51908v1.9723h-7.51908zm20.70828 0h-10.3541v1.9723h10.3541z"/></g><path d="m0 92.941h67.0556v9.059h-67.0556z"/></svg>',
 );
