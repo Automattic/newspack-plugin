0a64fa1cd2add1a112ae8259257e50fecb89cca0
Laurel Fulford <laurel.fulford@automattic.com>
2023-06-13 10:35:46 -0700

    feat: move RSS image to plugin and increase size

--- includes/class-newspack.php
+++ includes/class-newspack.php
@@ -145,6 +145,7 @@ final class Newspack {
 		include_once NEWSPACK_ABSPATH . 'includes/class-starter-content.php';
 		include_once NEWSPACK_ABSPATH . 'includes/class-amp-enhancements.php';
 		include_once NEWSPACK_ABSPATH . 'includes/class-newspack-image-credits.php';
+		include_once NEWSPACK_ABSPATH . 'includes/class-rss-add-image.php';
 
 		/* Integrations with other plugins. */
 		include_once NEWSPACK_ABSPATH . 'includes/plugins/class-jetpack.php';
