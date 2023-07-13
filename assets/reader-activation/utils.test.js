82b7d4f21287b091466a9d467356bdf4dfd8b45d
Miguel Peixe <miguel.peixe@automattic.com>
2023-07-13 17:41:06 -0300

    test: generate multiple ids to ensure length

--- assets/reader-activation/utils.test.js
+++ assets/reader-activation/utils.test.js
@@ -9,4 +9,14 @@ describe( 'generateID', () => {
 		const id = generateID( 10 );
 		expect( id ).toHaveLength( 10 );
 	} );
+	it( 'should always generate a random ID of the given length', () => {
+		let failedIds = 0;
+		for ( let i = 0; i < 1000; i++ ) {
+			const id = generateID( 10 );
+			if ( id.length !== 10 ) {
+				failedIds++;
+			}
+		}
+		expect( failedIds ).toEqual( 0 );
+	} );
 } );
