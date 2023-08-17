23dc31c3ea68d4c86e36a72b38768df0a1bc43e3
Derrick Koo <derrick.koo@automattic.com>
2023-08-17 12:13:19 -0600

    feat: replace Campaigns Analytics dashboard with a temp placeholder

--- assets/wizards/popups/views/analytics/index.js
+++ assets/wizards/popups/views/analytics/index.js
@@ -1,92 +1,42 @@
 /**
- * External dependencies.
- */
-import { stringify } from 'qs';
-import classnames from 'classnames';
-
-/**
  * WordPress dependencies.
  */
-import { useEffect, useState } from '@wordpress/element';
-import apiFetch from '@wordpress/api-fetch';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies.
  */
-import { withWizardScreen, Notice } from '../../../../components/src';
-import Filters from './Filters';
-import Chart from './Chart';
-import Info from './Info';
+import { Button, Card, withWizardScreen } from '../../../../components/src';
 import './style.scss';
-import { useFiltersState, useAnalyticsState } from './utils';
 
 /**
  * Popups Analytics screen.
  */
-const PopupAnalytics = ( { setError } ) => {
-	const [ filtersState, dispatchFilter ] = useFiltersState();
-	const [ siteKitWarningText, setSiteKitWarningText ] = useState();
-	const [ isLoading, setIsLoading ] = useState( false );
-	const [ state, updateState ] = useAnalyticsState();
-	const { report, labels, actions, key_metrics, post_edit_link } = state;
-
-	useEffect( () => {
-		setIsLoading( true );
-		apiFetch( { path: `/newspack/v1/popups-analytics/report/?${ stringify( filtersState ) }` } )
-			.then( response => {
-				updateState( { type: 'UPDATE_ALL', payload: response } );
-				setIsLoading( false );
-			} )
-			.catch( error => {
-				if (
-					error.code === 'newspack_campaign_analytics_sitekit_disconnected' ||
-					error.code === 'newspack_campaign_analytics_sitekit_auth'
-				) {
-					setSiteKitWarningText( error.message );
-					setIsLoading( false );
-				} else {
-					setError( error );
-				}
-			} );
-	}, [ filtersState ] );
-
-	const handleFilterChange = type => payload => dispatchFilter( { type, payload } );
-
-	if ( siteKitWarningText ) {
-		return (
-			<Notice
-				rawHTML
-				isWarning
-				noticeText={ `<a href="/wp-admin/admin.php?page=googlesitekit-splash">${ siteKitWarningText }</a>` }
-			/>
-		);
-	}
-
-	return (
-		<div
-			className={ classnames( 'newspack-campaigns-wizard-analytics__wrapper', {
-				'newspack-campaigns-wizard-analytics__wrapper--loading': isLoading,
-			} ) }
-		>
-			<Filters
-				disabled={ isLoading }
-				labelFilters={ labels }
-				eventActionFilters={ actions }
-				filtersState={ filtersState }
-				onChange={ handleFilterChange }
-			/>
-			{ report && <Chart data={ report } isLoading={ isLoading } /> }
-			{ key_metrics && (
-				<Info
-					keyMetrics={ key_metrics }
-					filtersState={ filtersState }
-					labelFilters={ labels }
-					isLoading={ isLoading }
-					postEditLink={ post_edit_link }
-				/>
-			) }
-		</div>
-	);
-};
+const PopupAnalytics = () => (
+	<div className="newspack-campaigns-wizard-analytics__wrapper">
+		<Card isNarrow>
+			<h2>{ __( 'Coming soon', 'newspack' ) }</h2>
+			<p>
+				{ [
+					<>
+						{ __(
+							'We’re currently redesigning this dashboard to accommodate GA4 and give you deeper insights into Campaign performance. In the meantime, you can find Campaign event data within your GA account. Review this ',
+							'newspack'
+						) }
+					</>,
+					<>
+						<a href="https://help.newspack.com/analytics/">{ __( 'help page', 'newspack' ) }</a>
+					</>,
+					<>{ __( ' to see how Campaign data is being recorded in GA.', 'newspack' ) }</>,
+				] }
+			</p>
+			<Card buttonsCard noBorder>
+				<Button href="https://help.newspack.com/analytics/" isPrimary>
+					{ __( 'View the help page', 'newspack' ) }
+				</Button>
+			</Card>
+		</Card>
+	</div>
+);
 
 export default withWizardScreen( PopupAnalytics );
