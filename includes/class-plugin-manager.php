98dc39818b888fe986b271e5ac5586996041f48c
Ken Gagne <ken.gagne@automattic.com>
2023-09-11 15:16:00 -0400

    Fixing spacing
    Darn linter

--- includes/class-plugin-manager.php
+++ includes/class-plugin-manager.php
@@ -292,7 +292,7 @@ class Plugin_Manager {
 				'PluginURI'   => esc_url( 'https://wordpress.org/plugins/ad-refresh-control/' ),
 				'Download'    => 'wporg',
 			],
-			'ads-txt'     => [
+			'ads-txt'                       => [
 				'Name'        => esc_html__( 'Ads.txt Manager', 'newspack' ),
 				'Description' => esc_html__( 'Create, manage, and validate your ads.txt and app-ads.txt from within WordPress, just like any other content asset.', 'newspack' ),
 				'Author'      => esc_html__( '10up', 'newspack' ),
@@ -300,7 +300,7 @@ class Plugin_Manager {
 				'AuthorURI'   => esc_url( 'https://10up.com/' ),
 				'Download'    => 'wporg',
 				'EditPath'    => 'options-general.php?page=adstxt-settings',
-],
+			],
 			'publisher-media-kit'           => [
 				'Name'        => esc_html__( 'Publisher Media Kit', 'newspack' ),
 				'Description' => esc_html__( 'Quick and easy option for small to medium sized publishers to digitize their media kit.', 'newspack' ),
