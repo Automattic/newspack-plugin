7a85b22347dc7f2436aee84e0e4d29a0d20f3399
Adam Borowski <aborowski24@gmail.com>
2023-09-26 16:19:17 +0200

    refactor(analytics): remove unused popups analytics code

--- includes/analytics/class-analytics.php
+++ includes/analytics/class-analytics.php
@@ -14,13 +14,6 @@ defined( 'ABSPATH' ) || exit;
  */
 class Analytics {
 	/**
-	 * An integer to indicate the context a block is rendered in, e.g. content|overlay campaign|inline campaign.
-	 *
-	 * @var integer
-	 */
-	public static $block_render_context = 3;
-
-	/**
 	 * Constructor
 	 */
 	public function __construct() {
@@ -36,31 +29,11 @@ class Analytics {
 	 * @return array Filtered config options.
 	 */
 	public static function set_extra_analytics_config_options( $gtag_opt ) {
-		if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
-			return $gtag_opt;
-		}
-
-		if ( ! self::can_use_site_kits_analytics() ) {
-			return $gtag_opt;
-		}
-
 		// Set transport type to 'beacon' to allow async requests to complete after a new page is loaded.
 		// See: https://developers.google.com/analytics/devguides/collection/gtagjs/sending-data#specify_different_transport_mechanisms.
 		$gtag_opt['transport_type'] = 'beacon';
 
 		return $gtag_opt;
-	}
-
-	/**
-	 * Can we rely on Site Kit's Analytics module?
-	 */
-	public static function can_use_site_kits_analytics() {
-		if ( ! class_exists( '\Google\Site_Kit\Modules\Analytics\Settings' ) ) {
-			return false;
-		}
-		$sitekit_manager     = Configuration_Managers::configuration_manager_class_for_plugin_slug( 'google-site-kit' );
-		$sitekit_ga_settings = get_option( \Google\Site_Kit\Modules\Analytics\Settings::OPTION, false );
-		return (bool) $sitekit_manager->is_module_active( 'analytics' ) && $sitekit_ga_settings['canUseSnippet'];
 	}
 }
 new Analytics();
