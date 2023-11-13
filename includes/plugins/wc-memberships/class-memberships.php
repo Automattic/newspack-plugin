795a2221be17a60500cba709695b5a9b4303609d
Miguel Peixe <miguel.peixe@a8c.com>
2023-11-13 09:48:13 -0300

    fix(metering): restrict comments on gated content

--- includes/plugins/wc-memberships/class-memberships.php
+++ includes/plugins/wc-memberships/class-memberships.php
@@ -28,7 +28,7 @@ class Memberships {
 	/**
 	 * Membership statuses that should grant access to restricted content.
 	 * See: https://woocommerce.com/document/woocommerce-memberships-user-memberships/#section-4
-	 * 
+	 *
 	 * @var array
 	 */
 	public static $active_statuses = [ 'active', 'complimentary', 'free-trial', 'pending' ];
@@ -598,6 +598,17 @@ class Memberships {
 		if ( get_queried_object_id() !== $post->ID ) {
 			return $excerpt;
 		}
+		return self::get_restricted_post_excerpt( $post );
+	}
+
+	/**
+	 * Get the post excerpt to be displayed in the gate.
+	 *
+	 * @param WP_Post $post Post object.
+	 *
+	 * @return string
+	 */
+	public static function get_restricted_post_excerpt( $post ) {
 		$gate_post_id = self::get_gate_post_id();
 
 		$content = $post->post_content;
