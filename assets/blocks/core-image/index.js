70b9daf444b0fd6a91941dd7a44077026f49a6ba
Jared Rethman <jaredrethman@gmail.com>
2024-02-16 13:44:25 -0600

    feat(blocks): refined ux
    better visual feedback when core/image fetches media meta

--- assets/blocks/core-image/index.js
+++ assets/blocks/core-image/index.js
@@ -2,8 +2,10 @@
  * External dependencies
  */
 import { addFilter } from '@wordpress/hooks';
-import { useSelect } from '@wordpress/data';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { Spinner } from '@wordpress/components';
 import { createHigherOrderComponent } from '@wordpress/compose';
+import './style.scss';
 
 /**
  * Add image credit meta to core/image block attributes
@@ -33,6 +35,7 @@ addFilter(
 	createHigherOrderComponent( BlockEdit => {
 		// eslint-disable-next-line react/display-name
 		return props => {
+			const { lockPostSaving, unlockPostSaving } = useDispatch( 'core/editor' );
 			// Check if this is an image block and it has an ID
 			if ( props.name === 'core/image' && props.attributes.id ) {
 				const imageId = props.attributes.id;
@@ -41,10 +44,24 @@ addFilter(
 					select => select( 'core' ).getMedia( imageId ),
 					[ imageId ]
 				) ?? { meta: {} };
-
+				// Meta added, proceed
 				if ( Object.keys( meta ).length > 0 ) {
 					props.setAttributes( { meta } );
+					unlockPostSaving( 'attachment-meta-empty' );
+					return <BlockEdit { ...props } />;
 				}
+				// Display loading and lock post saving
+				lockPostSaving( 'attachment-meta-empty' );
+				return (
+					<div className="newspack-block-core-image">
+						<BlockEdit { ...props } />
+						<div className="newspack-block-core-image__background">
+							<div className="newspack-block-core-image__spinner">
+								<Spinner />
+							</div>
+						</div>
+					</div>
+				);
 			}
 
 			// Return the original block edit component
