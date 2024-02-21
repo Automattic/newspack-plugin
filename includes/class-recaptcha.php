6f07cc9cfb1c64294f6d652f504759c138fcdd72
dkoo <derrick.koo@automattic.com>
2024-02-21 16:03:26 -0700

    docs: fix doc comment

--- includes/class-recaptcha.php
+++ includes/class-recaptcha.php
@@ -82,7 +82,7 @@ final class Recaptcha {
 	 */
 	public static function register_script() {
 		if ( self::can_use_captcha() ) {
-			// Note: version arg Must be null to avoid the & ver param being read as part of the reCAPTCHA site key .
+			// Note: version arg Must be null to avoid the &ver param being read as part of the reCAPTCHA site key .
 			\wp_register_script(
 				self::SCRIPT_HANDLE,
 				\esc_url( self::get_script_url() ),
