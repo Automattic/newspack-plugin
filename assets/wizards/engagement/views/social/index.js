badc6b76c439980d6618b92fc2ffb118304cc97b
Laurel Fulford <laurel.fulford@automattic.com>
2024-02-29 09:22:57 -0800

    fix: correct text domains in wizards/engagement

--- assets/wizards/engagement/views/social/index.js
+++ assets/wizards/engagement/views/social/index.js
@@ -26,12 +26,13 @@ class Social extends Component {
 		return (
 			<>
 				<ActionCard
-					title={ __( 'Publicize' ) }
+					title={ __( 'Publicize', 'newspack-plugin' ) }
 					badge="Jetpack"
 					description={ __(
-						'Publicize makes it easy to share your site’s posts on several social media networks automatically when you publish a new post.'
+						'Publicize makes it easy to share your site’s posts on several social media networks automatically when you publish a new post.',
+						'newspack-plugin'
 					) }
-					actionText={ __( 'Configure' ) }
+					actionText={ __( 'Configure', 'newspack-plugin' ) }
 					handoff="jetpack"
 					editLink="admin.php?page=jetpack#/sharing"
 				/>
