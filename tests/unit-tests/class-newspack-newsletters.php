f94faa533915a1ff358bf13f2ceb157e9a939f99
dkoo <derrick.koo@automattic.com>
2024-03-15 18:36:59 -0600

    test: add unit tests for ESP sync methods

---
+++ tests/unit-tests/class-newspack-newsletters.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Mock for the \Newspack_Newsletters plugin class, from the Newsletters plugin
+ *
+ * @package Newspack\Tests
+ */
+
+/**
+ * Mock Class for \Newspack Newsletters
+ */
+class Newspack_Newsletters {
+	const EMAIL_HTML_META = 'newspack_email_html';
+
+	/**
+	 * Mock method for service_provider.
+	 */
+	public static function service_provider() {
+		return \get_option( 'newspack_newsletters_service_provider', 'active_campaign' );
+	}
+}
