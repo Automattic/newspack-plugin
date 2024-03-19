8a0e19fd72e106b5eabfc9d54e7a6c814d903676
Jared Rethman <jaredrethman@gmail.com>
2024-03-18 21:51:48 -0500

    fix: remove unused __

--- assets/blocks/core-image/index.tsx
+++ assets/blocks/core-image/index.tsx
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
 import { addFilter } from '@wordpress/hooks';
 import { useSelect } from '@wordpress/data';
 import { useEffect } from '@wordpress/element';
