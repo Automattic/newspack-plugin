ee01d0987347b2ad6be8c5b2a3509ea02848d69a
Jared Rethman <jaredrethman@gmail.com>
2024-03-28 13:04:06 -0500

    feat: adding site status component
    includes php, js, css

---
+++ assets/wizards/dashboard/types/site-statuses.d.ts
@@ -0,0 +1,43 @@
+type Dependencies = Record< string, { isActive: boolean; label: string } >;
+
+type Action = {
+	label: string;
+	statusLabels?: { pending?: string; error?: string; success?: string };
+	canConnect?: boolean;
+	endpoint: string;
+	dependencies?: Dependencies;
+	then: ( args: any ) => boolean;
+};
+
+type Actions = {
+	[ k: string ]: Action;
+};
+
+type ActionLocal = {
+	dependencies: Dependencies;
+};
+
+type SiteStatus = {
+	label: string;
+	canConnect?: boolean;
+	statusLabels?: { [ k in Statuses ]?: string };
+	endpoint: string;
+	dependencies?: Dependencies | null;
+	then: ( args?: any ) => boolean;
+};
+
+type SiteActionModal = {
+	onRequestClose: ( a: boolean ) => void;
+	onSuccess: ( a: Record< string, any > ) => void;
+	plugins: string[];
+};
+
+type PrerequisitesStatus = {
+	prerequisite_status: {
+		[ k: string ]: {
+			active: boolean;
+		};
+	};
+};
+
+type Statuses = 'success' | 'error' | 'error-dependency' | 'pending' | 'pending-install' | 'idle';
