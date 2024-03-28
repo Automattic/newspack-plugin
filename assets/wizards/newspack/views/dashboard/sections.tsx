a062b7908951861d17f414e65f386fc0f1cf0aa8
Jared Rethman <jaredrethman@gmail.com>
2024-03-28 10:47:15 -0500

    feat: added dash card section components

--- assets/wizards/newspack/views/dashboard/sections.tsx
+++ assets/wizards/newspack/views/dashboard/sections.tsx
@@ -6,10 +6,19 @@
 import { __ } from '@wordpress/i18n';
 import { Fragment } from '@wordpress/element';
 /* eslint import/namespace: ['error', { allowComputed: true }] */
+import { Icon, icons } from './icons';
+import { Grid, Card } from '../../../components/src';
 
 const {
 	newspackDashboard: { sections: dashSections },
 } = window;
+
+function getIcon( iconName: keyof typeof icons ) {
+	if ( iconName in icons ) {
+		return icons[ iconName ];
+	}
+	return icons.help;
+}
 
 export default [
 	{
@@ -20,7 +29,28 @@ export default [
 			return dashSectionsKeys.map( sectionKey => {
 				return (
 					<Fragment key={ sectionKey }>
-						<p>{ dashSections[ sectionKey ].title }</p>
+						<hr />
+						<div className="newspack-dashboard__section">
+							<h3>{ dashSections[ sectionKey ].title }</h3>
+							<p>{ dashSections[ sectionKey ].desc }</p>
+							<Grid columns={ 3 } gutter={ 24 } key={ `${ sectionKey }-grid` }>
+								{ dashSections[ sectionKey ].cards.map( ( sectionCard, i ) => {
+									return (
+										<a href={ sectionCard.href } key={ `${ sectionKey }-card-${ i }` }>
+											<Card className="newspack-dashboard__card">
+												<div className="newspack-dashboard__card-icon">
+													<Icon size={ 32 } icon={ getIcon( sectionCard.icon ) } />
+												</div>
+												<div className="newspack-dashboard__card-text">
+													<h4>{ sectionCard.title }</h4>
+													<p>{ sectionCard.desc }</p>
+												</div>
+											</Card>
+										</a>
+									);
+								} ) }
+							</Grid>
+						</div>
 					</Fragment>
 				);
 			} );
