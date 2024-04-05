9da6da9b755321ec44c01f1f48ab579a1035ab56
Jared Rethman <jaredrethman@gmail.com>
2024-04-05 08:22:20 -0500

    fix: allow `exact` prop on Wizard Route

--- assets/wizards/newspack/views/settings/index.tsx
+++ assets/wizards/newspack/views/settings/index.tsx
@@ -12,50 +12,44 @@ import { __ } from '@wordpress/i18n';
 import { render } from '@wordpress/element';
 // Internal
 import { GlobalNotices, Footer, Notice, Wizard } from '../../../../components/src';
-import sections from './sections';
 
 const {
 	newspack_aux_data: { is_debug_mode: isDebugMode = false },
 } = window;
+
+const Connections = () => <h2>Connections</h2>;
+
+const Emails = () => <h2>Emails</h2>;
+
+const Social = () => <h2>Social</h2>;
 
 const Settings = () => {
 	return (
 		<>
 			<GlobalNotices />
 			{ isDebugMode && <Notice debugMode /> }
-<Wizard
-	headerText={ __( 'Newspack / Settings', 'newspack' ) }
-	sections={ [
-		{
-			label: __( 'Connections', 'newspack' ),
-			path: '',
-			render: () => (
-				<h2>
-					Connections
-				</h2>
-			),
-		},
-		{
-			label: __( 'Emails', 'newspack' ),
-			path: '/emails',
-			render: () => (
-				<h2>
-					Emails
-				</h2>
-			),
-		},
-		{
-			label: __( 'Social', 'newspack' ),
-			path: '/social',
-			render: () => (
-				<h2>
-					Social
-				</h2>
-			),
-		},
-	] }
-	renderAboveSections={ () => <></> }
-/>
+			<Wizard
+				headerText={ __( 'Newspack / Settings', 'newspack' ) }
+				sections={ [
+					{
+						label: __( 'Connections', 'newspack' ),
+						path: '/',
+						exact: true,
+						render: Connections,
+					},
+					{
+						label: __( 'Emails', 'newspack' ),
+						path: '/emails',
+						render: Emails,
+					},
+					{
+						label: __( 'Social', 'newspack' ),
+						path: '/social',
+						render: Social,
+					},
+				] }
+				renderAboveSections={ () => <></> }
+			/>
 			<Footer />
 		</>
 	);
