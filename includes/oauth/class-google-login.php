42ca78864a6c940910ee06eb86dc19375408e092
Adam Cassis <adam@adamcassis.com>
2024-04-24 15:31:24 +0200

    fix: handle inability to set transient

--- includes/oauth/class-google-login.php
+++ includes/oauth/class-google-login.php
@@ -144,7 +144,12 @@ class Google_Login {
 		Logger::log( 'Got user email from Google: ' . $user_email );
 
 		// Associate the email address with the a unique ID for later retrieval.
-		set_transient( self::EMAIL_TRANSIENT_PREFIX . OAuth::get_unique_id(), $user_email, 20 );
+		$has_set_transient = set_transient( self::EMAIL_TRANSIENT_PREFIX . OAuth::get_unique_id(), $user_email, 20 );
+		// If transient setting failed, the email address will not be available for the registration endpoint.
+		if ( ! $has_set_transient ) {
+			self::handle_error( __( 'Failed setting transient.', 'newspack-plugin' ) );
+			\wp_die( \esc_html__( 'Authentication failed.', 'newspack' ) );
+		}
 
 		/** Close window if it's a popup. */
 		?>
