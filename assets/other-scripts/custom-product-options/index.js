844f05dbfa7ca4fdba76f3dad251e815d2fa0cec
dkoo <derrick.koo@automattic.com>
2024-05-13 16:55:46 -0600

    fix: autocomplete orders only for virtual products

---
+++ assets/other-scripts/custom-product-options/index.js
@@ -0,0 +1,20 @@
+/* globals jQuery */
+( function ( $ ) {
+	if ( ! $ ) {
+		return;
+	}
+	$( '#variable_product_options' ).on( 'change', 'input.variable_is_virtual', function ( e ) {
+		console.log( e.currentTarget, $( e.currentTarget ).is( ':checked' ) );
+		$( e.currentTarget )
+			.closest( '.woocommerce_variation' )
+			.find( '.show_if_variation_virtual' )
+			.hide();
+
+		if ( $( e.currentTarget ).is( ':checked' ) ) {
+			$( e.currentTarget )
+				.closest( '.woocommerce_variation' )
+				.find( '.show_if_variation_virtual' )
+				.show();
+		}
+	} );
+} )( jQuery );
