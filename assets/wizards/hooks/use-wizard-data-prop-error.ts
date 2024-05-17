26f032eec1a3d950788bee5e8c417001374f5c13
Jared Rethman <jaredrethman@gmail.com>
2024-05-17 09:32:59 -0500

    feat(ia): new hooks for managing granular component level errors

---
+++ assets/wizards/hooks/use-wizard-data-prop-error.ts
@@ -0,0 +1,51 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { useDispatch } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import { Wizard } from '../../components/src';
+import { WIZARD_STORE_NAMESPACE } from '../../components/src/wizard/store';
+/**
+ * Hook for managing granular error within a specific Wizard data object.
+ *
+ * @param slug State group name to store data in
+ * @param prop Property in state group name to manage errors for.
+ *
+ * @return     Object containing callback and value to manage errors
+ *
+ * @example
+ * ```ts
+ * const { error, setError, resetError } = useWizardDataPropError( 'slug', 'prop' );
+ * ```
+ */
+function useWizardDataPropError( slug: string, prop: string ) {
+	const { setDataPropError } = useDispatch( WIZARD_STORE_NAMESPACE );
+	const { error } = Wizard.useWizardDataProp( slug, prop );
+
+	return {
+		error,
+		setError(
+			err: string | Error | { message: string },
+			defaultVal = __( 'Something went wrong.', 'newspack-plugin' )
+		) {
+			let message;
+			if ( ! err ) {
+				message = defaultVal;
+			} else if ( typeof err === 'string' ) {
+				message = err;
+			} else if ( err instanceof Error || 'message' in err ) {
+				message = err.message;
+			}
+			setDataPropError( { slug, prop, message } );
+		},
+		resetError() {
+			setDataPropError( { slug, prop, message: '' } );
+		},
+	};
+}
+
+export default useWizardDataPropError;
