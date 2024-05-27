c294e003f69f690b74f34311baf48884e3aec385
Adam Cassis <adam.cassis@automattic.com>
2024-05-27 20:08:01 +0200

    fix(reader-revenue): prevent sending duplicate receipt emails

--- includes/reader-revenue/woocommerce/class-woocommerce-connection.php
+++ includes/reader-revenue/woocommerce/class-woocommerce-connection.php
@@ -530,6 +530,9 @@ class WooCommerce_Connection {
 		if ( ! is_a( $order, 'WC_Order' ) || ! Emails::can_send_email( Reader_Revenue_Emails::EMAIL_TYPES['RECEIPT'] ) ) {
 			return $enable;
 		}
+		if ( $order->get_meta( '_newspack_receipt_email_sent', true ) ) {
+			return $enable;
+		}
 
 		$frequencies = [
 			'month' => __( 'Monthly', 'newspack-plugin' ),
@@ -591,8 +594,11 @@ class WooCommerce_Connection {
 			$order->get_billing_email(),
 			$placeholders
 		);
-
-		return false;
+		if ( $sent ) {
+			$order->add_meta_data( '_newspack_receipt_email_sent', true, true );
+			return false;
+		}
+		return $enable;
 	}
 
 	/**
