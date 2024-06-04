aab9fcfbf0b11674441da7d3a820ee6b5ab3bd5c
Jared Rethman <jaredrethman@gmail.com>
2024-06-04 16:04:47 -0500

    feat(ia): added analytics component

---
+++ assets/wizards/newspack/views/settings/connections/analytics.tsx
@@ -0,0 +1,35 @@
+/**
+ * Settings Wizard: Connections > Analytics
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import WizardsPluginCard from '../../../../wizards-plugin-card';
+
+/**
+ * Analytics Plugins screen.
+ */
+function Analytics( { editLink }: { editLink: string } ) {
+	/**
+	 * Render.
+	 */
+	return (
+		<WizardsPluginCard
+			{ ...{
+				editLink,
+				slug: 'google-site-kit',
+				name: __( 'Google Analytics', 'newspack-plugin' ),
+				path: '/newspack/v1/plugins/google-site-kit',
+				actionText: __( 'View', 'newspack-plugin' ),
+			} }
+		/>
+	);
+}
+
+export default Analytics;
