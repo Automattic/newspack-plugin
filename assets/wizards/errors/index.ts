c03a8d768471c1a405de97a4bdc260d43a5039b8
Jared Rethman <jaredrethman@gmail.com>
2024-06-04 22:26:27 -0500

    feat(ia): recaptcha error messages

--- assets/wizards/errors/index.ts
+++ assets/wizards/errors/index.ts
@@ -1,2 +1,11 @@
+import { __ } from '@wordpress/i18n';
+
 export { default as WizardError } from './class-wizard-error';
 export { default as WizardApiError } from './class-wizard-api-error';
+
+export const WIZARD_ERROR_MESSAGES = {
+	RECAPTCHA_KEY_SECRET_INVALID: __(
+		'You must enter a valid site key and secret to use reCAPTCHA.',
+		'newspack-plugin'
+	),
+};
