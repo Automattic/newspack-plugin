6c1502045a2dd82ab5c660abfcd59da4ed21144e
Jared Rethman <jaredrethman@gmail.com>
2024-06-04 21:22:29 -0500

    feat(ia): apis error messages

--- assets/wizards/errors/index.ts
+++ assets/wizards/errors/index.ts
@@ -1,2 +1,12 @@
+import { __ } from '@wordpress/i18n';
+
 export { default as WizardError } from './class-wizard-error';
 export { default as WizardApiError } from './class-wizard-api-error';
+
+export const WIZARD_ERROR_MESSAGES = {
+	MAILCHIMP_API_KEY_INVALID: __( 'Invalid Mailchimp API Key.', 'newspack-plugin' ),
+	GOOGLEOAUTH_REFRESH_TOKEN_EXPIRED: __(
+		'Missing Google refresh token. Please re-authenticate site.',
+		'newspack-plugin'
+	),
+};
