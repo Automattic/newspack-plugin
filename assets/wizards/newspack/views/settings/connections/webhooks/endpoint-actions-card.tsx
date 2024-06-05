3851fe7dac3de3c06b04d0444f20cd963aa386b1
Jared Rethman <jaredrethman@gmail.com>
2024-06-05 09:24:49 -0500

    feat(ia): added webhook endpoint actions card component

---
+++ assets/wizards/newspack/views/settings/connections/webhooks/endpoint-actions-card.tsx
@@ -0,0 +1,69 @@
+/**
+ * Settings Wizard: Connections > Webhooks > Endpoint Actions Card
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { Fragment } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { getEndpointTitle } from './utils';
+import EndpointActions from './endpoint-actions';
+import WizardsActionCard from '../../../../../wizards-action-card';
+
+function EndpointActionsCard( {
+	endpoint,
+	setAction,
+}: {
+	endpoint: Endpoint;
+	setAction: ( action: WebhookActions, id: number | string ) => void;
+} ) {
+	return (
+		<WizardsActionCard
+			isMedium
+			className="newspack-webhooks__endpoint mt16"
+			toggleChecked={ ! endpoint.disabled }
+			toggleOnChange={ () => setAction( 'toggle', endpoint.id ) }
+			key={ endpoint.id }
+			title={ getEndpointTitle( endpoint ) }
+			disabled={ endpoint.system }
+			description={ () => {
+				if ( endpoint.disabled && endpoint.disabled_error ) {
+					return `${ __(
+						'This endpoint is disabled due to excessive request errors',
+						'newspack-plugin'
+					) }: ${ endpoint.disabled_error }`;
+				}
+				return (
+					<Fragment>
+						{ __( 'Actions:', 'newspack-plugin' ) }{ ' ' }
+						{ endpoint.global ? (
+							<span className="newspack-webhooks__endpoint-action">
+								{ __( 'global', 'newspack-plugin' ) }
+							</span>
+						) : (
+							endpoint.actions.map( action => (
+								<span key={ action } className="newspack-webhooks__endpoint-action">
+									{ action }
+								</span>
+							) )
+						) }
+					</Fragment>
+				);
+			} }
+			actionText={
+				<EndpointActions
+					endpoint={ endpoint }
+					setAction={ setAction }
+					isSystem={ endpoint.system }
+				/>
+			}
+		/>
+	);
+}
+
+export default EndpointActionsCard;
