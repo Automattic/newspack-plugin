fed8f5469688e6eff12c5841d33a6b41427522c9
Jared Rethman <jaredrethman@gmail.com>
2024-06-06 22:14:45 -0500

    refactor(ia): ga4 measurement id validation
    updated `autocomplete` to `one-time-code` to avoid browser password save prompts

--- assets/wizards/newspack/views/settings/connections/custom-events.tsx
+++ assets/wizards/newspack/views/settings/connections/custom-events.tsx
@@ -11,8 +11,21 @@ import { useState, useEffect } from '@wordpress/element';
 /**
  * Internal dependencies
  */
+import { WizardError } from '../../../../errors';
 import { useWizardApiFetch } from '../../../../hooks/use-wizard-api-fetch';
 import { Button, Grid, Notice, TextControl } from '../../../../../components/src';
+
+/**
+ * Validate GA4 Measurement ID.
+ *
+ * @link https://measureschool.com/ga4-measurement-id/
+ * @param measurementId Measurement ID to validate
+ * @return boolean True if the measurement ID is valid, false otherwise
+ */
+function isValidGA4MeasurementID( measurementId: string = '' ) {
+	const ga4Pattern = /^G-[A-Za-z0-9]{10,}$/;
+	return ga4Pattern.test( measurementId );
+}
 
 /**
  * Analytics Custom Events screen.
@@ -21,13 +34,37 @@ function CustomEvents() {
 	const [ ga4Credentials, setGa4Credentials ] = useState< Ga4Credentials >(
 		window.newspackSettings.connections.sections.customEvents
 	);
-	const { wizardApiFetch, errorMessage, resetError } = useWizardApiFetch(
+	const { wizardApiFetch, errorMessage, resetError, setError } = useWizardApiFetch(
 		'newspack-settings/connections/custom-events'
 	);
 
 	useEffect( () => {
+		if ( ! isValidGA4MeasurementID( ga4Credentials.measurement_id ) ) {
+			setError(
+				new WizardError(
+					__(
+						'You need a valid Measurement ID (e.g. "G-ABCDE12345") to activate Newspack Custom Events.',
+						'newspack-plugin'
+					),
+					'ga4_invalid_measurement_id'
+				)
+			);
+			return;
+		}
+		if ( ! ga4Credentials.measurement_protocol_secret ) {
+			setError(
+				new WizardError(
+					__(
+						'You need a valid Measurement API Secret to activate Newspack Custom Events.',
+						'newspack-plugin'
+					),
+					'ga4_invalid_measurement_protocol_secret'
+				)
+			);
+			return;
+		}
 		resetError();
-	}, [] );
+	}, [ ga4Credentials.measurement_id, ga4Credentials.measurement_protocol_secret ] );
 
 	function updateGa4Credentials() {
 		wizardApiFetch< Ga4Credentials >(
@@ -41,12 +78,11 @@ function CustomEvents() {
 			},
 			{
 				onSuccess( fetchedData ) {
-					window.newspackSettings.connections.sections.analytics = {
-						...window.newspackSettings.connections.sections.analytics,
+					window.newspackSettings.connections.sections.customEvents = {
+						...window.newspackSettings.connections.sections.customEvents,
 						...fetchedData,
 					};
 					setGa4Credentials( fetchedData );
-					resetError();
 				},
 			}
 		);
@@ -69,7 +105,7 @@ function CustomEvents() {
 					value={ ga4Credentials.measurement_id }
 					label={ __( 'Measurement ID', 'newspack-plugin' ) }
 					help={ __(
-						'You can find this in Site Kit Settings, or in Google Analytics > Admin > Data Streams and clickng the data stream. Example: G-ABCD1234',
+						'You can find this in Site Kit Settings, or in Google Analytics > Admin > Data Streams and clickng the data stream. Example: G-ABCDE12345',
 						'newspack-plugin'
 					) }
 					onChange={ ( value: string ) =>
@@ -88,14 +124,14 @@ function CustomEvents() {
 					onChange={ ( value: string ) =>
 						setGa4Credentials( { ...ga4Credentials, measurement_protocol_secret: value } )
 					}
-					autoComplete="off"
+					autoComplete="one-time-code"
 				/>
 			</Grid>
 			<Button
 				className="newspack__analytics-newspack-custom-events__save-button"
 				variant="primary"
 				onClick={ updateGa4Credentials }
-				disabled={ ! ga4Credentials.measurement_id || ! ga4Credentials.measurement_protocol_secret }
+				disabled={ !! errorMessage }
 			>
 				{ __( 'Save', 'newspack-plugin' ) }
 			</Button>
