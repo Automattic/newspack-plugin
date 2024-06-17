c8c842bcb8c6e81997225879a79034eaa163837f
dkoo <derrick.koo@automattic.com>
2024-06-17 15:00:47 -0600

    fix: account for multiple submit buttons per form

--- assets/other-scripts/recaptcha/index.js
+++ assets/other-scripts/recaptcha/index.js
@@ -12,29 +12,55 @@ const isV3 = 'v3' === newspack_recaptcha_data.version;
 const siteKey = newspack_recaptcha_data.site_key;
 const isInvisible = 'v2_invisible' === newspack_recaptcha_data.version;
 
-if ( isV2 ) {
-	window.renderCaptchas = () => {
-		const widgetContainers = [ ...document.querySelectorAll( '.grecaptcha-container' ) ];
-		widgetContainers.forEach( container => {
-			const containerId = container.id;
-			const form = container.closest( 'form' );
-			const submitButton =
-				isInvisible && form && form.querySelector( 'input[type="submit"], button[type="submit"]' ); // If using the invisible widget, the target element must be the form's submit button.
-			const options = {
-				sitekey: siteKey,
-				size: isInvisible ? 'invisible' : 'normal',
-			};
-
-			if ( isV2 && isInvisible && submitButton ) {
-				submitButton.addEventListener( 'click', e => e.preventDefault() ); // Prevent the submit button from submitting the form so reCAPTCHA can intervene.
-				options.callback = () => form.requestSubmit( submitButton ); // If reCAPTCHA passes the action with a token, submit the form.
-			}
-
-			const widgetId = grecaptcha.render( submitButton || container, options );
-			newspack_grecaptcha.widgets[ containerId ] = widgetId;
-		} );
-	};
+/**
+ * Specify a function to execute when the DOM is fully loaded.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/packages/dom-ready/
+ *
+ * @param {Function} callback A function to execute after the DOM is ready.
+ * @return {void}
+ */
+function domReady( callback ) {
+	if ( typeof document === 'undefined' ) {
+		return;
+	}
+	if (
+		document.readyState === 'complete' || // DOMContentLoaded + Images/Styles/etc loaded, so we call directly.
+		document.readyState === 'interactive' // DOMContentLoaded fires at this point, so we call directly.
+	) {
+		return void callback();
+	}
+	// DOMContentLoaded has not fired yet, delay callback until then.
+	document.addEventListener( 'DOMContentLoaded', callback );
 }
+
+domReady( function () {
+	if ( isV2 ) {
+		window.renderCaptchas = () => {
+			const widgetContainers = [ ...document.querySelectorAll( '.grecaptcha-container' ) ];
+			widgetContainers.forEach( container => {
+				const containerId = container.id;
+				const form = container.closest( 'form' );
+				const submitButtons = isInvisible &&
+					form && [ ...form.querySelectorAll( 'input[type="submit"], button[type="submit"]' ) ]; // If using the invisible widget, the target element must be the form's submit button.
+				const options = {
+					sitekey: siteKey,
+					size: isInvisible ? 'invisible' : 'normal',
+				};
+
+				if ( isV2 && isInvisible && 0 < submitButtons.length ) {
+					submitButtons.forEach( submitButton => {
+						submitButton.addEventListener( 'click', e => e.preventDefault() ); // Prevent the submit button from submitting the form so reCAPTCHA can intervene.
+						options.callback = () => form.requestSubmit( submitButton ); // If reCAPTCHA passes the action with a token, submit the form.
+
+						const widgetId = grecaptcha.render( submitButton || container, options );
+						newspack_grecaptcha.widgets[ containerId ] = widgetId;
+					} );
+				}
+			} );
+		};
+	}
+} );
 
 /**
  * Refresh all reCAPTCHA tokens.
