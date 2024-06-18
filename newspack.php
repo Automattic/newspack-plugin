83cd5b205d2120b3c2ae5aedaa77747d1cb83846
dkoo <derrick.koo@automattic.com>
2024-06-18 12:14:45 -0600

    chore: undo unintended semver change

--- newspack.php
+++ newspack.php
@@ -14,7 +14,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'NEWSPACK_PLUGIN_VERSION', '4.3.0-recaptcha.4' );
+define( 'NEWSPACK_PLUGIN_VERSION', '4.3.0' );
 
 // Load language files.
 load_plugin_textdomain( 'newspack-plugin', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
