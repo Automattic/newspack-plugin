3b1158118889a297917128f86bfd6b4a7489e572
Jared Rethman <jaredrethman@gmail.com>
2024-07-05 12:17:30 -0500

    feat(ia): moved error messages to constants file

--- assets/wizards/errors/index.ts
+++ assets/wizards/errors/index.ts
@@ -2,10 +2,3 @@ import { __ } from '@wordpress/i18n';
 
 export { default as WizardError } from './class-wizard-error';
 export { default as WizardApiError } from './class-wizard-api-error';
-
-export const WIZARD_ERROR_MESSAGES = {
-	RECAPTCHA_KEY_SECRET_INVALID: __(
-		'You must enter a valid site key and secret to use reCAPTCHA.',
-		'newspack-plugin'
-	),
-};
