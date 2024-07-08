2e4f500cd6182cdccaa595dcbdbd50a61fee9a29
Jared Rethman <jaredrethman@gmail.com>
2024-07-08 10:39:00 -0500

    fix: ci / eslint

--- assets/wizards/wizards-tab.tsx
+++ assets/wizards/wizards-tab.tsx
@@ -2,11 +2,6 @@
  * Wizards Tab component.
  */
 
-/**
- * WordPress dependencies
- */
-import { __ } from '@wordpress/i18n';
-
 function WizardsTab( { title, children }: { title: string; children: React.ReactNode } ) {
 	return (
 		<div className="newspack-wizard__sections">
