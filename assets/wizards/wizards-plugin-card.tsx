b309da05fa341ed55f98c91915646f41823956e1
Jared Rethman <jaredrethman@gmail.com>
2024-07-08 21:20:00 -0500

    feat(ia): added `onStatusChange` @param desc

--- assets/wizards/wizards-plugin-card.tsx
+++ assets/wizards/wizards-plugin-card.tsx
@@ -105,13 +105,14 @@ function WizardsPluginCardButton( {
 /**
  * Wizard Plugin Card component.
  *
- * @param {Object} props Component props.
- * @param {string} props.slug Plugin slug.
- * @param {string} props.title Plugin title.
- * @param {string} props.subTitle Plugin subtitle. String appended to title.
- * @param {string} props.editLink Plugin edit link.
- * @param {string} props.description Plugin description.
- * @param {string} props.statusDescription Plugin status description.
+ * @param props Component props.
+ * @param props.slug Plugin slug.
+ * @param props.title Plugin title.
+ * @param props.subTitle Plugin subtitle. String appended to title.
+ * @param props.editLink Plugin edit link.
+ * @param props.description Plugin description.
+ * @param props.statusDescription Plugin status description.
+ * @param props.onStatusChange Callback invoked when the plugin status changes.
  */
 function WizardsPluginCard( {
 	slug,
