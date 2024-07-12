61607bf5b49449afa3d4a9308380b9d05c1df599
Jared Rethman <jaredrethman@gmail.com>
2024-07-12 15:17:22 -0500

    refactor(ia): plugin support for new structure

--- assets/wizards/newspack/views/settings/syndication/plugins.tsx
+++ assets/wizards/newspack/views/settings/syndication/plugins.tsx
@@ -13,38 +13,37 @@ import { Fragment } from '@wordpress/element';
  */
 import WizardsPluginCard from '../../../../wizards-plugin-card';
 
-const PLUGINS: Record< string, PluginCard & { isConfigurable: boolean; isTogglable: boolean } > = {
+const PLUGINS: Record< string, PluginCard > = {
 	'publish-to-apple-news': {
 		slug: 'publish-to-apple-news',
 		title: __( 'Apple News', 'newspack-plugin' ),
 		editLink: 'admin.php?page=apple_news_index',
 		isConfigurable: true,
-		isTogglable: true,
-		// actionText: __( 'Configure', 'newspack-plugin' ),
 		description: __( 'Export and synchronize posts to Apple format', 'newspack-plugin' ),
 	},
-	// distributor: {
-	// 	slug: 'distributor',
-	// 	title: __( 'Distributor', 'newspack-plugin' ),
-	// 	editLink: 'admin.php?page=googlesitekit-splash',
-	// 	actionText: __( 'Configure', 'newspack-plugin' ),
-	// 	description( _, isFetching ) {
-	// 		if ( isFetching ) {
-	// 			return __( 'Loading…', 'newspack-plugin' );
-	// 		}
-	// 		return __(
-	// 			'Distributor is a WordPress plugin that makes it easy to syndicate and reuse content across your websites — whether in a single multisite or across the web.',
-	// 			'newspack-plugin'
-	// 		);
-	// 	},
-	// },
+	distributor: {
+		slug: 'distributor',
+		title: __( 'Distributor', 'newspack-plugin' ),
+		description: __(
+			'Distributor is a WordPress plugin that makes it easy to syndicate and reuse content across your websites — whether in a single multisite or across the web.',
+			'newspack-plugin'
+		),
+	},
 };
 
 function Plugins() {
 	return (
 		<Fragment>
 			{ Object.keys( PLUGINS ).map( pluginKey => {
-				return <WizardsPluginCard key={ pluginKey } { ...PLUGINS[ pluginKey ] } />;
+				return (
+					<WizardsPluginCard
+						key={ pluginKey }
+						isTogglable
+						isStatusPrepended={ false }
+						isMedium={ false }
+						{ ...PLUGINS[ pluginKey ] }
+					/>
+				);
 			} ) }
 		</Fragment>
 	);
