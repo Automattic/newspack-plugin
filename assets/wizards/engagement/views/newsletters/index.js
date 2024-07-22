611c89d51a7310156b9251ed25acdea26d5cc6b3
dkoo <derrick.koo@automattic.com>
2024-07-22 16:37:24 -0600

    feat: show deprecation warning for CM in Engagement wizard

--- assets/wizards/engagement/views/newsletters/index.js
+++ assets/wizards/engagement/views/newsletters/index.js
@@ -14,7 +14,7 @@ import once from 'lodash/once';
 import { useEffect, useState, Fragment } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 import { sprintf, __ } from '@wordpress/i18n';
-import { CheckboxControl, TextareaControl, ExternalLink } from '@wordpress/components';
+import { CheckboxControl, TextareaControl, ExternalLink, Notice } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -187,6 +187,18 @@ export const NewspackNewsletters = ( {
 								{ __( 'Authorize', 'newspack-plugin' ) }
 							</Button>
 						</Card>
+					) }
+					{ 'campaign_monitor' ===
+						config?.settings?.newspack_newsletters_service_provider?.value && (
+						<Notice isWarning isDismissible={ false }>
+							<h2>{ __( 'Campaign Monitor support will be deprecated', 'newspack-plugin' ) }</h2>
+							<p>
+								{ __(
+									'Please connect a different service provider to ensure continued support.',
+									'newspack-'
+								) }
+							</p>
+						</Notice>
 					) }
 					{ values( config.settings )
 						.filter(
