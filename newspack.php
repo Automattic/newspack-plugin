0c43cdca7fc81e380a941b01e0a55f746280687c
dkoo <derrick.koo@automattic.com>
2024-08-02 14:08:09 -0600

    chore: revert unintended semver change

--- newspack.php
+++ newspack.php
@@ -14,7 +14,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'NEWSPACK_PLUGIN_VERSION', '4.7.0-recaptcha-ras-acc.5' );
+define( 'NEWSPACK_PLUGIN_VERSION', '4.7.0' );
 
 // Load language files.
 load_plugin_textdomain( 'newspack-plugin', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
