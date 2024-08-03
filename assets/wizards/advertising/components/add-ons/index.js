9bbac1a1133bfbd776b2327e75219139e1dd6381
Jared Rethman <jaredrethman@gmail.com>
2024-08-02 20:16:55 -0500

    refactor: pr feedback - reload browser after install/uninstall

--- assets/wizards/advertising/components/add-ons/index.js
+++ assets/wizards/advertising/components/add-ons/index.js
@@ -20,21 +20,23 @@ export default function AddOns() {
 				'super-cool-ad-inserter': {
 					actionText: __( 'Configure', 'newspack-plugin' ),
 					href: '#/settings/scaip',
+					shouldRefreshAfterUpdate: true,
 					onClick() {
 						document.getElementById( 'plugin-settings-scaip' ).scrollIntoView();
 					},
 				},
 				'ad-refresh-control': {
 					actionText: __( 'Configure', 'newspack-plugin' ),
+					href: '#/settings/ad-refresh-control',
+					shouldRefreshAfterUpdate: true,
 					onClick() {
 						document.getElementById( 'ad-refresh-control' ).scrollIntoView();
 					},
-					href: '#/settings/ad-refresh-control',
 				},
 				'publisher-media-kit': {
-					shouldRefreshAfterUpdate: true,
 					actionText: __( 'Edit Media Kit', 'newspack-plugin' ),
 					href: newspack_ads_wizard.mediakit_edit_url ? newspack_ads_wizard.mediakit_edit_url : '',
+					shouldRefreshAfterUpdate: true,
 				},
 			} }
 		/>
