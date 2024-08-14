559d294ad1307482d3a8773b15225827a7511792
Rasmy Nguyen <raz@automattic.com>
2024-08-14 15:06:22 -0400

    fix(ras-acc): remove left and right padding from social icons

--- includes/util.php
+++ includes/util.php
@@ -563,7 +563,7 @@ function newspack_get_social_markup( $color = 'white' ) {
 	$cm          = Configuration_Managers::configuration_manager_class_for_plugin_slug( 'wordpress_seo' );
 	$social_urls = [];
 	$markup      = [
-		'block_markup' => '<!-- wp:social-links {"iconColor":"primary-text","iconColorValue":"primary-text","className":"is-style-filled-primary-text","layout":{"type":"flex","flexWrap":"nowrap"}} --><ul class="wp-block-social-links has-icon-color is-style-filled-primary-text">',
+		'block_markup' => '<!-- wp:social-links {"iconColor":"primary-text","iconColorValue":"primary-text","className":"is-style-filled-primary-text","style":{"spacing":{"padding":{"right":"0","left":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap"}} --><ul class="wp-block-social-links has-icon-color is-style-filled-primary-text" style="padding-right:0;padding-left:0">',
 		'html_markup'  => '',
 	];
 
