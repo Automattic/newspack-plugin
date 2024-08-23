a80ebc5fb19fa89e0fbb877ef7962d9b7f3dba66
Jared Rethman <jaredrethman@gmail.com>
2024-08-23 09:48:12 -0500

    fix: ci / eslint

--- assets/wizards/newspack/views/settings/seo/accounts.tsx
+++ assets/wizards/newspack/views/settings/seo/accounts.tsx
@@ -5,7 +5,6 @@
 /**
  * WordPress dependencies.
  */
-import { __ } from '@wordpress/i18n';
 import { ACCOUNTS } from './constants';
 import { Grid, TextControl } from '../../../../../components/src';
 
