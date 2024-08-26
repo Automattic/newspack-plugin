80e8b74d161bb0a6eb014d6e2bedb7f0c1a73efc
Jared Rethman <jaredrethman@gmail.com>
2024-08-26 11:23:17 -0500

    fix: rename `assets` to `src`

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 /**
  **** WARNING: No ES6 modules here. Not transpiled! ****
  */
-/* eslint-disable import/no-nodejs-modules */
-/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable */
 
 /**
  * External dependencies
