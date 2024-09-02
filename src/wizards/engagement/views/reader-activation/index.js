6a75d78a7c81a810c180a28569856d859e3f4805
Adam Cassis <adam.cassis@automattic.com>
2024-09-02 12:00:13 +0200

    feat: prevent missing activecampaign master list id

--- src/wizards/engagement/views/reader-activation/index.js
+++ src/wizards/engagement/views/reader-activation/index.js
@@ -391,10 +391,17 @@ export default withWizardScreen( ( { wizardApiFetch } ) => {
 						<Button
 							isPrimary
 							onClick={ () => {
-								if (isMailchimp && config.sync_esp && config.mailchimp_audience_id === '') {
-									// eslint-disable-next-line no-alert
-									alert( __( 'Please select a Mailchimp Audience ID.', 'newspack-plugin' ) );
-									return
+								if ( config.sync_esp ) {
+									if (isMailchimp && config.mailchimp_audience_id === '') {
+										// eslint-disable-next-line no-alert
+										alert( __( 'Please select a Mailchimp Audience ID.', 'newspack-plugin' ) );
+										return
+									}
+									if (isActiveCampaign && config.active_campaign_master_list === '') {
+										// eslint-disable-next-line no-alert
+										alert( __( 'Please select an ActiveCampaign Master List.', 'newspack-plugin' ) );
+										return
+									}
 								}
 								saveConfig( {
 									newsletters_label: config.newsletters_label, // TODO: Deprecate this in favor of user input via the prompt copy wizard.
