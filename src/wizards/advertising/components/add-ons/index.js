db04b2f3952073c20aa4500ea2a7d41d352fa22a
Adam Cassis <adam.cassis@automattic.com>
2024-09-13 10:30:17 +0200

    feat(ads): media kit page handling in add-ons

--- src/wizards/advertising/components/add-ons/index.js
+++ src/wizards/advertising/components/add-ons/index.js
@@ -1,3 +1,5 @@
+/* globals newspack_ads_wizard */
+
 /**
  * Ad Add-ons component
  */
@@ -6,14 +8,80 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import apiFetch from '@wordpress/api-fetch';
 
 /**
  * Internal dependencies
  */
-import { PluginToggle } from '../../../../components/src';
+import { PluginToggle, ActionCard } from '../../../../components/src';
+import { useState } from 'react';
 
-export default function AddOns() {
+const MediaKitToggle = () => {
+	const [ isInFlight, setInFlight ] = useState( false );
+	const [ editURL, setEditURL ] = useState(
+		newspack_ads_wizard.media_kit_page_edit_url
+	);
+	const [ pageStatus, setPageStatus ] = useState(
+		newspack_ads_wizard.media_kit_page_status
+	);
+
+	const toggleMediaKit = () => {
+		setInFlight( true );
+		apiFetch( {
+			path: '/newspack/v1/wizard/billboard/media-kit',
+			method: pageStatus === 'publish' ? 'DELETE' : 'POST',
+		} )
+			.then( ( { edit_url, page_status } ) => {
+				setEditURL( edit_url );
+				setPageStatus( page_status );
+			} )
+			.finally( () => setInFlight( false ) );
+	};
+
+	const props = editURL
+		? {
+				href: editURL,
+				actionText: __( 'Edit Media Kit page', 'newspack-plugin' ),
+		  }
+		: {};
+
+	let description = __(
+		'Media Kit page is not published. Click the link to edit and publish it.',
+		'newspack-plugin'
+	);
+	let toggleEnabled = false;
+	switch ( pageStatus ) {
+		case 'publish':
+			description = __(
+				'Media Kit page is published. Click the link to edit it, or toggle this card to unpublish.',
+				'newspack-plugin'
+			);
+			toggleEnabled = true;
+			break;
+		case '':
+			description = __(
+				'Media Kit page has not been created. Toggle this card to create it.',
+				'newspack-plugin'
+			);
+			toggleEnabled = true;
+			break;
+	}
+
 	return (
+		<ActionCard
+			disabled={ isInFlight || ! toggleEnabled }
+			title={ __( 'Media Kit', 'newspack-plugin' ) }
+			description={ description }
+			toggle
+			toggleChecked={ Boolean( editURL ) }
+			toggleOnChange={ toggleMediaKit }
+			{ ...props }
+		/>
+	);
+};
+
+export default () => (
+	<>
 		<PluginToggle
 			plugins={ {
 				'super-cool-ad-inserter': {
@@ -26,5 +94,6 @@ export default function AddOns() {
 				},
 			} }
 		/>
-	);
-}
+		<MediaKitToggle />
+	</>
+);
