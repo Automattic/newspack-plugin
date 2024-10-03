de84fee4c11184bda2a5ccbdc7a9f563fbec16fc
Laurel Fulford <laurel.fulford@automattic.com>
2024-10-03 08:18:30 -0700

    feat: add elections block patterns

--- includes/class-newspack.php
+++ includes/class-newspack.php
@@ -162,6 +162,7 @@ final class Newspack {
 		include_once NEWSPACK_ABSPATH . 'includes/plugins/wc-memberships/class-memberships.php';
 		include_once NEWSPACK_ABSPATH . 'includes/plugins/class-woocommerce.php';
 		include_once NEWSPACK_ABSPATH . 'includes/plugins/class-teams-for-memberships.php';
+		include_once NEWSPACK_ABSPATH . 'includes/plugins/class-newspack-elections.php';
 
 		include_once NEWSPACK_ABSPATH . 'includes/class-patches.php';
 		include_once NEWSPACK_ABSPATH . 'includes/polyfills/class-amp-polyfills.php';
