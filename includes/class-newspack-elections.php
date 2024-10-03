d8110715947c0c01c5dede53fbb8cca34c08d1f5
Laurel Fulford <laurel.fulford@automattic.com>
2024-10-02 17:05:29 -0700

    feat: add Newspack Elections block patterns

---
+++ includes/class-newspack-elections.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Govpack
+ *
+ * @package Govpack
+ */
+
+namespace Newspack;
+
+/**
+ *
+ */
+class Elections_Block_Patterns {
+	/**
+	 * Initialize hooks.
+	 */
+	public static function init() {
+		add_action( 'admin_init', [ __CLASS__, 'register_block_patterns' ] );
+	}
+
+	/**
+	 * Get block patterns.
+	 *
+	 * Each pattern content should be a PHP file in the block-patterns directory
+	 * named after the pattern slug.
+	 *
+	 * @return array
+	 */
+	public static function get_block_patterns() {
+		return [
+			'home-pre-results-ap'    => __( 'Homepage Pre-Results Module — AP', 'newspack-plugin' ),
+			'home-results-ap'        => __( 'Homepage Results Module — AP', 'newspack-plugin' ),
+			'live-results-post-ap'   => __( 'Live Election Results Post — AP', 'newspack-plugin' ),
+			'home-pre-results-ddhq'  => __( 'Homepage Pre-Results Module — DDHQ', 'newspack-plugin' ),
+			'home-results-ddhq'      => __( 'Homepage Results Module — DDHQ', 'newspack-plugin' ),
+			'live-results-post-ddhq' => __( 'Live Election Results Post — DDHQ', 'newspack-plugin' ),
+		];
+	}
+
+	/**
+	 * Register block patterns.
+	 */
+	public static function register_block_patterns() {
+		// Bail if Newspack Elections is not active.
+		if ( ! class_exists( '\Govpack\Core\Govpack' ) ) {
+			return false;
+		}
+
+		\register_block_pattern_category( 'newspack-plugin', [ 'label' => __( 'Newspack Elections', 'newspack-plugin' ) ] );
+		$patterns = self::get_block_patterns();
+		foreach ( $patterns as $slug => $title ) {
+			$path = __DIR__ . '/templates/block-patterns/elections/' . $slug . '.php';
+			if ( ! file_exists( $path ) ) {
+				continue;
+			}
+			ob_start();
+			require $path;
+			$content = ob_get_clean();
+			if ( empty( $content ) ) {
+				continue;
+			}
+			\register_block_pattern(
+				'newspack-elections/' . $slug,
+				[
+					'categories'  => [ 'newspack-plugin' ],
+					'title'       => $title,
+					'description' => _x( 'Help format and provide context to AP and DDHQ elections embeds.', 'Block pattern description', 'newspack-plugin' ),
+					'content'     => $content,
+				]
+			);
+		}
+	}
+}
+Elections_Block_Patterns::init();
--- includes/class-newspack.php
+++ includes/class-newspack.php
@@ -117,6 +117,7 @@ final class Newspack {
 		include_once NEWSPACK_ABSPATH . 'includes/tracking/class-twitter-pixel.php';
 		include_once NEWSPACK_ABSPATH . 'includes/revisions-control/class-revisions-control.php';
 		include_once NEWSPACK_ABSPATH . 'includes/authors/class-authors-custom-fields.php';
+		include_once NEWSPACK_ABSPATH . 'includes/class-newspack-elections.php';
 
 		include_once NEWSPACK_ABSPATH . 'includes/starter_content/class-starter-content-provider.php';
 		include_once NEWSPACK_ABSPATH . 'includes/starter_content/class-starter-content-generated.php';
