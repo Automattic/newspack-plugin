0f2eae5e00e064fa53302ccf56aed97a9d2c2dac
Leo Germani <leogermani@automattic.com>
2024-10-15 15:34:33 -0300

    feat: display list remote name on newsletter wizard

--- src/wizards/engagement/views/newsletters/index.js
+++ src/wizards/engagement/views/newsletters/index.js
@@ -352,8 +352,7 @@ export const SubscriptionLists = ( { lockedLists, onUpdate, initialProvider } ) 
 							isSmall
 							simple
 							hasWhiteHeader
-							title={ list.name }
-							description={ list?.type_label ? list.type_label : null }
+							title={ list.remote_name }
 							disabled={ inFlight }
 							toggleOnChange={ handleChange( index, 'active' ) }
 							toggleChecked={ list.active }
