856f034a2558f4ec793ba083baad2314de484b01
Rasmy Nguyen <raz@automattic.com>
2024-10-15 14:02:34 -0400

    fix: use delete_option_ hook

--- includes/plugins/google-site-kit/class-googlesitekit.php
+++ includes/plugins/google-site-kit/class-googlesitekit.php
@@ -9,7 +9,7 @@ namespace Newspack;
 
 use Google\Site_Kit\Context;
 use Google\Site_Kit\Modules\Analytics_4\Settings;
-use Google\Site_Kit\Core\Authentication\Disconnected_Reason;
+use Google\Site_Kit\Core\Authentication\Has_Connected_Admins;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -27,7 +27,7 @@ class GoogleSiteKit {
 		add_action( 'wp_footer', [ __CLASS__, 'insert_ga4_analytics' ] );
 		add_filter( 'option_googlesitekit_analytics_settings', [ __CLASS__, 'filter_ga_settings' ] );
 		add_filter( 'googlesitekit_gtag_opt', [ __CLASS__, 'add_ga_custom_parameters' ] );
-		add_action( 'updated_user_meta', [ __CLASS__, 'maybe_log_disconnect' ], 10, 5 );
+		add_action( 'delete_option_' . Has_Connected_Admins::OPTION, [ __CLASS__, 'maybe_log_disconnect' ] );
 	}
 
 	/**
@@ -176,17 +176,11 @@ class GoogleSiteKit {
 	/**
 	 * Log when a user disconnects from Google Site Kit.
 	 *
-	 * @param int    $meta_id    Meta ID.
-	 * @param int    $object_id  Object ID.
-	 * @param string $meta_key   Meta key.
-	 * @param mixed  $meta_value Meta value.
+	 * @param string $option Option being deleted.
 	 */
-	public static function maybe_log_disconnect( $meta_id, $object_id, $meta_key, $meta_value ) {
-		if ( str_contains( $meta_key, Disconnected_Reason::OPTION ) ) {
-			// TODO: Determine what needs to be logged/alerted.
-			Logger::log( 'User has been disconnected from Google Site Kit.' );
-			Logger::log( 'Disconnect reason: ' . $meta_value );
-		}
+	public static function maybe_log_disconnect( $option ) {
+		// TODO: Determine what needs to be logged/alerted.
+		Logger::log( 'Google Site Kit has been disconnected.' );
 	}
 }
 GoogleSiteKit::init();
