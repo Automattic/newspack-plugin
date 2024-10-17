17c9ded416ab6d575d3e008b63e0b804761f0124
Jared Rethman <jaredrethman@gmail.com>
2024-10-17 08:46:07 -0500

    refactor: remove unnecessary properties from `Brand` type

--- src/wizards/newspack/views/settings/additional-brands/types.d.ts
+++ src/wizards/newspack/views/settings/additional-brands/types.d.ts
@@ -24,41 +24,9 @@ type Brand = {
 			menu: number;
 		} >;
 	};
-	yoast_head?: string;
-	yoast_head_json?: {
-		title: string;
-		robots: {
-			index: string;
-			follow: string;
-			'max-snippet': string;
-			'max-image-preview': string;
-			'max-video-preview': string;
-		};
-		og_locale: string;
-		og_type: string;
-		og_title: string;
-		og_url: string;
-		og_site_name: string;
-		twitter_card: string;
-		twitter_site: string;
-		schema: {
-			'@context': string;
-			'@graph': Array< {
-				'@type': string;
-				'@id': string;
-				[ key: string ]: any;
-			} >;
-		};
-	};
-	_links?: {
-		self: Array< { href: string } >;
-		collection: Array< { href: string } >;
-		about: Array< { href: string } >;
-		'wp:post_type': Array< { href: string } >;
-		curies: Array< {
-			name: string;
-			href: string;
-			templated: boolean;
-		} >;
-	};
 };
+
+interface PublicPage {
+	id: string;
+	title: { rendered: string };
+}
