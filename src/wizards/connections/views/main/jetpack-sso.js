c5c9978c21b8bc61780b5425206d492fe686b496
Miguel Peixe <miguel.peixe@automattic.com>
2024-10-21 10:25:40 -0300

    feat: add render condition and form fields

--- src/wizards/connections/views/main/jetpack-sso.js
+++ src/wizards/connections/views/main/jetpack-sso.js
@@ -1,8 +1,9 @@
+/* globals newspack_connections_data */
 /**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { BaseControl, ExternalLink } from '@wordpress/components';
+import { BaseControl, CheckboxControl } from '@wordpress/components';
 import { useEffect, useState } from '@wordpress/element';
 import apiFetch from '@wordpress/api-fetch';
 
@@ -12,6 +13,7 @@ import apiFetch from '@wordpress/api-fetch';
 import {
 	ActionCard,
 	Button,
+	Grid,
 	Notice,
 	SectionHeader,
 	SelectControl,
@@ -57,6 +59,9 @@ const JetpackSSO = () => {
 			setIsLoading( false );
 		}
 	};
+	if ( ! newspack_connections_data.can_use_jetpack_sso ) {
+		return null;
+	}
 	return (
 		<>
 			<SectionHeader id="jetpack-sso" title={ __( 'Jetpack SSO', 'newspack-plugin' ) } />
@@ -66,7 +71,7 @@ const JetpackSSO = () => {
 				description={ () => (
 					<>
 						{ __(
-							'Improve security by requiring two-factor authentication for users with higher capabilities.',
+							'Improve security by requiring two-factor authentication via WordPress.com for users with higher capabilities.',
 							'newspack-plugin'
 						) }
 					</>
@@ -90,46 +95,51 @@ const JetpackSSO = () => {
 				{ settings.force_2fa && (
 					<>
 						{ error && <Notice isError noticeText={ error } /> }
-						{ ! settings.jetpack_sso && (
-							<Notice
-								noticeText={ __(
-									'Jetpack SSO module is not enabled.',
-									'newspack-plugin'
-								) }
-							/>
-						) }
 						{ settings.jetpack_sso_force_2fa && (
+							<>
 							<Notice
+								isError
 								noticeText={ __(
 									'Two-factor authentication is currently enforced for all users via Jetpack configuration.',
 									'newspack-plugin'
 								) }
 							/>
+							<p>
+								{ __(
+									'Customize which capabilties to enforce 2FA by untoggling the “Require accounts to use WordPress.com Two-Step Authentication” option in Jetpack settings.',
+									'newspack-plugin'
+								) }
+							</p>
+							</>
 						) }
-						<BaseControl
-							id="force-2fa-cap"
-							label={ __( 'Select the user capability to enforce two-factor authentication', 'newspack-plugin' ) }
-						>
-							<SelectControl
-								label={ __( 'Capability', 'newspack-plugin' ) }
-								hideLabelFromVision
-								value={ settingsToUpdate?.force_2fa_cap || '' }
-								onChange={ value =>
-									setSettingsToUpdate( { ...settingsToUpdate, force_2fa_cap: value } )
-								}
-								options={
-									Object.keys( settings.available_caps || {} ).map( cap => ( {
-										label: settings.available_caps[ cap ],
-										value: cap,
-									} ) )
-								}
+						<Grid columns={ 1 }>
+							<BaseControl
+								id="force-2fa-cap"
+								label={ __( 'Select the user capability to enforce two-factor authentication', 'newspack-plugin' ) }
+							>
+								<SelectControl
+									label={ __( 'Capability', 'newspack-plugin' ) }
+									hideLabelFromVision
+									value={ settingsToUpdate?.force_2fa_cap || '' }
+									onChange={ value =>
+										setSettingsToUpdate( { ...settingsToUpdate, force_2fa_cap: value } )
+									}
+									options={
+										Object.keys( settings.available_caps || {} ).map( cap => ( {
+											label: settings.available_caps[ cap ],
+											value: cap,
+										} ) )
+									}
+								/>
+							</BaseControl>
+						</Grid>
+						<Grid columns={ 1 }>
+							<CheckboxControl
+								checked={ settingsToUpdate?.obfuscate_account || false }
+								onChange={ value => setSettingsToUpdate( { ...settingsToUpdate, obfuscate_account: value } ) }
+								label={ __( 'Obfuscate restricted accounts by throwing WP’s “user not found” errors on login form attempts.', 'newspack-plugin' ) }
 							/>
-						</BaseControl>
-						<p>
-							<ExternalLink href="https://jetpack.com/support/sso/">
-								{ __( 'Learn more about Jetpack SSO', 'newspack-plugin' ) }
-							</ExternalLink>
-						</p>
+						</Grid>
 					</>
 				) }
 			</ActionCard>
