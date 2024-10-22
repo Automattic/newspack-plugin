0c754a23de6313c500e7458ed5820a084183ceea
Jared Rethman <jaredrethman@gmail.com>
2024-10-22 11:41:58 -0500

    feat: conditionally load additional-brands component
    if `newspack-multibranded-site` plugin is active

--- src/wizards/newspack/views/settings/sections.tsx
+++ src/wizards/newspack/views/settings/sections.tsx
@@ -4,23 +4,25 @@
  * Component for outputting sections with grid and cards
  */
 import { __ } from '@wordpress/i18n';
+import { lazy } from '@wordpress/element';
 
 const settingsTabs = window.newspackSettings;
 
+import Seo from './seo';
 import Social from './social';
 import Emails from './emails';
 import Connections from './connections';
 import Syndication from './syndication';
 import DisplaySettings from './display-settings';
 import ThemeAndBrand from './theme-and-brand';
-import Seo from './seo';
-import AdditionalBrands from './additional-brands';
 
 type SectionKeys = keyof typeof settingsTabs;
 
-const sectionComponents: Record<
-	SectionKeys | 'default',
-	( a: { isPartOfSetup?: boolean } ) => React.ReactNode
+const sectionComponents: Partial<
+	Record<
+		SectionKeys | 'default',
+		( props: { isPartOfSetup?: boolean } ) => React.ReactNode
+	>
 > = {
 	connections: Connections,
 	social: Social,
@@ -29,18 +31,30 @@ const sectionComponents: Record<
 	seo: Seo,
 	'theme-and-brand': ThemeAndBrand,
 	'display-settings': DisplaySettings,
-	'additional-brands': AdditionalBrands,
 	default: () => <h2>🚫 { __( 'Not found' ) }</h2>,
 };
 
+/**
+ * Load additional brands section if `newspack-multibranded-site` plugin is active.
+ */
+if ( 'additional-brands' in settingsTabs ) {
+	sectionComponents[ 'additional-brands' ] = lazy(
+		() =>
+			import(
+				/* webpackChunkName: "newspack-wizards" */ './additional-brands'
+			)
+	);
+}
+
 const settingsSectionKeys = Object.keys( settingsTabs ) as SectionKeys[];
 
-export default settingsSectionKeys.map( sectionPath => {
-	return {
+export default settingsSectionKeys.reduce( ( acc: any[], sectionPath ) => {
+	acc.push( {
 		label: settingsTabs[ sectionPath ].label,
 		exact: '/' === ( settingsTabs[ sectionPath ].path ?? '' ),
 		path: settingsTabs[ sectionPath ].path ?? `/${ sectionPath }`,
 		activeTabPaths: settingsTabs[ sectionPath ].activeTabPaths ?? undefined,
 		render: sectionComponents[ sectionPath ] ?? sectionComponents.default,
-	};
-} );
+	} );
+	return acc;
+}, [] );
