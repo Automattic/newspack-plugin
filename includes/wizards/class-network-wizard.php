6e95d92e84a0a8ef1ab6fd58b61031fdd1b6612d
ronchambers <ronchambers@gmail.com>
2024-10-26 12:52:04 -0700

    feat(ia-network-settings): adjustments to menu

--- includes/wizards/class-network-wizard.php
+++ includes/wizards/class-network-wizard.php
@@ -8,10 +8,7 @@
 namespace Newspack;
 
 use Newspack\Wizards\Traits\Admin_Header;
-use Newspack_Network\Admin as Newspack_Network_Admin;
 use Newspack_Network\Site_Role as Newspack_Network_Site_Role;
-use Newspack_Network\Hub\Distributor_Settings as Newspack_Network_Hub_Distributor_Settings;
-use Newspack_Network\Node\Settings as Newspack_Network_Node_Settings;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -59,6 +56,9 @@ class Network_Wizard extends Wizard {
 			'np_hub_subscriptions'                  => __( 'Network / Subscriptions', 'newspack-plugin' ),
 		];
 
+		// Remove "Node Settings" from the menu.
+		remove_action( 'admin_menu', [ \Newspack_Network\Node\Settings::class, 'add_menu' ] );
+
 		// Hooks: 'admin_menu'=>'add_page', 'admin_enqueue_scripts'=>'enqueue_scripts_and_styles', 'admin_body_class'=>'add_body_class'.
 		parent::__construct();
 
@@ -81,50 +81,38 @@ class Network_Wizard extends Wizard {
 	 */
 	public function add_page() {
 
-		// see local notes for all the different ways the menu is contructed.
-		
-		global $menu;
+		// Move the entire menu.  See function's docblock for notes.
+		$this->move_menu();
 
-		// Find the Newspack Network menu item in the admin menu.
-		$network_key = null;
-		foreach ( $menu as $k => $v ) {
-			// Get the network key from the menu array.
-			if ( $v[2] === 'newspack-network' ) {
-				$network_key = $k;
-				break;
+		if( 'node' === $this->get_site_role() ) {
+			
+			if ( is_callable( [ \Newspack_Network\Node\Settings::class, 'render' ] ) ) {
+			
+				// Re-add "Node settings" as hidden
+				add_submenu_page(
+					'', // 'newspack-network',
+					__( 'Node Settings', 'newspack-plugin' ),
+					__( 'Node Settings', 'newspack-plugin' ),
+					'manage_options', // copied from original callback.
+					'newspack-network-node',
+					[ \Newspack_Network\Node\Settings::class, 'render' ]
+				);
+				
+				// @todo Need to force the main menu to be Active when on this tab...
+				// also, maybe stop wordpress from condensing the menu when only one item is it...
+				// OR just don't do tabs for is_node ?????
+
 			}
-		}
-		
-		// Verify a key was found.
-		if ( empty( $network_key ) ) {
-			return;
+
 		}
 
-		// Adjust the network menu attributes.
-		// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-		$menu[ $network_key ][0] = __( 'Network', 'newspack-plugin' );
-		// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-		$menu[ $network_key ][6] = 'data:image/svg+xml;base64,' . base64_encode( '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path fill="none" stroke="none" d="M12 3.3c-4.8 0-8.8 3.9-8.8 8.8 0 4.8 3.9 8.8 8.8 8.8 4.8 0 8.8-3.9 8.8-8.8s-4-8.8-8.8-8.8zm6.5 5.5h-2.6C15.4 7.3 14.8 6 14 5c2 .6 3.6 2 4.5 3.8zm.7 3.2c0 .6-.1 1.2-.2 1.8h-2.9c.1-.6.1-1.2.1-1.8s-.1-1.2-.1-1.8H19c.2.6.2 1.2.2 1.8zM12 18.7c-1-.7-1.8-1.9-2.3-3.5h4.6c-.5 1.6-1.3 2.9-2.3 3.5zm-2.6-4.9c-.1-.6-.1-1.1-.1-1.8 0-.6.1-1.2.1-1.8h5.2c.1.6.1 1.1.1 1.8s-.1 1.2-.1 1.8H9.4zM4.8 12c0-.6.1-1.2.2-1.8h2.9c-.1.6-.1 1.2-.1 1.8 0 .6.1 1.2.1 1.8H5c-.2-.6-.2-1.2-.2-1.8zM12 5.3c1 .7 1.8 1.9 2.3 3.5H9.7c.5-1.6 1.3-2.9 2.3-3.5zM10 5c-.8 1-1.4 2.3-1.8 3.8H5.5C6.4 7 8 5.6 10 5zM5.5 15.3h2.6c.4 1.5 1 2.8 1.8 3.7-1.8-.6-3.5-2-4.4-3.7zM14 19c.8-1 1.4-2.2 1.8-3.7h2.6C17.6 17 16 18.4 14 19z"></path></svg>' );
-
-		// Try to move the network item to a higher position near "Newspack".
-		$new_position = '3.6';
-
-		// if position/key collision, keep increasing increment.
-		while ( array_key_exists( $new_position, $menu ) ) {
-			$new_position .= '6';
-		}
-
-		// Move network menu in the array.
-		// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-		$menu[ $new_position ] = $menu[ $network_key ];
-		unset( $menu[ $network_key ] );
 	}
 
 	/**
 	 * Enqueue scripts and styles. Called by parent constructor 'admin_enqueue_scripts'.
 	 */
 	public function enqueue_scripts_and_styles() {
-		// Scripts and styles are enqueued by Admin Header instead of parent class.
+		// Dont load parent.  Scripts and styles are enqueued by Admin Header instead.
 	}
 	
 	/**
@@ -191,9 +179,11 @@ class Network_Wizard extends Wizard {
 	 */
 	private function get_tabs() {
 		
-		$settings_screens = [ 'newspack-network', 'newspack-network-node', 'newspack-network-distributor-settings' ];
+		if ( in_array( $this->get_screen_slug(), [ 'newspack-network', 'newspack-network-node', 'newspack-network-distributor-settings' ], true ) ) {
 
-		if ( in_array( $this->get_screen_slug(), $settings_screens, true ) ) {
+			if ( '' === static::get_site_role() ) {
+				return [];
+			}
 
 			$tabs = [
 				[
@@ -232,6 +222,70 @@ class Network_Wizard extends Wizard {
 	}
 
 	/**
+	 * Move entire menu.
+	 * 
+	 * The code below will move the entire $menu instead of overriding the different add_menu_page/add_submenu_page functions.
+	 * It's just a lot easier to use the code below because the Network Plugin has different submenu pages for each of the
+	 * different Site Roles (none, is_node, is_hub). It became difficult to try to rebuild the menu/submenus based on the current
+	 * Site Role, some of which have a first submenu item of an "admin page" for is_node, but "post type" for is_hub.
+	 * 
+	 * Page/Post Type loading order:
+	 * 
+	 * 	 No site role: MENU PARENT URL: admin.php?page=newspack-network
+	 * 	 	 callback: class-admin.php add_admin_meun - doesn't display, most likely becasue wp is hiding single item menus
+	 * 	 	 callback: node settings (node) add menu  - doesn't show because if is_node is false.
+	 * 	 Node role: MENU PARENT URL: admin.php?page=newspack-network
+	 * 	 	 callback: class-admin.php add_admin_meun - yes it displays
+	 * 	 	 callback: node settings (node) add menu  - yes it displays
+	 * 	 Hub role: MENU PARENT URL: edit.php?post_type=newspack_hub_nodes
+	 * 	 	 callback: hub class nodes register post type - 'show_in_menu' is set to Network_Admin::PAGE_SLUG
+	 * 	 	 callback: hub db subscriptions cpt           - 'show_in_menu' is set to Network_Admin::PAGE_SLUG
+	 * 	 	 callback: hub db orders cpt                  - 'show_in_menu' is set to Network_Admin::PAGE_SLUG
+	 * 	 	 callback: class-admin.php add_admin_meun     - yes it displays (this defines the parent $menu item but shows 4th)
+	 * 	 	 callback: hub event log add menu             - yes it displays
+	 * 	 	 callback: hub membership menu                - yes it displays
+	 * 	 	 callback: hub districtubor settings add menu - yes it displays
+	 */
+	private function move_menu() {
+		
+		global $menu;
+
+		// Find the Newspack Network menu item in the admin menu.
+		$network_key = null;
+		foreach ( $menu as $k => $v ) {
+			// Get the network key from the menu array.
+			if ( $v[2] === 'newspack-network' ) {
+				$network_key = $k;
+				break;
+			}
+		}
+		
+		// Verify a key was found.
+		if ( empty( $network_key ) ) {
+			return;
+		}
+
+		// Adjust the network menu attributes.
+		// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+		$menu[ $network_key ][0] = __( 'Network', 'newspack-plugin' );
+		// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+		$menu[ $network_key ][6] = 'data:image/svg+xml;base64,' . base64_encode( '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" aria-hidden="true" focusable="false"><path fill="none" stroke="none" d="M12 3.3c-4.8 0-8.8 3.9-8.8 8.8 0 4.8 3.9 8.8 8.8 8.8 4.8 0 8.8-3.9 8.8-8.8s-4-8.8-8.8-8.8zm6.5 5.5h-2.6C15.4 7.3 14.8 6 14 5c2 .6 3.6 2 4.5 3.8zm.7 3.2c0 .6-.1 1.2-.2 1.8h-2.9c.1-.6.1-1.2.1-1.8s-.1-1.2-.1-1.8H19c.2.6.2 1.2.2 1.8zM12 18.7c-1-.7-1.8-1.9-2.3-3.5h4.6c-.5 1.6-1.3 2.9-2.3 3.5zm-2.6-4.9c-.1-.6-.1-1.1-.1-1.8 0-.6.1-1.2.1-1.8h5.2c.1.6.1 1.1.1 1.8s-.1 1.2-.1 1.8H9.4zM4.8 12c0-.6.1-1.2.2-1.8h2.9c-.1.6-.1 1.2-.1 1.8 0 .6.1 1.2.1 1.8H5c-.2-.6-.2-1.2-.2-1.8zM12 5.3c1 .7 1.8 1.9 2.3 3.5H9.7c.5-1.6 1.3-2.9 2.3-3.5zM10 5c-.8 1-1.4 2.3-1.8 3.8H5.5C6.4 7 8 5.6 10 5zM5.5 15.3h2.6c.4 1.5 1 2.8 1.8 3.7-1.8-.6-3.5-2-4.4-3.7zM14 19c.8-1 1.4-2.2 1.8-3.7h2.6C17.6 17 16 18.4 14 19z"></path></svg>' );
+
+		// Try to move the network item to a higher position near "Newspack".
+		$new_position = '3.6';
+
+		// if position/key collision, keep increasing increment.
+		while ( array_key_exists( $new_position, $menu ) ) {
+			$new_position .= '6';
+		}
+
+		// Move network menu in the array.
+		// phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
+		$menu[ $new_position ] = $menu[ $network_key ];
+		unset( $menu[ $network_key ] );
+	}
+
+	/**
 	 * Submenu file filter. Used to determine active submenu items.
 	 * 
 	 * For admin pages return slug only.
@@ -241,9 +295,9 @@ class Network_Wizard extends Wizard {
 	 * @return string
 	 */
 	public function submenu_file( $submenu_file ) {
-		if ( isset( $_GET['page'] ) && $_GET['page'] === 'newspack-network-distributor-settings' ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-			return 'newspack-network';
-		}	
+		// if ( isset( $_GET['page'] ) && $_GET['page'] === 'newspack-network-distributor-settings' ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		// 	return 'newspack-network';
+		// }	
 		return $submenu_file;
 	}
 }
