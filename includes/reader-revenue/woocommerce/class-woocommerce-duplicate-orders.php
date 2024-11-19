4353272d4a33c6ba09f19fd3075a0f83e9a1c4e3
Adam Cassis <adam.cassis@automattic.com>
2024-11-19 15:44:48 +0100

    feat: handle order series dismissal

--- includes/reader-revenue/woocommerce/class-woocommerce-duplicate-orders.php
+++ includes/reader-revenue/woocommerce/class-woocommerce-duplicate-orders.php
@@ -16,18 +16,26 @@ class WooCommerce_Duplicate_Orders {
 	const CRON_HOOK_NAME = 'newspack_wc_check_order_series';
 	const ADMIN_NOTICE_TRANSIENT_NAME = 'newspack_wc_check_order_series_admin_notice';
 	const DUPLICATED_ORDERS_OPTION_NAME = 'newspack_wc_order_series';
+	const DISMISSED_DUPLICATE_ORDER_META_NAME = '_newspack_dismissed_duplicate';
 
 	/**
 	 * Initialize.
 	 *
 	 * @codeCoverageIgnore
 	 */
-	public static function init() {
+	public static function init(): void {
 		if ( ! wp_next_scheduled( self::CRON_HOOK_NAME ) ) {
 			wp_schedule_event( time(), 'daily', self::CRON_HOOK_NAME );
 		}
 		add_action( self::CRON_HOOK_NAME, [ __CLASS__, 'check_for_order_series' ] );
 		add_action( 'admin_notices', [ __CLASS__, 'display_admin_notice' ] );
+	}
+
+	/**
+	 * Is this site using HPOS?
+	 */
+	private static function is_using_hpos(): bool {
+		return class_exists( '\Automattic\WooCommerce\Utilities\OrderUtil' ) && \Automattic\WooCommerce\Utilities\OrderUtil::custom_orders_table_usage_is_enabled();
 	}
 
 	/**
@@ -37,12 +45,10 @@ class WooCommerce_Duplicate_Orders {
 	 *
 	 * @param int $series_interval The interval to consider for matching transactions.
 	 */
-	public static function detect_order_series( $series_interval = 10 ) {
+	public static function detect_order_series( $series_interval = 10 ): array {
 		global $wpdb;
 
-		$is_hpos_enabled = class_exists( '\Automattic\WooCommerce\Utilities\OrderUtil' ) && \Automattic\WooCommerce\Utilities\OrderUtil::custom_orders_table_usage_is_enabled();
-
-		if ( $is_hpos_enabled ) {
+		if ( self::is_using_hpos() ) {
 			$query = "
 			SELECT
 				o_billing.email AS email,
@@ -58,6 +64,11 @@ class WooCommerce_Duplicate_Orders {
 				AND o.total_amount > 0.00
 				AND o.status = 'wc-completed'
 				AND o_meta.order_id IS NULL
+				AND o.id NOT IN (
+					SELECT order_id
+					FROM {$wpdb->prefix}wc_orders_meta
+					WHERE meta_key = '" . self::DISMISSED_DUPLICATE_ORDER_META_NAME . "' AND meta_value = '1'
+				)
 				AND EXISTS (
 					SELECT 1
 					FROM {$wpdb->prefix}wc_orders AS o2
@@ -88,6 +99,11 @@ class WooCommerce_Duplicate_Orders {
 			WHERE p.post_type = 'shop_order'
 				AND p.post_status = 'wc-completed'
 				AND pm_email.meta_key = '_billing_email'
+				AND p.ID NOT IN (
+					SELECT post_id
+					FROM {$wpdb->postmeta}
+					WHERE meta_key = '" . self::DISMISSED_DUPLICATE_ORDER_META_NAME . "' AND meta_value = '1'
+				)
 				AND pm_amount.meta_key = '_order_total'
 				AND CAST(pm_amount.meta_value AS DECIMAL(10,2)) > 0.00
 				AND pm_renewal.post_id IS NULL
@@ -111,7 +127,7 @@ class WooCommerce_Duplicate_Orders {
 	/**
 	 * Add an admin notice about the detected order series.
 	 */
-	public static function check_for_order_series() {
+	public static function check_for_order_series(): void {
 		$order_series = self::detect_order_series();
 		if ( empty( $order_series ) ) {
 			return;
@@ -122,7 +138,7 @@ class WooCommerce_Duplicate_Orders {
 	/**
 	 * Display an admin notice if duplicate orders are found.
 	 */
-	public static function display_admin_notice() {
+	public static function display_admin_notice(): void {
 		if ( ! function_exists( 'wc_price' ) ) {
 			return;
 		}
@@ -135,7 +151,9 @@ class WooCommerce_Duplicate_Orders {
 			</p>
 			<ul>
 				<?php foreach ( $order_series as $order_series ) : ?>
-					<li>
+					<li style="display: flex; align-items: center;">
+						<p style="margin: 0;">
+
 						<?php
 						ob_start();
 						?>
@@ -161,12 +179,33 @@ class WooCommerce_Duplicate_Orders {
 							esc_html( date_i18n( get_option( 'date_format' ), strtotime( $order_series['date'] ) ) ),
 							wp_kses_post( trim( $order_list ) )
 						);
+
+						$order_series_id = implode( '-', $order_ids );
 					?>
+						</p>
+						<form method="post" style="display:inline; margin-left: 8px;">
+							<input type="hidden" name="dismiss_order_ids" value="<?php echo esc_attr( $order_series['ids'] ); ?>">
+							<?php submit_button( __( 'Dismiss', 'newspack-plugin' ), 'small', 'dismiss_order', false, [ 'id' => 'dismiss_order_series_' . $order_series_id ] ); ?>
+						</form>
 					</li>
 				<?php endforeach; ?>
 			</ul>
 		</div>
 		<?php
+		if ( isset( $_POST['dismiss_order'] ) && isset( $_POST['dismiss_order_ids'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing
+			$order_ids = explode( ',', sanitize_text_field( wp_unslash( $_POST['dismiss_order_ids'] ) ) ); // phpcs:ignore WordPress.Security.NonceVerification.Missing
+			foreach ( $order_ids as $order_id ) {
+				$wc_order = \wc_get_order( $order_id );
+				if ( $wc_order ) {
+					$wc_order->add_meta_data( self::DISMISSED_DUPLICATE_ORDER_META_NAME, 1 );
+					$wc_order->save();
+				}
+			}
+			self::check_for_order_series();
+			// Refresh the page to reflect changes.
+			wp_safe_redirect( isset( $_SERVER['REQUEST_URI'] ) ? $_SERVER['REQUEST_URI'] : admin_url() );
+			exit;
+		}
 	}
 }
 
