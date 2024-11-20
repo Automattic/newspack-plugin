cb30408601593cebf7da539c1a9e1b9515434ba8
Miguel Peixe <miguel.peixe@automattic.com>
2024-11-20 14:32:10 -0300

    chore: boilerplate for woo configuration

--- src/wizards/audience/views/setup/woocommerce.js
+++ src/wizards/audience/views/setup/woocommerce.js
@@ -1,5 +1,21 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies.
+ */
 import { withWizardScreen } from '../../../../components/src';
+import WizardsTab from '../../../wizards-tab';
 
 export default withWizardScreen( function () {
-	return <p>Checkout & Payment</p>;
+	return (
+		<WizardsTab
+			title={ __( 'Checkout & Payment', 'newspack-plugin' ) }
+			description={ __( 'WooCommerce configuration for donors and subscribers.', 'newspack-plugin' ) }
+		>
+			<p>In progress.</p>
+		</WizardsTab>
+	);
 } );
