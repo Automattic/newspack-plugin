fe8f591fd6c5215e62d6b75462506108c3c61f16
dkoo <derrick.koo@automattic.com>
2024-12-03 16:38:41 -0700

    Revert "test: add unit test for scheduled sync"
    This reverts commit ee581ed1ee34203e847659600fcc3c45ff1d5cb2.

--- tests/mocks/newsletters-mocks.php
+++ tests/mocks/newsletters-mocks.php
@@ -1,11 +1,7 @@
 <?php // phpcs:disable WordPress.Files.FileName.InvalidClassFileName, Squiz.Commenting.FunctionComment.Missing, Squiz.Commenting.ClassComment.Missing, Squiz.Commenting.VariableComment.Missing, Squiz.Commenting.FileComment.Missing, Generic.Files.OneObjectStructurePerFile.MultipleFound, Universal.Files.SeparateFunctionsFromOO.Mixed
 
 if ( ! class_exists( 'Newspack_Newsletters_Contacts' ) ) {
-	class Newspack_Newsletters_Contacts {
-		public static function upsert() {
-			return true;
-		}
-	}
+	class Newspack_Newsletters_Contacts {}
 }
 
 if ( ! class_exists( 'Newspack_Newsletters' ) ) {
