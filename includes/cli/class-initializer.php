e8f694a29c3f85b1cfadeba45b1a8d0a50c4e4f2
Leo Germani <leogermani@automattic.com>
2024-12-11 18:46:28 -0300

    Revert "feat: command to initialize cron job to slowly backfill CAP term data (#3425)"
    This reverts commit 0b0d79abb8a8747abf6b8dc30326f57e11423aa6.

--- includes/cli/class-initializer.php
+++ includes/cli/class-initializer.php
@@ -60,13 +60,6 @@ class Initializer {
 
 		WP_CLI::add_command( 'newspack migrate-co-authors-guest-authors', [ 'Newspack\CLI\Co_Authors_Plus', 'migrate_guest_authors' ] );
 		WP_CLI::add_command( 'newspack backfill-non-editing-contributors', [ 'Newspack\CLI\Co_Authors_Plus', 'backfill_non_editing_contributor' ] );
-		WP_CLI::add_command(
-			'newspack schedule-co-authors-author-term-backfill',
-			[
-				'Newspack\CLI\Co_Authors_Plus',
-				'schedule_author_term_backfill',
-			]
-		);
 		WP_CLI::add_command( 'newspack migrate-expired-subscriptions', [ 'Newspack\CLI\WooCommerce_Subscriptions', 'migrate_expired_subscriptions' ] );
 	}
 }
