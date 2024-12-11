6e949f22f086843175f27cf5dd30ab10507ef4a2
Miguel Peixe <miguel.peixe@automattic.com>
2024-12-11 09:54:03 -0300

    test: execute queued dispatches

--- tests/unit-tests/data-events.php
+++ tests/unit-tests/data-events.php
@@ -77,6 +77,32 @@ class Newspack_Test_Data_Events extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Test that executing queued dispatches triggers the dispatched action hook.
+	 */
+	public function test_execute_queued_dispatches() {
+		$action_name = 'test_action';
+		$data        = [ 'test' => 'data' ];
+
+		$hook_request = null;
+		$hook_queued_dispatches = null;
+
+		$hook = function( $request, $queued_dispatches ) use ( &$hook_request, &$hook_queued_dispatches ) {
+			$hook_request = $request;
+			$hook_queued_dispatches = $queued_dispatches;
+		};
+		add_action( 'newspack_data_events_dispatched', $hook, 10, 2 );
+
+		Data_Events::register_action( $action_name );
+		Data_Events::dispatch( $action_name, $data );
+		Data_Events::execute_queued_dispatches();
+
+		$this->assertIsArray( $hook_request );
+		$this->assertIsArray( $hook_queued_dispatches );
+		$this->assertEquals( $action_name, $hook_queued_dispatches[0]['action_name'] );
+		$this->assertEquals( $data, $hook_queued_dispatches[0]['data'] );
+	}
+
+	/**
 	 * Test triggering the handler.
 	 */
 	public function test_handler() {
