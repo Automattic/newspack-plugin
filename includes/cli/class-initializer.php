982aa27deb4cd0a5fcfecdbfe33c470326b5ebe1
dkoo <derrick.koo@automattic.com>
2024-12-16 13:38:30 -0700

    refactor: move Mailchimp CLI commands to their own class

--- includes/cli/class-initializer.php
+++ includes/cli/class-initializer.php
@@ -26,6 +26,7 @@ class Initializer {
 		include_once NEWSPACK_ABSPATH . 'includes/cli/class-ras.php';
 		include_once NEWSPACK_ABSPATH . 'includes/cli/class-ras-esp-sync.php';
 		include_once NEWSPACK_ABSPATH . 'includes/cli/class-co-authors-plus.php';
+		include_once NEWSPACK_ABSPATH . 'includes/cli/class-mailchimp.php';
 	}
 
 	/**
@@ -58,18 +59,18 @@ class Initializer {
 		);
 
 		WP_CLI::add_command(
-			'newspack esp merge-fields list',
-			[ 'Newspack\CLI\RAS_ESP_Sync', 'cli_mailchimp_list_merge_fields' ]
+			'newspack mailchimp merge-fields list',
+			[ 'Newspack\CLI\Mailchimp', 'cli_mailchimp_list_merge_fields' ]
 		);
 
 		WP_CLI::add_command(
-			'newspack esp merge-fields delete',
-			[ 'Newspack\CLI\RAS_ESP_Sync', 'cli_mailchimp_delete_merge_fields' ]
+			'newspack mailchimp merge-fields delete',
+			[ 'Newspack\CLI\Mailchimp', 'cli_mailchimp_delete_merge_fields' ]
 		);
 
 		WP_CLI::add_command(
-			'newspack esp merge-fields fix-duplicates',
-			[ 'Newspack\CLI\RAS_ESP_Sync', 'cli_mailchimp_fix_duplicate_merge_fields' ]
+			'newspack mailchimp merge-fields fix-duplicates',
+			[ 'Newspack\CLI\Mailchimp', 'cli_mailchimp_fix_duplicate_merge_fields' ]
 		);
 
 		WP_CLI::add_command( 'newspack migrate-co-authors-guest-authors', [ 'Newspack\CLI\Co_Authors_Plus', 'migrate_guest_authors' ] );
