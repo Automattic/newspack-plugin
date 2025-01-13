5a259e9521d74c75967e91b86d65c3d396f75eca
Rasmy Nguyen <raz@automattic.com>
2025-01-13 17:16:44 -0500

    feat: render byline in editor

--- src/bylines/index.js
+++ src/bylines/index.js
@@ -9,21 +9,46 @@ import { ToggleControl, TextareaControl } from '@wordpress/components';
 /**
  * External dependencies
  */
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 /**
  * Internal dependencies
  */
 import './style.scss';
 
 // const ALLOWED_TAGS = [ 'Author' ];
+const BYLINE_ID = 'newspack-byline';
 const META_KEY_ACTIVE = '_newspack_byline_active';
 const META_KEY_BYLINE = '_newspack_byline';
+
+
+/**
+ * Prepend byline to content in the Editor.
+ *
+ * @param {string} byline The byline
+ */
+const prependBylineToContent = byline => {
+	const contentEl = document.querySelector( '.wp-block-post-content' );
+	if ( contentEl && typeof byline === 'string' ) {
+		let bylineEl = document.getElementById( BYLINE_ID );
+		if ( ! bylineEl ) {
+			bylineEl = document.createElement( 'div' );
+			bylineEl.id = BYLINE_ID;
+			contentEl.insertBefore( bylineEl, contentEl.firstChild );
+		}
+		bylineEl.innerHTML = byline;
+	}
+};
 
 const BylinesSettingsPanel = () => {
 	const { editPost } = useDispatch( 'core/editor' );
 	const { getEditedPostAttribute } = useSelect( select => select( 'core/editor' ) );
 	const [ isEnabled, setIsEnabled ] = useState( !! getEditedPostAttribute( 'meta' )[ META_KEY_ACTIVE ] );
 	const [ byline, setByline ] = useState( getEditedPostAttribute( 'meta' )[ META_KEY_BYLINE ] || '' );
+
+	useEffect( () => {
+		prependBylineToContent( isEnabled ? byline : '' );
+	}, [ byline, isEnabled ] );
+
 	// Enabled toggle handler.
 	const handleEnableToggle = value => {
 		editPost( { meta: { [ META_KEY_ACTIVE ]: value } } );
