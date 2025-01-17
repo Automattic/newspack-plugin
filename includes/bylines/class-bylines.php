f8ceb4448c89fb4ac4cb320732db26651a7dbc83
Rasmy Nguyen <raz@automattic.com>
2025-01-17 11:41:00 -0500

    fix: output byline on post

--- includes/bylines/class-bylines.php
+++ includes/bylines/class-bylines.php
@@ -34,6 +34,7 @@ class Bylines {
 		}
 		add_action( 'init', [ __CLASS__, 'register_post_meta' ] );
 		add_action( 'enqueue_block_editor_assets', [ __CLASS__, 'enqueue_block_editor_assets' ] );
+		add_filter( 'the_content', [ __CLASS__, 'output_byline_on_post' ] );
 	}
 
 	/**
@@ -117,6 +118,26 @@ class Bylines {
 	 */
 	public static function auth_callback() {
 		return \current_user_can( 'edit_posts' );
+	}
+
+	/**
+	 * Outputs the byline on the post.
+	 *
+	 * @param string $content The post content.
+	 *
+	 * @return string The post content with the byline prepended.
+	 */
+	public static function output_byline_on_post( $content ) {
+		if ( ! \is_single() || ! \get_post_meta( \get_the_ID(), self::META_KEY_ACTIVE, true ) ) {
+			return $content;
+		}
+		$byline = \get_post_meta( \get_the_ID(), self::META_KEY_BYLINE, true );
+		if ( ! $byline ) {
+			return $content;
+		}
+		$byline      = preg_replace( '/<Author id=(\d*)>(\D*)<\/Author>/', '<a href="' . \get_site_url() . '/?author=$1">$2</a>', $byline );
+		$byline_html = '<div class="newspack-byline">' . \wp_kses_post( $byline ) . '</div>';
+		return $byline_html . $content;
 	}
 }
 Bylines::init();
