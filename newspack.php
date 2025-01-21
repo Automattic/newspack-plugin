3cb4cf98ff114e161d861b735f2e86477369038c
dkoo <derrick.koo@automattic.com>
2025-01-21 12:07:37 -0700

    feat: allow "auto-complete orders" for shippable products too
    BREAKING CHANGE: shippable product orders will auto-complete by default after this change.

--- newspack.php
+++ newspack.php
@@ -14,7 +14,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'NEWSPACK_PLUGIN_VERSION', '5.12.0' );
+define( 'NEWSPACK_PLUGIN_VERSION', '5.12.0--recaptcha-test.8' );
 
 // Define NEWSPACK_PLUGIN_FILE.
 if ( ! defined( 'NEWSPACK_PLUGIN_FILE' ) ) {
