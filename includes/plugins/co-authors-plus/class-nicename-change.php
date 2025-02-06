21d71024afdf6d237d44cf49135571c2f513f047
Leo Germani <leogermani@automattic.com>
2025-02-06 18:57:44 -0300

    feat: check username availability on CLI

--- includes/plugins/co-authors-plus/class-nicename-change.php
+++ includes/plugins/co-authors-plus/class-nicename-change.php
@@ -33,7 +33,29 @@ class Nicename_Change {
 		// add a cli command to update the nicename.
 		if ( defined( 'WP_CLI' ) && WP_CLI ) {
 			\WP_CLI::add_command( 'newspack nicename-change', [ __CLASS__, 'cli_change_nicename' ] );
+			\WP_CLI::add_command( 'newspack nicename-check', [ __CLASS__, 'cli_check_nicename' ] );
 		}
+	}
+
+	/**
+	 * CLI command to check if a nicename is available.
+	 *
+	 * Usage: wp newspack nicename-check <new_nicename>
+	 *
+	 * @param array $args  The arguments.
+	 */
+	public static function cli_check_nicename( $args ) {
+		if ( empty( $args ) ) {
+			\WP_CLI::error( 'Please provide the nicename you want to check.' );
+		}
+
+		$new_nicename = $args[0];
+
+		if ( ! self::cli_check_and_output_nicename( $new_nicename ) ) {
+			\WP_CLI::error( 'Nicename not available.' );
+		}
+
+		\WP_CLI::success( $new_nicename . ' is available.' );
 	}
 
 	/**
@@ -49,15 +71,17 @@ class Nicename_Change {
 		}
 
 		$user_id = (int) $args[0];
-		$new_nicename = $args[1];
-
 		$user_data = get_user_by( 'ID', $user_id );
 
 		if ( ! $user_data ) {
 			\WP_CLI::error( 'User not found.' );
 		}
 
-		$old_nicename = $user_data->user_nicename;
+		$new_nicename = $args[1];
+
+		if ( ! self::cli_check_and_output_nicename( $new_nicename ) ) {
+			\WP_CLI::error( 'Please choose a different nicename or fix the issues above by either removing the users/terms or merging the terms together.' );
+		}
 
 		// Update the nicename.
 		wp_update_user(
@@ -68,6 +92,107 @@ class Nicename_Change {
 		);
 
 		\WP_CLI::success( 'Nicename updated.' );
+	}
+
+	/**
+	 * Checks if the nicename is already in use and outputs it to CLI.
+	 *
+	 * @param string $nicename  The nicename to exclude.
+	 * @return bool True if the nicename is available, false otherwise.
+	 */
+	private static function cli_check_and_output_nicename( $nicename ) {
+		$existing_nicenames = self::get_existing_nicenames( $nicename );
+
+		if ( ! empty( $existing_nicenames ) ) {
+			\WP_CLI::line( 'This nicename is already in use:' );
+
+			foreach ( $existing_nicenames as $existing_nicename ) {
+				\WP_CLI::line( sprintf( ' - %s: %s (ID: %d, %d posts)', $existing_nicename['type'], $existing_nicename['value'], $existing_nicename['id'], $existing_nicename['num_posts'] ) );
+			}
+
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Checks if the nicename is already in use and return the usaged is it is.
+	 *
+	 * This will check both user_nicename and the author taxonomy used by Co-Authors Plus.
+	 *
+	 * @param string $nicename  The nicename to exclude.
+	 * @return array An array with the existing usages of this nicename. Each item of the array will be an array with the following keys:
+	 * * type: The type of usage (user_nicename or author taxonomy).
+	 * * id: The ID of the item using this nicename.
+	 * * value: Either the nicename or the term slug.
+	 * * num_posts: The number of posts associated with this nicename.
+	 */
+	private static function get_existing_nicenames( $nicename ) {
+		$existing_nicenames = [];
+
+		// Check the user_nicename.
+		$user = get_user_by( 'slug', $nicename );
+		if ( $user ) {
+			$existing_nicenames[] = [
+				'type'      => 'WP User',
+				'id'        => $user->ID,
+				'value'     => $user->user_nicename,
+				'num_posts' => self::get_num_of_posts( $nicename ),
+			];
+		}
+
+		// Check the author taxonomy.
+		$term = get_term_by( 'slug', $nicename, 'author' );
+		if ( $term ) {
+			$existing_nicenames[] = [
+				'type'      => 'CAP Author term',
+				'id'        => $term->term_id,
+				'value'     => $term->slug,
+				'num_posts' => self::get_num_of_posts( $nicename ),
+			];
+		}
+		$term = get_term_by( 'slug', 'cap-' . $nicename, 'author' );
+		if ( $term ) {
+			$existing_nicenames[] = [
+				'type'      => 'CAP Author term',
+				'id'        => $term->term_id,
+				'value'     => $term->slug,
+				'num_posts' => self::get_num_of_posts( $nicename ),
+			];
+		}
+
+		return $existing_nicenames;
+	}
+
+	/**
+	 * Gets the number of posts associated with a nicename.
+	 *
+	 * @param string $nicename  The nicename to check.
+	 * @return int The number of posts associated with this nicename.
+	 */
+	private static function get_num_of_posts( $nicename ) {
+		global $coauthors_plus, $wpdb;
+		$cap_enabled = false;
+		if ( is_object( $coauthors_plus ) && method_exists( $coauthors_plus, 'search_authors' ) ) {
+			$cap_enabled = true;
+		}
+
+		if ( $cap_enabled ) {
+			$term = get_term_by( 'slug', $nicename, 'author' );
+			if ( ! $term ) {
+				return 0;
+			}
+			$query = $wpdb->prepare( "SELECT COUNT(object_id) FROM $wpdb->term_relationships WHERE term_taxonomy_id = %d", $term->term_taxonomy_id );
+		} else {
+			$user = get_user_by( 'slug', $nicename );
+			if ( ! $user ) {
+				return 0;
+			}
+			$query = $wpdb->prepare( "SELECT COUNT(ID) FROM $wpdb->posts WHERE post_author = %d", $user->ID );
+		}
+
+		return (int) $wpdb->get_var( $query ); //phpcs:ignore
 	}
 
 	/**
