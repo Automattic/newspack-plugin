7d974075780a73ace6ffe17403ba653c94359e17
Laurel Fulford <laurel.fulford@automattic.com>
2025-02-07 16:29:32 -0800

    fix: adjust spacing; update JavaScript for Transaction Details specifically

--- src/newspack-ui/js/toggle-section/index.js
+++ src/newspack-ui/js/toggle-section/index.js
@@ -1,6 +1,7 @@
+/* globals jQuery */
 import { domReady } from '../utils'; // Global utils.
 
-domReady( function () {
+function update_toggle_section() {
 	function toggle_section( element ) {
 		const toggle_header = element.querySelector( '.newspack-ui__toggle-section__header' );
 		toggle_header.addEventListener( 'click', function () {
@@ -11,4 +12,15 @@ domReady( function () {
 	[ ...document.querySelectorAll( '.newspack-ui__toggle-section' ) ].forEach( x =>
 		toggle_section( x )
 	);
+}
+
+domReady( function () {
+	update_toggle_section();
+
+	// Fire again specifically for the Transaction Details:
+	if ( jQuery ) {
+		jQuery( document ).on( 'updated_checkout', function() {
+			update_toggle_section();
+		} );
+	}
 } );
