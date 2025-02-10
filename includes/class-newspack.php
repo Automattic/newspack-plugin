e2422488ef8f789753ff6034396188cd5df2e806
Miguel Peixe <miguel.peixe@automattic.com>
2025-02-10 11:51:12 -0300

    chore(ia): clean up engagement wizard (#3722)

--- includes/class-newspack.php
+++ includes/class-newspack.php
@@ -170,7 +170,6 @@ final class Newspack {
 		include_once NEWSPACK_ABSPATH . 'includes/wizards/class-newsletters-wizard.php';
 
 		/* Unified Wizards */
-		include_once NEWSPACK_ABSPATH . 'includes/wizards/class-engagement-wizard.php';
 		include_once NEWSPACK_ABSPATH . 'includes/wizards/class-health-check-wizard.php';
 		include_once NEWSPACK_ABSPATH . 'includes/class-wizards.php';
 
--- includes/class-wizards.php
+++ includes/class-wizards.php
@@ -32,7 +32,6 @@ class Wizards {
 			'components-demo'         => new Components_Demo(),
 			'seo'                     => new SEO_Wizard(),
 			'health-check'            => new Health_Check_Wizard(),
-			'engagement'              => new Engagement_Wizard(),
 			// v2 Information Architecture.
 			'newspack-dashboard'      => new Newspack_Dashboard(),
 			'setup'                   => new Setup_Wizard(),
