ee28ba04a119d94f43f04219044cb9fa4f61de26
Rasmy Nguyen <raz@automattic.com>
2025-02-21 15:18:57 -0500

    fix: disable when FF is not present

--- includes/reader-activation/class-reader-activation-emails.php
+++ includes/reader-activation/class-reader-activation-emails.php
@@ -7,6 +7,8 @@
 
 namespace Newspack;
 
+use Newspack\WooCommerce_My_Account;
+
 defined( 'ABSPATH' ) || exit;
 
 /**
@@ -147,22 +149,24 @@ class Reader_Activation_Emails {
 				],
 			],
 		];
-		$configs[ self::EMAIL_TYPES['CHANGE_EMAIL'] ]   = [
-			'name'                   => self::EMAIL_TYPES['CHANGE_EMAIL'],
-			'label'                  => __( 'Change Email', 'newspack-plugin' ),
-			'description'            => __( 'Email sent to the reader when changing email addresses.', 'newspack-plugin' ),
-			'template'               => dirname( NEWSPACK_PLUGIN_FILE ) . '/includes/templates/reader-activation-emails/change-email.php',
-			'editor_notice'          => __( 'This email will be sent to a reader after they\'ve updated their email address.', 'newspack-plugin' ),
-			'available_placeholders' => array_merge(
-				$available_placeholders,
-				[
+		if ( WooCommerce_My_Account::is_email_change_enabled() ) {
+			$configs[ self::EMAIL_TYPES['CHANGE_EMAIL'] ] = [
+				'name'                   => self::EMAIL_TYPES['CHANGE_EMAIL'],
+				'label'                  => __( 'Change Email', 'newspack-plugin' ),
+				'description'            => __( 'Email sent to the reader when changing email addresses.', 'newspack-plugin' ),
+				'template'               => dirname( NEWSPACK_PLUGIN_FILE ) . '/includes/templates/reader-activation-emails/change-email.php',
+				'editor_notice'          => __( 'This email will be sent to a reader after they\'ve updated their email address.', 'newspack-plugin' ),
+				'available_placeholders' => array_merge(
+					$available_placeholders,
 					[
-						'label'    => __( 'the verification link', 'newspack-plugin' ),
-						'template' => '*EMAIL_VERIFICATION_URL*',
-					],
-				]
-			),
-		];
+						[
+							'label'    => __( 'the verification link', 'newspack-plugin' ),
+							'template' => '*EMAIL_VERIFICATION_URL*',
+						],
+					]
+				),
+			];
+		}
 		return $configs;
 	}
 
@@ -204,6 +208,9 @@ class Reader_Activation_Emails {
 	 * @param \WP_User $user User object.
 	 */
 	public static function send_change_email_verification_email( $user ) {
+		if ( ! WooCommerce_My_Account::is_email_change_enabled() ) {
+			return;
+		}
 		Emails::send_email(
 			self::EMAIL_TYPES['CHANGE_EMAIL'],
 			$user->data->user_email,
