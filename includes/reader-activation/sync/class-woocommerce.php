ba115424b0e2a7b62a829a8d341a3a70e4e315fa
dkoo <derrick.koo@automattic.com>
2025-02-24 12:30:33 -0700

    fix(nrh): better check for avoiding fatal when WC_Customer doesn't exist

--- includes/reader-activation/sync/class-woocommerce.php
+++ includes/reader-activation/sync/class-woocommerce.php
@@ -56,7 +56,7 @@ class WooCommerce {
 	 * @return \WC_Order|false Order object or false.
 	 */
 	private static function get_current_product_order_for_sync( $customer ) {
-		if ( ! is_a( $customer, 'WC_Customer' || ! function_exists( 'WC' ) ) ) {
+		if ( ! class_exists( 'WC_Customer' ) || ! is_a( $customer, 'WC_Customer' ) ) {
 			return false;
 		}
 
