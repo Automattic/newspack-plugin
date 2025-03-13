00347082674dd96884ee3a93f63568742a6fcf10
Ron Chambers <116242607+ronchambers@users.noreply.github.com>
2025-03-12 22:29:53 -0700

    fix: guest contributors role init: move line

--- includes/plugins/co-authors-plus/class-guest-contributor-role.php
+++ includes/plugins/co-authors-plus/class-guest-contributor-role.php
@@ -50,16 +50,11 @@ class Guest_Contributor_Role {
 	 */
 	public static function initialize() {
 		add_filter( 'coauthors_edit_author_cap', [ __CLASS__, 'coauthors_edit_author_cap' ] );
-
-		// Setup the custom role directly after global $wp_roles is initialized in wp-settings.php.
-		// Do not use 'admin_init' as this will not account for CLI commands where the admin is not initialized.
-		// @link https://github.com/WordPress/WordPress/blob/414951bc877589f671b0367918069166304662c2/wp-settings.php#L646-L653 .
-		add_action( 'setup_theme', [ __CLASS__, 'setup_custom_role_and_capability' ] );
-
 		add_action( 'template_redirect', [ __CLASS__, 'prevent_myaccount_update' ] );
 		add_action( 'newspack_before_delete_account', [ __CLASS__, 'before_delete_account' ] );
 
 		add_action( 'init', [ __CLASS__, 'early_init' ], 5 );
+		add_action( 'init', [ __CLASS__, 'setup_custom_role_and_capability' ] );
 
 		// Do not allow guest authors to login.
 		\add_filter( 'wp_authenticate_user', [ __CLASS__, 'wp_authenticate_user' ], 10, 2 );
