3fc44af1f55f2776e0eff317c71bafc6c9db5456
Takshil Kunadia <takshilkunadia@gmail.com>
2025-03-14 13:48:13 +0530

    feat(correction-box): upgrade variations and update icons

--- src/blocks/correction-box/variation.js
+++ src/blocks/correction-box/variation.js
@@ -6,9 +6,17 @@
 import { Path, SVG } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
-export const icon = (
+export const topCorrectionsIcon = (
 	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 		<Path d="M4 14.5H20V16H4V14.5ZM4 20H13V18.5H4V20ZM12 5.9L10.1 4L4.7 9.4L4.1 12L6.7 11.4L12.1 6L12 5.9Z" />
+		<Path d="M16 7L20 3L24 7" stroke="#406ebc" stroke-width="1.5" fill="none"/>
+	</SVG>
+);
+
+export const bottomCorrectionsIcon = (
+	<SVG xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+		<Path d="M4 14.5H20V16H4V14.5ZM4 20H13V18.5H4V20ZM12 5.9L10.1 4L4.7 9.4L4.1 12L6.7 11.4L12.1 6L12 5.9Z" />
+		<Path d="M16 3L20 7L24 3" stroke="#406ebc" stroke-width="1.5" fill="none"/>
 	</SVG>
 );
 
@@ -22,23 +30,25 @@ const variations = [
 		name: 'newspack/top-correction-box',
 		title: __( 'Top Corrections Box', 'newspack-plugin' ),
 		icon: {
-			src: icon,
-			foreground: '#668bc9',
+			src: topCorrectionsIcon,
+			foreground: '#406ebc',
 		},
 		keywords: [ __( 'Top', 'newspack-plugin' ), __( 'Corrections', 'newspack-plugin' ), __( 'clarifications', 'newspack-plugin' ), __( 'updates', 'newspack-plugin' ) ],
 		attributes: { location: 'top' },
 		description: __( 'Display corrections whose location is set to "Top".', 'newspack-plugin' ),
+		isActive: [ 'location' ],
 	},
 	{
 		name: "newspack/bottom-correction-box",
 		title: __( 'Bottom Corrections Box', 'newspack-plugin' ),
 		icon: {
-			src: icon,
-			foreground: '#668bc9',
+			src: bottomCorrectionsIcon,
+			foreground: '#406ebc',
 		},
 		keywords: [ __( 'Bottom', 'newspack-plugin' ), __( 'Corrections', 'newspack-plugin' ), __( 'clarifications', 'newspack-plugin' ), __( 'updates', 'newspack-plugin' ) ],
 		attributes: { location: 'bottom' },
 		description: __( 'Display corrections whose location is set to "Bottom".', 'newspack-plugin' ),
+		isActive: [ 'location' ],
 	}
 ];
 
