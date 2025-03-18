09bd17901c8c9b3548f8d747df90696bafde4dd5
Rasmy Nguyen <raz@automattic.com>
2025-03-18 11:57:11 -0400

    fix: reattach submit event listener

--- src/reader-activation-newsletters/newsletters-modal.js
+++ src/reader-activation-newsletters/newsletters-modal.js
@@ -31,19 +31,18 @@ export function refreshNewslettersSignupModal( email ) {
 						.json()
 						.then( ( { html } ) => {
 							if ( html ) {
-								// Create dom node from html.
 								const parser = new DOMParser();
 								const doc = parser.parseFromString( html, 'text/html' );
-								// Remove existing form.
 								const existingForm = container.querySelector( 'form' );
 								if ( existingForm ) {
 									existingForm.remove();
 								}
-								// Append new form.
 								const newForm = doc.querySelector( '.newspack-newsletters-signup form' );
 								if ( newForm ) {
 									container.appendChild( newForm );
 								}
+								// Dispatch refresh event on the container.
+								container.dispatchEvent( new Event( 'newspack:refresh' ) );
 							}
 						} );
 			} );
