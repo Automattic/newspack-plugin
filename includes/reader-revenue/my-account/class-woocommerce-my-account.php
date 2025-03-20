3b1d8e2f99f9f18b2b0340c8eb63dce8e4883d55
Adam Cassis <adam.cassis@automattic.com>
2025-03-20 09:05:36 +0100

    refactor: remove unnecessary ternaries

--- includes/reader-revenue/my-account/class-woocommerce-my-account.php
+++ includes/reader-revenue/my-account/class-woocommerce-my-account.php
@@ -405,10 +405,10 @@ class WooCommerce_My_Account {
 	 */
 	public static function redirect_to_account_details() {
 		// phpcs:disable WordPress.Security.NonceVerification.Recommended
-		$is_resubscribe_request       = isset( $_REQUEST['resubscribe'] ) ? true : false;
-		$is_renewal_request           = isset( $_REQUEST['subscription_renewal'] ) ? true : false;
-		$is_cancel_membership_request = isset( $_REQUEST['cancel_membership'] ) ? true : false;
-		$is_checkout_request          = isset( $_REQUEST['my_account_checkout'] ) ? true : false;
+		$is_resubscribe_request       = isset( $_REQUEST['resubscribe'] );
+		$is_renewal_request           = isset( $_REQUEST['subscription_renewal'] );
+		$is_cancel_membership_request = isset( $_REQUEST['cancel_membership'] );
+		$is_checkout_request          = isset( $_REQUEST['my_account_checkout'] );
 		// phpcs:enable WordPress.Security.NonceVerification.Recommended
 
 		if (
