1b95c052ec7443d74ff6e17fcddf3f3e9a432266
dkoo <derrick.koo@automattic.com>
2025-04-03 10:00:24 -0600

    fix: remove "Plugins" heading (all are now integrated into Newspack Ads)

--- src/wizards/advertising/views/settings/index.js
+++ src/wizards/advertising/views/settings/index.js
@@ -6,7 +6,7 @@
  * Internal dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { PluginSettings, SectionHeader, withWizardScreen } from '../../../../components/src';
+import { PluginSettings, withWizardScreen } from '../../../../components/src';
 import Suppression from '../../components/suppression';
 import AdRefreshControlSettings from '../../components/ad-refresh-control';
 import MediaKitToggle from '../../components/media-kit';
@@ -19,7 +19,6 @@ function Settings() {
 		<>
 			<PluginSettings pluginSlug="newspack-ads" title={ __( 'Settings', 'newspack-plugin' ) }>
 				<Suppression />
-				<SectionHeader heading={ 1 } title={ __( 'Plugins', 'newspack-plugin' ) } />
 				<AdRefreshControlSettings />
 				<MediaKitToggle />
 			</PluginSettings>
