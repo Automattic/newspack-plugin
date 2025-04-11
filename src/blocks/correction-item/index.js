a09948f03aac3c2f9413aea5b842e47b5e6a4124
dkoo <derrick.koo@automattic.com>
2025-04-11 11:19:54 -0600

    feat(scss): export colors as module for JS consumption

--- src/blocks/correction-item/index.js
+++ src/blocks/correction-item/index.js
@@ -9,6 +9,7 @@ import { __ } from '@wordpress/i18n';
  */
 import './style.scss';
 import metadata from './block.json';
+import colors from '../../shared/scss/_colors.module.scss';
 
 export const title = __( 'Correction Item', 'newspack-plugin' );
 
@@ -60,7 +61,7 @@ export const settings = {
 	title,
 	icon: {
 		src: icon,
-		foreground: '#406ebc',
+		foreground: colors['primary-400'],
 	},
 	description: __(
 		'Display an archive of all the corrections and clarifications.',
