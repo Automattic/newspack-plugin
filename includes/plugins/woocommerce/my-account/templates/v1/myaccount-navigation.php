c139d6fe1d1026b41acbdf9775bd12cedfe7f781
dkoo <derrick.koo@automattic.com>
2025-05-13 12:33:44 -0600

    fix: avoid overlap in menu header & footer items

--- includes/plugins/woocommerce/my-account/templates/v1/myaccount-navigation.php
+++ includes/plugins/woocommerce/my-account/templates/v1/myaccount-navigation.php
@@ -20,24 +20,26 @@ do_action( 'woocommerce_before_account_navigation' );
 </div>
 
 <nav class="woocommerce-MyAccount-navigation" aria-label="<?php esc_attr_e( 'Account pages', 'newspack-plugin' ); ?>">
-	<button class="newspack-my-account__icon-button newspack-my-account__icon-button--close-navigation">
-		<span class="screen-reader-text"><?php _e( 'Close navigation', 'newspack-plugin' ); ?></span>
-	</button>
-	<a class="newspack-my-account__site-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" aria-label="<?php esc_attr_e( 'Back to homepage', 'newspack-plugin' ); ?>">
-		<img src="<?php echo esc_url( \get_site_icon_url( 96 ) ); ?>" />
-	</a>
+	<header>
+		<button class="newspack-my-account__icon-button newspack-my-account__icon-button--close-navigation">
+			<span class="screen-reader-text"><?php _e( 'Close navigation', 'newspack-plugin' ); ?></span>
+		</button>
+		<a class="newspack-my-account__site-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" aria-label="<?php esc_attr_e( 'Back to homepage', 'newspack-plugin' ); ?>">
+			<img src="<?php echo esc_url( \get_site_icon_url( 96 ) ); ?>" />
+		</a>
 
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="newspack-my-account__home-link"><?php _e( 'Back to Homepage', 'newspack-plugin' ); ?></a>
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="newspack-my-account__home-link"><?php _e( 'Back to Homepage', 'newspack-plugin' ); ?></a>
 
-	<ul>
-		<?php foreach ( wc_get_account_menu_items() as $endpoint => $label ) : ?>
-			<li class="<?php echo esc_attr( wc_get_account_menu_item_classes( $endpoint ) ); ?>">
-				<a href="<?php echo esc_url( wc_get_account_endpoint_url( $endpoint ) ); ?>" <?php echo wc_is_current_account_menu_item( $endpoint ) ? 'aria-current="page"' : ''; ?>>
-					<?php echo esc_html( $label ); ?>
-				</a>
-			</li>
-		<?php endforeach; ?>
-	</ul>
+		<ul>
+			<?php foreach ( wc_get_account_menu_items() as $endpoint => $label ) : ?>
+				<li class="<?php echo esc_attr( wc_get_account_menu_item_classes( $endpoint ) ); ?>">
+					<a href="<?php echo esc_url( wc_get_account_endpoint_url( $endpoint ) ); ?>" <?php echo wc_is_current_account_menu_item( $endpoint ) ? 'aria-current="page"' : ''; ?>>
+						<?php echo esc_html( $label ); ?>
+					</a>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	</header>
 
 	<footer class="newspack-my-account__navigation-footer">
 		<ul>
