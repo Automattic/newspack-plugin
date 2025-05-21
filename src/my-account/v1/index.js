da37dced32ffa11ade0f0c2a8a46a6d08122a7c6
dkoo <derrick.koo@automattic.com>
2025-05-21 10:14:13 -0600

    refactor: move modals.js to Newspack UI instead of My Account assets

--- src/my-account/v1/index.js
+++ src/my-account/v1/index.js
@@ -5,7 +5,6 @@
 /**
  * Internal dependencies.
  */
-import './modals';
 import './account-settings';
 import './subscriptions';
 import './style.scss';
--- src/newspack-ui/js/index.js
+++ src/newspack-ui/js/index.js
@@ -1,2 +1,3 @@
 import './segmented-control';
+import './modals';
 import './notices';
