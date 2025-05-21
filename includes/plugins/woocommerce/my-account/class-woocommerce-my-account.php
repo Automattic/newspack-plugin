82859fb0d4fefb6072628500f953485d403fee95
dkoo <derrick.koo@automattic.com>
2025-05-21 16:07:26 -0600

    fix: filename for verify.php template part

--- includes/plugins/woocommerce/my-account/class-woocommerce-my-account.php
+++ includes/plugins/woocommerce/my-account/class-woocommerce-my-account.php
@@ -815,7 +815,7 @@ class WooCommerce_My_Account {
 			\add_action(
 				'woocommerce_account_content',
 				function() {
-					include __DIR__ . '/templates/myaccount-verify.php';
+					include __DIR__ . '/templates/verify.php';
 				}
 			);
 		}
