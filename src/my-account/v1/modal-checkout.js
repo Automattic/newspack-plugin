f5a366b2ecdbed25ecce20a2907a82cf3316bad4
Miguel Peixe <miguel.peixe@automattic.com>
2025-05-22 10:17:11 -0300

    feat: modal checkout for My Account's reorders

--- src/my-account/v1/modal-checkout.js
+++ src/my-account/v1/modal-checkout.js
@@ -53,4 +53,18 @@ domReady( () => {
 			}
 		);
 	} );
+
+	/**
+	 * Order again.
+	 */
+	const orderAgain = document.querySelectorAll( 'p.order-again a' );
+	orderAgain.forEach( button => {
+		registerModalCheckoutButton( button, null, 'order_again', data => {
+			// Track the reorder.
+			window.newspackRAS.push( [
+				'product_reordered',
+				{ order_id: data.order_id, product_id: data.product_id },
+			] );
+		} );
+	} );
 } );
