b30a908cb0b3cddcb75a7a615b6b752abcbcb623
dkoo <derrick.koo@automattic.com>
2025-05-26 17:35:42 -0600

    fix: update messaging when resetting existing password

--- includes/plugins/woocommerce/my-account/templates/v1/account-settings.php
+++ includes/plugins/woocommerce/my-account/templates/v1/account-settings.php
@@ -193,10 +193,10 @@ endif;
 	<h1 class="newspack-ui__font--m"><?php \esc_html_e( 'Password', 'newspack-plugin' ); ?></h1>
 	<?php if ( $without_password || ! empty( filter_input( INPUT_GET, My_Account_UI_V1_Passwords::RESET_PASSWORD_URL_PARAM, FILTER_SANITIZE_FULL_SPECIAL_CHARS ) ) ) : ?>
 	<p>
-		<?php \esc_html_e( 'Create a password to secure your account.', 'newspack-plugin' ); ?>
+		<?php echo \esc_html( $without_password ? __( 'Create a password to secure your account.', 'newspack-plugin' ) : __( 'Reset your account password.', 'newspack-plugin' ) ); ?>
 	</p>
 	<a id="newspack-my-account__reset-password" class="woocommerce-Button button primary newspack-ui__button--wide-on-mobile" href="<?php echo '?' . \esc_attr( $newspack_reset_password_arg ) . '=' . \esc_attr( \wp_create_nonce( $newspack_reset_password_arg ) ); ?>">
-		<?php \esc_html_e( 'Create a password', 'newspack-plugin' ); ?>
+		<?php echo \esc_html( $without_password ? __( 'Create a password', 'newspack-plugin' ) : __( 'Reset password', 'newspack-plugin' ) ); ?>
 	</a>
 	<?php else : ?>
 	<form method="post" class="woocommerce-ResetPassword lost_reset_password">
