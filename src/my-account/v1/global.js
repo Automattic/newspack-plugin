f76efacbf3f188d3fcd2b3aa7cd6d7b79259126a
Adam Cassis <adam.cassis@automattic.com>
2025-06-10 23:08:27 +0200

    style: format code

--- src/my-account/v1/global.js
+++ src/my-account/v1/global.js
@@ -8,18 +8,17 @@
 import { domReady } from '../../utils';
 
 domReady( () => {
-	const interactionElements = [
-		'.newspack-ui--block-on-interaction',
-		'.newspack-ui__dropdown__content a',
-
-	];
+	const interactionElements = [ '.newspack-ui--block-on-interaction', '.newspack-ui__dropdown__content a' ];
 	const blockUIonInteraction = [ ...document.querySelectorAll( interactionElements.join( ',' ) ) ];
 	blockUIonInteraction.forEach( element => {
 		const parent = element.closest( 'form, div' );
-		if ( ( 'button' === element.tagName.toLowerCase() || 'input' === element.tagName.toLowerCase() ) && 'form' === parent.tagName.toLowerCase() ) {
+		if (
+			( 'button' === element.tagName.toLowerCase() || 'input' === element.tagName.toLowerCase() ) &&
+			'form' === parent.tagName.toLowerCase()
+		) {
 			parent.addEventListener( 'submit', e => {
 				e.target.classList.add( 'newspack-ui--loading' );
-			} )
+			} );
 		} else {
 			element.addEventListener( 'click', e => {
 				e.target.closest( 'form, div' ).classList.add( 'newspack-ui--loading' );
