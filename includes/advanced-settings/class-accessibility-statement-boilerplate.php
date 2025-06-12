68b96082fba8768a9952028b08cad89bd002407e
Laurel Fulford <laurel.fulford@automattic.com>
2025-06-12 14:08:54 -0700

    fix: update intro text to boilerplate

--- includes/advanced-settings/class-accessibility-statement-boilerplate.php
+++ includes/advanced-settings/class-accessibility-statement-boilerplate.php
@@ -12,7 +12,7 @@
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"fontSize":"small"} -->
-<p class="has-small-font-size"><em>We recommend generating your own Accessibility Statement using </em><a href="https://www.w3.org/WAI/planning/statements/generator/#preview" target="_blank" rel="noreferrer noopener"><em>the tool on the W3C website</em></a><em>. If you use this boilerplate text, please review, make edits as needed, and update the highlighted text with your publication's information.</em></p>
+<p class="has-small-font-size"><em>We recommend generating your own Accessibility Statement using <a href="https://href.li/?https://www.w3.org/WAI/planning/statements/generator/#preview" target="_blank" rel="noreferrer noopener">the tool on the W3C website</a>. If you use this boilerplate text, be sure to review, make edits as needed, and update the <strong>highlighted text</strong> with your publication’s information.</em></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
