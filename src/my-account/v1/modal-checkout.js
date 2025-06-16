1c2d2d0ba9592c967b9bd67536848858d4a5875d
Miguel Peixe <miguel.peixe@automattic.com>
2025-06-16 11:10:59 -0300

    chore: lint

--- src/my-account/v1/modal-checkout.js
+++ src/my-account/v1/modal-checkout.js
@@ -57,22 +57,14 @@ domReady( () => {
 	orderPay.forEach( button => {
 		const subscriptionId = button.dataset?.subscriptionId;
 		const action = button.dataset?.action || 'pay_order';
-		registerModalCheckoutButton(
-			button,
-			button.dataset?.title || null,
-			action,
-			data => {
-				// Track the action.
-				window.newspackRAS.push( [
-					action,
-					{
-						subscription_id:
-							subscriptionId || data.subscription_renewal
-								? data.subscription_renewal
-								: data.subscription_ids?.[ 0 ],
-					},
-				] );
-			}
-		);
+		registerModalCheckoutButton( button, button.dataset?.title || null, action, data => {
+			// Track the action.
+			window.newspackRAS.push( [
+				action,
+				{
+					subscription_id: subscriptionId || data.subscription_renewal ? data.subscription_renewal : data.subscription_ids?.[ 0 ],
+				},
+			] );
+		} );
 	} );
 } );
