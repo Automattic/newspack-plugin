184c3a8e5b36907f926fa71c09641dfc86f7fc6d
claudiulodro <claudiulodro@gmail.com>
2025-06-20 10:54:00 -0700

    fix: add sticky post support to lite site

--- includes/lite-site/templates/archive.php
+++ includes/lite-site/templates/archive.php
@@ -36,13 +36,33 @@ namespace Newspack;
 
 	$recent_posts = get_posts( $query_args );
 
+	// Render sticky posts prominently at the top.
+	$sticky_post_ids_option = get_option( 'sticky_posts' );
+	$sticky_post_ids        = [];
+	if ( ! empty( $sticky_post_ids_option ) ) {
+		$sticky_post_ids = array_values( $sticky_post_ids_option );
+		$sticky_posts    = get_posts( [
+			'post__in' => $sticky_post_ids,
+		] );
+		foreach ( $sticky_posts as $sticky_post ) {
+			printf(
+				'<li><h3><a href="/%s/%d">%s</a></h3></li>',
+				esc_attr( Lite_Site::get_url_base() ),
+				esc_attr( $sticky_post->ID ),
+				esc_html( $sticky_post->post_title )
+			);
+		}
+	}
+
 	foreach ( $recent_posts as $current_post ) {
-		printf(
-			'<li><a href="/%s/%d">%s</a></li>',
-			esc_attr( Lite_Site::get_url_base() ),
-			esc_attr( $current_post->ID ),
-			esc_html( $current_post->post_title )
-		);
+		if ( ! in_array( $current_post->ID, $sticky_post_ids ) ) {
+			printf(
+				'<li><a href="/%s/%d">%s</a></li>',
+				esc_attr( Lite_Site::get_url_base() ),
+				esc_attr( $current_post->ID ),
+				esc_html( $current_post->post_title )
+			);
+		}
 	}
 	?>
 	</ul>
