f0821d3149192e061f9ac938b0067e933cb4ea25
Leo Germani <leogermani@automattic.com>
2025-06-27 15:41:28 -0300

    fix: permission check on newsletter menu

--- includes/wizards/class-newsletters-wizard.php
+++ includes/wizards/class-newsletters-wizard.php
@@ -285,6 +285,10 @@ class Newsletters_Wizard extends Wizard {
 		remove_submenu_page( 'edit.php?post_type=' . Newspack_Newsletters::NEWSPACK_NEWSLETTERS_CPT, 'edit-tags.php?taxonomy=post_tag&amp;post_type=' . Newspack_Newsletters::NEWSPACK_NEWSLETTERS_CPT );
 		remove_submenu_page( 'edit.php?post_type=' . Newspack_Newsletters::NEWSPACK_NEWSLETTERS_CPT, 'edit-tags.php?taxonomy=' . Newspack_Newsletters_Ads::ADVERTISER_TAX . '&amp;post_type=' . Newspack_Newsletters::NEWSPACK_NEWSLETTERS_CPT );
 
+		if ( method_exists( '\Newspack_Newsletters', 'user_can_edit_newsletters' ) && ! \Newspack_Newsletters::user_can_edit_newsletters() ) {
+			return;
+		}
+
 		// Re-add Ads (Advertising) item with updated title. ( See 'remove_action' above. See Newsletters Plugin: Newspack_Newsletters_Ads > 'add_ads_page' ) .
 		add_submenu_page(
 			'edit.php?post_type=' . Newspack_Newsletters::NEWSPACK_NEWSLETTERS_CPT,
