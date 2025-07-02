a8d0300a6660ab235c05e33c58cb10dad168e156
Miguel Peixe <miguel.peixe@automattic.com>
2025-07-02 16:13:32 -0300

    fix(log): ensure `user_email` is always a string

--- includes/class-logger.php
+++ includes/class-logger.php
@@ -116,7 +116,7 @@ class Logger {
 			[
 				'type'       => $type,
 				'data'       => $data,
-				'user_email' => $email,
+				'user_email' => $email ? $email : '', // always cast to string.
 				'file'       => $file,
 				'log_level'  => $log_level,
 			]
