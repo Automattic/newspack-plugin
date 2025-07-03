6493f77dfff593f9baba0d63c426a677e27f9dd6
Ramon Corrales <ramon.corrales@automattic.com>
2025-07-03 01:57:47 -0500

    refactor(collections): make module active checks more atomic for reusability

--- includes/optional-modules/class-collections.php
+++ includes/optional-modules/class-collections.php
@@ -32,8 +32,7 @@ class Collections {
 	 * Initialize the module.
 	 */
 	public static function init() {
-		// Only initialize if the feature is enabled and the module is active.
-		if ( ! self::is_feature_enabled() || ! Optional_Modules::is_optional_module_active( self::MODULE_NAME ) ) {
+		if ( ! self::is_module_active() ) {
 			return;
 		}
 
@@ -61,6 +60,15 @@ class Collections {
 		 * @param bool $is_enabled Whether the Collections module is enabled.
 		 */
 		return apply_filters( 'newspack_collections_enabled', $is_enabled );
+	}
+
+	/**
+	 * Whether the Collections module is enabled and active.
+	 *
+	 * @return bool True if Collections is enabled and active.
+	 */
+	public static function is_module_active() {
+		return self::is_feature_enabled() && Optional_Modules::is_optional_module_active( self::MODULE_NAME );
 	}
 }
 
