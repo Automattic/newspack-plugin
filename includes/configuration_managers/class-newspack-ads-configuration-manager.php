72a6ce93e67a70ae06f83e859c223cccf35cb8e8
Miguel Peixe <miguel.peixe@automattic.com>
2025-07-30 14:21:39 -0300

    chore: lint

--- includes/configuration_managers/class-newspack-ads-configuration-manager.php
+++ includes/configuration_managers/class-newspack-ads-configuration-manager.php
@@ -85,7 +85,7 @@ class Newspack_Ads_Configuration_Manager extends Configuration_Manager {
 	/**
 	 * Get the parent ad units.
 	 *
-	 * @return array | WP_Error Array of ad units or WP_Error if Newspack Ads isn't installed and activated.
+	 * @return array|WP_Error Array of ad units or WP_Error if Newspack Ads isn't installed and activated.
 	 */
 	public function get_parent_ad_units() {
 		return $this->is_configured() ?
