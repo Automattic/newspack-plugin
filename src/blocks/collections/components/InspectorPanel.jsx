17460ca94971cb2c3eb24a9f8a50746192cc5eb6
Thomas Guillot <thomas@automattic.com>
2025-09-04 16:17:20 +0100

    feat(collections): reorganise inspectorpanel

--- src/blocks/collections/components/InspectorPanel.jsx
+++ src/blocks/collections/components/InspectorPanel.jsx
@@ -1,6 +1,15 @@
 import { __ } from '@wordpress/i18n';
 import { InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, RangeControl, ToggleControl, SelectControl, ButtonGroup, Button, BaseControl } from '@wordpress/components';
+import {
+	BaseControl,
+	PanelBody,
+	RangeControl,
+	ToggleControl,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalToggleGroupControl as ToggleGroupControl,
+	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
+	__experimentalToggleGroupControlOption as ToggleGroupControlOption,
+} from '@wordpress/components';
 import { useCallback } from '@wordpress/element';
 
 import { AutocompleteTokenField } from '../../../components/src';
@@ -53,16 +62,22 @@ const InspectorPanel = ( { attributes, setAttributes } ) => {
 
 	return (
 		<InspectorControls>
-			<PanelBody title={ __( 'Query Settings', 'newspack-plugin' ) }>
-				<SelectControl
-					label={ __( 'Query Type', 'newspack-plugin' ) }
+			<PanelBody title={ __( 'Settings', 'newspack-plugin' ) }>
+				<ToggleGroupControl
+					label={ __( 'Mode', 'newspack-plugin' ) }
 					value={ queryType }
-					options={ [
-						{ label: __( 'Recent Collections', 'newspack-plugin' ), value: 'recent' },
-						{ label: __( 'Specific Collections', 'newspack-plugin' ), value: 'specific' },
-					] }
 					onChange={ value => setAttributes( { queryType: value } ) }
-				/>
+					isBlock
+					help={
+						queryType === 'recent'
+							? __( 'The block will display the most recent collection(s).', 'newspack-plugin' )
+							: __( 'The block will display only the specifically selected collection(s).', 'newspack-plugin' )
+					}
+					__next40pxDefaultSize
+				>
+					<ToggleGroupControlOption label={ __( 'Recent', 'newspack-plugin' ) } value="recent" />
+					<ToggleGroupControlOption label={ __( 'Static', 'newspack-plugin' ) } value="specific" />
+				</ToggleGroupControl>
 
 				{ queryType === 'specific' && (
 					<AutocompleteTokenField
@@ -70,19 +85,21 @@ const InspectorPanel = ( { attributes, setAttributes } ) => {
 						onChange={ value => setAttributes( { selectedCollections: value } ) }
 						fetchSuggestions={ fetchCollectionSuggestions }
 						fetchSavedInfo={ fetchSavedCollections }
-						label={ __( 'Select Collections', 'newspack-plugin' ) }
-						help={ __( 'Type to search and select specific collections to display.', 'newspack-plugin' ) }
+						label={ __( 'Collections', 'newspack-plugin' ) }
+						help={ __( 'Begin typing any word in a collection title. Click on an autocomplete result to select it.', 'newspack-plugin' ) }
+						__next40pxDefaultSize
 					/>
 				) }
 
 				{ queryType === 'recent' && (
 					<>
 						<RangeControl
-							label={ __( 'Number of Collections', 'newspack-plugin' ) }
+							label={ __( 'Number of collections', 'newspack-plugin' ) }
 							value={ numberOfItems }
 							onChange={ value => setAttributes( { numberOfItems: value } ) }
 							min={ 1 }
 							max={ 24 }
+							__next40pxDefaultSize
 						/>
 
 						<RangeControl
@@ -92,6 +109,7 @@ const InspectorPanel = ( { attributes, setAttributes } ) => {
 							min={ 0 }
 							max={ 50 }
 							help={ __( 'Number of collections to skip from the beginning', 'newspack-plugin' ) }
+							__next40pxDefaultSize
 						/>
 
 						<AutocompleteTokenField
@@ -99,8 +117,9 @@ const InspectorPanel = ( { attributes, setAttributes } ) => {
 							onChange={ value => setAttributes( { includeCategories: value } ) }
 							fetchSuggestions={ fetchCategorySuggestions }
 							fetchSavedInfo={ fetchSavedCategories }
-							label={ __( 'Include Categories', 'newspack-plugin' ) }
-							help={ __( 'Type to search categories to include.', 'newspack-plugin' ) }
+							label={ __( 'Included categories', 'newspack-plugin' ) }
+							style={ { marginBottom: '16px' } }
+							__next40pxDefaultSize
 						/>
 
 						<AutocompleteTokenField
@@ -108,107 +127,138 @@ const InspectorPanel = ( { attributes, setAttributes } ) => {
 							onChange={ value => setAttributes( { excludeCategories: value } ) }
 							fetchSuggestions={ fetchCategorySuggestions }
 							fetchSavedInfo={ fetchSavedCategories }
-							label={ __( 'Exclude Categories', 'newspack-plugin' ) }
-							help={ __( 'Type to search categories to exclude.', 'newspack-plugin' ) }
+							label={ __( 'Excluded categories', 'newspack-plugin' ) }
+							style={ { marginBottom: '16px' } }
+							__next40pxDefaultSize
 						/>
 					</>
 				) }
+
+				<ToggleControl
+					label={ __( 'Show "See all" link', 'newspack-plugin' ) }
+					checked={ showSeeAllLink }
+					onChange={ value => setAttributes( { showSeeAllLink: value } ) }
+				/>
+
+				{ showSeeAllLink && (
+					<BaseControl id="see-all-text-control" aria-label={ __( '"See all" link text', 'newspack-plugin' ) }>
+						<input
+							type="text"
+							value={ seeAllLinkText ? seeAllLinkText : __( 'See all', 'newspack-plugin' ) }
+							onChange={ e => setAttributes( { seeAllLinkText: e.target.value } ) }
+							placeholder={ __( 'See all', 'newspack-plugin' ) }
+							className="components-text-control__input"
+							style={ { height: '40px' } }
+						/>
+					</BaseControl>
+				) }
 			</PanelBody>
 
-			<PanelBody title={ __( 'Layout Settings', 'newspack-plugin' ) }>
-				<BaseControl id="collections-layout" label={ __( 'Layout', 'newspack-plugin' ) }>
-					<ButtonGroup>
-						<Button isPressed={ layout === 'list' } onClick={ () => setAttributes( { layout: 'list' } ) }>
-							{ __( 'List', 'newspack-plugin' ) }
-						</Button>
-						<Button isPressed={ layout === 'grid' } onClick={ () => setAttributes( { layout: 'grid' } ) }>
-							{ __( 'Grid', 'newspack-plugin' ) }
-						</Button>
-					</ButtonGroup>
-				</BaseControl>
-
-				{ layout === 'grid' && (
+			{ layout === 'grid' && (
+				<PanelBody title={ __( 'Grid', 'newspack-plugin' ) }>
 					<RangeControl
 						label={ __( 'Columns', 'newspack-plugin' ) }
 						value={ columns }
 						onChange={ value => setAttributes( { columns: value } ) }
 						min={ 1 }
 						max={ 6 }
+						__next40pxDefaultSize
 					/>
-				) }
+				</PanelBody>
+			) }
 
-				{ layout === 'list' && (
+			<PanelBody title={ __( 'Featured Image', 'newspack-plugin' ) }>
+				<ToggleControl
+					label={ __( 'Show featured image', 'newspack-plugin' ) }
+					checked={ showFeaturedImage }
+					onChange={ value => setAttributes( { showFeaturedImage: value } ) }
+				/>
+				{ layout === 'list' && showFeaturedImage && (
 					<>
-						<BaseControl id="collections-image-alignment" label={ __( 'Image Alignment', 'newspack-plugin' ) }>
-							<ButtonGroup>
-								<Button isPressed={ imageAlignment === 'top' } onClick={ () => setAttributes( { imageAlignment: 'top' } ) }>
-									{ __( 'Top', 'newspack-plugin' ) }
-								</Button>
-								<Button isPressed={ imageAlignment === 'left' } onClick={ () => setAttributes( { imageAlignment: 'left' } ) }>
-									{ __( 'Left', 'newspack-plugin' ) }
-								</Button>
-								<Button isPressed={ imageAlignment === 'right' } onClick={ () => setAttributes( { imageAlignment: 'right' } ) }>
-									{ __( 'Right', 'newspack-plugin' ) }
-								</Button>
-							</ButtonGroup>
-						</BaseControl>
+						<ToggleGroupControl
+							label={ __( 'Alignment', 'newspack-plugin' ) }
+							value={ imageAlignment }
+							onChange={ value => setAttributes( { imageAlignment: value } ) }
+							isBlock
+							__next40pxDefaultSize
+						>
+							<ToggleGroupControlOption
+								label={ __( 'Top', 'newspack-plugin' ) }
+								value="top"
+								title={ __( 'Show image on top', 'newspack-plugin' ) }
+							/>
+							<ToggleGroupControlOption
+								label={ __( 'Left', 'newspack-plugin' ) }
+								value="left"
+								title={ __( 'Show image on left', 'newspack-plugin' ) }
+							/>
+							<ToggleGroupControlOption
+								label={ __( 'Right', 'newspack-plugin' ) }
+								value="right"
+								title={ __( 'Show image on right', 'newspack-plugin' ) }
+							/>
+						</ToggleGroupControl>
 
-						<BaseControl id="collections-image-size" label={ __( 'Image Size', 'newspack-plugin' ) }>
-							<ButtonGroup>
-								<Button isPressed={ imageSize === 'small' } onClick={ () => setAttributes( { imageSize: 'small' } ) }>
-									{ __( 'Small', 'newspack-plugin' ) }
-								</Button>
-								<Button isPressed={ imageSize === 'medium' } onClick={ () => setAttributes( { imageSize: 'medium' } ) }>
-									{ __( 'Medium', 'newspack-plugin' ) }
-								</Button>
-								<Button isPressed={ imageSize === 'large' } onClick={ () => setAttributes( { imageSize: 'large' } ) }>
-									{ __( 'Large', 'newspack-plugin' ) }
-								</Button>
-							</ButtonGroup>
-						</BaseControl>
+						<ToggleGroupControl
+							label={ __( 'Size', 'newspack-plugin' ) }
+							value={ imageSize }
+							onChange={ value => setAttributes( { imageSize: value } ) }
+							isBlock
+							__next40pxDefaultSize
+						>
+							<ToggleGroupControlOption
+								label={ __( 'S', 'newspack-plugin' ) }
+								value="small"
+								title={ __( 'Small', 'newspack-plugin' ) }
+							/>
+							<ToggleGroupControlOption
+								label={ __( 'M', 'newspack-plugin' ) }
+								value="medium"
+								title={ __( 'Medium', 'newspack-plugin' ) }
+							/>
+							<ToggleGroupControlOption
+								label={ __( 'L', 'newspack-plugin' ) }
+								value="large"
+								title={ __( 'Large', 'newspack-plugin' ) }
+							/>
+						</ToggleGroupControl>
 					</>
 				) }
 			</PanelBody>
 
-			<PanelBody title={ __( 'Display Settings', 'newspack-plugin' ) }>
+			<PanelBody title={ __( 'Collection Meta', 'newspack-plugin' ) }>
 				<ToggleControl
-					label={ __( 'Show Featured Image', 'newspack-plugin' ) }
-					checked={ showFeaturedImage }
-					onChange={ value => setAttributes( { showFeaturedImage: value } ) }
-				/>
-
-				<ToggleControl
-					label={ __( 'Show Category', 'newspack-plugin' ) }
-					checked={ showCategory }
-					onChange={ value => setAttributes( { showCategory: value } ) }
-				/>
-
-				<ToggleControl
-					label={ __( 'Show Title', 'newspack-plugin' ) }
+					label={ __( 'Show title', 'newspack-plugin' ) }
 					checked={ showTitle }
 					onChange={ value => setAttributes( { showTitle: value } ) }
 				/>
 
 				<ToggleControl
-					label={ __( 'Show Excerpt', 'newspack-plugin' ) }
+					label={ __( 'Show category', 'newspack-plugin' ) }
+					checked={ showCategory }
+					onChange={ value => setAttributes( { showCategory: value } ) }
+				/>
+
+				<ToggleControl
+					label={ __( 'Show excerpt', 'newspack-plugin' ) }
 					checked={ showExcerpt }
 					onChange={ value => setAttributes( { showExcerpt: value } ) }
 				/>
 
 				<ToggleControl
-					label={ __( 'Show Period', 'newspack-plugin' ) }
+					label={ __( 'Show period', 'newspack-plugin' ) }
 					checked={ showPeriod }
 					onChange={ value => setAttributes( { showPeriod: value } ) }
 				/>
 
 				<ToggleControl
-					label={ __( 'Show Volume', 'newspack-plugin' ) }
+					label={ __( 'Show volume', 'newspack-plugin' ) }
 					checked={ showVolume }
 					onChange={ value => setAttributes( { showVolume: value } ) }
 				/>
 
 				<ToggleControl
-					label={ __( 'Show Number', 'newspack-plugin' ) }
+					label={ __( 'Show number', 'newspack-plugin' ) }
 					checked={ showNumber }
 					onChange={ value => setAttributes( { showNumber: value } ) }
 				/>
@@ -228,16 +278,17 @@ const InspectorPanel = ( { attributes, setAttributes } ) => {
 							min={ 1 }
 							max={ 5 }
 							help={ __( 'Maximum number of CTAs to display', 'newspack-plugin' ) }
+							__next40pxDefaultSize
 						/>
 
 						<ToggleControl
-							label={ __( 'Show Subscription URL', 'newspack-plugin' ) }
+							label={ __( 'Show subscription URL', 'newspack-plugin' ) }
 							checked={ showSubscriptionUrl }
 							onChange={ value => setAttributes( { showSubscriptionUrl: value } ) }
 						/>
 
 						<ToggleControl
-							label={ __( 'Show Order URL', 'newspack-plugin' ) }
+							label={ __( 'Show order URL', 'newspack-plugin' ) }
 							checked={ showOrderUrl }
 							onChange={ value => setAttributes( { showOrderUrl: value } ) }
 						/>
@@ -256,31 +307,10 @@ const InspectorPanel = ( { attributes, setAttributes } ) => {
 								onChange={ e => setAttributes( { specificCTAs: e.target.value } ) }
 								placeholder={ __( '', 'newspack-plugin' ) }
 								className="components-text-control__input"
+								style={ { height: '40px' } }
 							/>
 						</BaseControl>
 					</>
-				) }
-
-				<ToggleControl
-					label={ __( 'Show "See All" Link', 'newspack-plugin' ) }
-					checked={ showSeeAllLink }
-					onChange={ value => setAttributes( { showSeeAllLink: value } ) }
-				/>
-
-				{ showSeeAllLink && (
-					<BaseControl
-						id="see-all-text-control"
-						label={ __( 'See All Link Text', 'newspack-plugin' ) }
-						help={ __( 'Custom text for the see all link.', 'newspack-plugin' ) }
-					>
-						<input
-							type="text"
-							value={ seeAllLinkText }
-							onChange={ e => setAttributes( { seeAllLinkText: e.target.value } ) }
-							placeholder={ __( 'See all', 'newspack-plugin' ) }
-							className="components-text-control__input"
-						/>
-					</BaseControl>
 				) }
 			</PanelBody>
 		</InspectorControls>
