6007b1de9487d3d256628ff5523ec5b3fca5de42
Rasmy Nguyen <raz@automattic.com>
2025-09-29 17:35:44 -0400

    fix: update pattern

--- includes/plugins/wc-memberships/block-patterns/countdown-subscribe.php
+++ includes/plugins/wc-memberships/block-patterns/countdown-subscribe.php
@@ -6,8 +6,8 @@
  */
 
 ?>
-<!-- wp:group {"className":"is-style-border","layout":{"type":"constrained"}} -->
-<div class="wp-block-group is-style-border">
+<!-- wp:group {"metadata":{"categories":["newspack-memberships"],"patternName":"newspack-memberships/countdown-subscribe","name":"Countdown with Subscribe"},"className":"is-style-border","style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-border" style="padding-top:0;padding-bottom:0">
 <!-- wp:columns {"verticalAlignment":null} -->
 <div class="wp-block-columns">
 <!-- wp:column {"verticalAlignment":"center","width":"20%"} -->
@@ -16,16 +16,16 @@
 <div class="wp-block-group">
 <!-- wp:newspack/content-gate-countdown {"textColor":"primary"} /-->
 
-<!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}},"textColor":"secondary-variation"} -->
-<p class="has-secondary-variation-color has-text-color" style="text-transform:uppercase">Free articles this week</p>
+<!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}},"textColor":"contrast","fontSize":"small"} -->
+<p class="has-contrast-color has-text-color has-small-font-size" style="text-transform:uppercase">Free articles this week</p>
 <!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
 </div>
 <!-- /wp:column -->
 
-<!-- wp:column {"width":"50%"} -->
-<div class="wp-block-column" style="flex-basis:50%">
+<!-- wp:column {"width":"60%"} -->
+<div class="wp-block-column" style="flex-basis:60%">
 <!-- wp:heading {"textAlign":"center","level":4} -->
 <h4 class="wp-block-heading has-text-align-center" id="h-get-unlimited-access">Get unlimited access.</h4>
 <!-- /wp:heading -->
