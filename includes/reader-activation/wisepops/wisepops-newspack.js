5a2cd21c32854700f2f017bf48a894984826ea43
leogermani <leogermani@gmail.com>
2025-10-14 09:41:57 -0300

    add todo

--- includes/reader-activation/wisepops/wisepops-newspack.js
+++ includes/reader-activation/wisepops/wisepops-newspack.js
@@ -7,6 +7,12 @@ wisepops_newspack.keys.forEach(element => {
 if ( window.wisepops && typeof window.wisepops !== 'undefined' ) {
 	window.wisepops( 'properties', newspack_wisepops_data );
 
+	// TODO: Get more data from Reader Activation to pass to Wisepops.
+	// window.newspackRAS = window.newspackRAS || [];
+	// window.newspackRAS.push( function ( ras ) {
+	// 	console.log( ras.store.getAll() );
+	// });
+
 	window.wisepops('listen', 'after-form-submit', function(event) {
 		const emailValue = event.target.elements['email'].value;
 		newspackReaderActivation.dispatchActivity( 'should_register', {
