7eb4cf8f05c061201cb4cbb790d127656fd616f8
Miguel Peixe <miguel.peixe@automattic.com>
2025-10-15 17:09:48 -0300

    fix: hook caller

--- includes/plugins/woocommerce/class-woocommerce-connection.php
+++ includes/plugins/woocommerce/class-woocommerce-connection.php
@@ -63,7 +63,7 @@ class WooCommerce_Connection {
 		\add_action( 'woocommerce_payment_complete', [ __CLASS__, 'order_paid' ], 101 );
 		\add_action( 'woocommerce_after_checkout_validation', [ __CLASS__, 'rate_limit_checkout' ], 10, 2 );
 		\add_filter( 'woocommerce_add_payment_method_form_is_valid', [ __CLASS__, 'rate_limit_payment_methods' ] );
-		\add_action( 'wc_stripe_save_to_subs_checked', '__return_true' );
+		\add_filter( 'wc_stripe_save_to_subs_checked', '__return_true' );
 
 		\add_filter( 'page_template', [ __CLASS__, 'page_template' ] );
 		\add_filter( 'get_post_metadata', [ __CLASS__, 'get_post_metadata' ], 10, 3 );
