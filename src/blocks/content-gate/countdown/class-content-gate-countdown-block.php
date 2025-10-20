fd4be5211929d160a55c44ec418f230216e4a9a9
Miguel Peixe <miguel.peixe@automattic.com>
2025-10-20 15:26:25 -0400

    fix: script dependency

--- src/blocks/content-gate/countdown/class-content-gate-countdown-block.php
+++ src/blocks/content-gate/countdown/class-content-gate-countdown-block.php
@@ -33,7 +33,7 @@ class Content_Gate_Countdown_Block {
 		wp_enqueue_script(
 			'newspack-content-gate-countdown-block',
 			\Newspack\Newspack::plugin_url() . '/dist/content-gate-countdown-block.js',
-			[ 'wp-i18n', 'newspack-memberships-gate-metering' ],
+			[ 'wp-i18n', 'newspack-content-gate-metering' ],
 			NEWSPACK_PLUGIN_VERSION,
 			true
 		);
