60f15105c47116ac8b716310e71c3d1d714e22c5
Laurel Fulford <laurel.fulford@automattic.com>
2025-12-03 14:00:34 -0800

    fix: include dependencies in enqueue script

--- includes/class-blocks.php
+++ includes/class-blocks.php
@@ -47,10 +47,11 @@ final class Blocks {
 		Newspack::load_common_assets();
 
 		// Enqueue universal editor modifications.
+		$editor_asset = require_once dirname( NEWSPACK_PLUGIN_FILE ) . '/dist/editor.asset.php';
 		\wp_enqueue_script(
 			'newspack-editor',
 			Newspack::plugin_url() . '/dist/editor.js',
-			[],
+			$editor_asset['dependencies'],
 			NEWSPACK_PLUGIN_VERSION,
 			true
 		);
