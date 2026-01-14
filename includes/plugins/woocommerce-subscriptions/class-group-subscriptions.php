0cbc7c3bf89cd4987c9313937cdb73a57e911190
dkoo <derrick.koo@automattic.com>
2026-01-14 15:35:55 -0700

    fix: update capability required to edit group subscription settings

--- includes/plugins/woocommerce-subscriptions/class-group-subscriptions.php
+++ includes/plugins/woocommerce-subscriptions/class-group-subscriptions.php
@@ -302,7 +302,7 @@ class Group_Subscriptions {
 	 */
 	public static function ajax_search_users() {
 		check_ajax_referer( 'newspack_group_subscription_search_users', 'nonce' );
-		if ( ! current_user_can( 'edit_posts' ) ) {
+		if ( ! current_user_can( 'manage_woocommerce' ) ) {
 			wp_die( 'Insufficient permissions' );
 		}
 		if ( ! isset( $_POST['search'] ) ) {
