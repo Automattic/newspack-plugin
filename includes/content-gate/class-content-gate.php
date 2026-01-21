880d7340b983a038cd1daaee57f0d56f6570eb75
Miguel Peixe <miguel.peixe@automattic.com>
2026-01-21 09:53:43 -0300

    Update includes/content-gate/class-content-gate.php
    Co-authored-by: Derrick Koo <dkoo@users.noreply.github.com>

--- includes/content-gate/class-content-gate.php
+++ includes/content-gate/class-content-gate.php
@@ -541,7 +541,7 @@ class Content_Gate {
 			}
 			$gate[ $gate_mode ]['gate_layout_id'] = $gate_layout_id;
 			self::update_gate_settings( $gate_id, $gate );
-			\wp_safe_redirect( \admin_url( 'post.php?post=' . $gate_layout_id . '&action=edit' ) );
+			\wp_safe_redirect( \get_edit_post_link( $gate_layout_id, 'edit' ) );
 			exit;
 		}
 	}
