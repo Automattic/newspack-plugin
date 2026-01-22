62cd9c7b598ca87e9553e8349c6bc4c17641e069
Jason Lee <jason.lee@automattic.com>
2026-01-22 15:35:40 -0700

    feat: add support for setting tag labels on term creation

--- includes/tag-labels/class-tag-labels.php
+++ includes/tag-labels/class-tag-labels.php
@@ -99,10 +99,14 @@ class Tag_Labels {
 	 * Initialize hooks.
 	 */
 	public static function init() {
+		add_action( 'post_tag_pre_add_form', array( __CLASS__, 'enqueue_scripts' ) );
 		add_action( 'post_tag_term_edit_form_top', array( __CLASS__, 'enqueue_scripts' ) );
 
+		add_action( 'post_tag_add_form_fields', [ __CLASS__, 'add_term' ] );
 		add_action( 'post_tag_edit_form_fields', [ __CLASS__, 'edit_term' ] );
-		add_action( 'edited_post_tag', [ __CLASS__, 'save_term' ] );
+
+		add_action( 'created_post_tag', [ __CLASS__, 'save_new_term' ] );
+		add_action( 'edited_post_tag', [ __CLASS__, 'save_existing_term' ] );
 	}
 
 	/**
@@ -121,7 +125,45 @@ class Tag_Labels {
 	}
 
 	/**
-	 * Add term edit fields.
+	 * Term creation fields.
+	 *
+	 * Toggle to determine if the term is a label.
+	 * Also, override for flag (text used on label).
+	 */
+	public static function add_term() {
+		$checkbox_id = self::TAG_LABEL_META_KEY;
+		?>
+		<div class="form-field newspack-label-enable term-<?php echo esc_attr( self::TAG_LABEL_META_KEY ); ?>-wrap">
+			<label for="<?php echo esc_attr( $checkbox_id ); ?>"><?php esc_html_e( 'Display as label', 'newspack-plugin' ); ?></label>
+			<input
+				aria-describedby="<?php echo esc_attr( self::TAG_LABEL_META_KEY ); ?>-description"
+				type="checkbox"
+				name="<?php echo esc_attr( $checkbox_id ); ?>"
+				value="true"
+			>
+			<p class="description" id="<?php echo esc_attr( self::TAG_LABEL_META_KEY ); ?>-description">
+				<?php echo esc_html__( 'Show this tag as a highlighted label wherever posts are displayed.', 'newspack-plugin' ); ?>
+			</p>
+		</div>
+		<div class="form-field newspack-label-setting term-<?php echo esc_attr( self::TAG_LABEL_FLAG_META_KEY ); ?>-wrap" style="display: none;">
+			<label for="<?php echo esc_attr( self::TAG_LABEL_FLAG_META_KEY ); ?>"><?php esc_html_e( 'Label text', 'newspack-plugin' ); ?></label>
+			<input
+				aria-describedby="<?php echo esc_attr( self::TAG_LABEL_FLAG_META_KEY ); ?>-description"
+				type="text"
+				name="<?php echo esc_attr( self::TAG_LABEL_FLAG_META_KEY ); ?>"
+				placeholder="Enter custom label text"
+				value=""
+				disabled
+			>
+			<p class="description" id="<?php echo esc_attr( self::TAG_LABEL_FLAG_META_KEY ); ?>-description">
+				<?php echo esc_html__( 'Custom text to display instead of the tag name.', 'newspack-plugin' ); ?>
+			</p>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Term edit fields.
 	 *
 	 * Toggle to determine if the term is a label.
 	 * Also, override for flag (text used on label).
@@ -176,19 +218,13 @@ class Tag_Labels {
 		<?php
 	}
 
+	// phpcs:disable WordPress.Security.NonceVerification.Missing -- nonce verified in accessors
 	/**
-	 * Store our custom term meta when term is saved
+	 * Store custom term meta on save.
 	 *
 	 * @param int $term_id Term ID.
 	 */
-	public static function save_term( $term_id ) {
-
-		// See wp-admin/edit-tag-form.php for where this is set.
-		check_admin_referer( 'update-tag_' . $term_id );
-
-		if ( ! current_user_can( 'edit_term', $term_id ) ) {
-			return;
-		}
+	private static function save_term( $term_id ) {
 
 		// Save label data if label is enabled; otherwise kill it.
 		if ( ! empty( $_POST[ self::TAG_LABEL_META_KEY ] ) ) {
@@ -204,6 +240,38 @@ class Tag_Labels {
 			delete_term_meta( $term_id, self::TAG_LABEL_META_KEY );
 			delete_term_meta( $term_id, self::TAG_LABEL_FLAG_META_KEY );
 		}
+	}
+	// phpcs:enable WordPress.Security.NonceVerification.Missing
+
+	/**
+	 * Save label settings for new term.
+	 *
+	 * @param int $term_id Term ID.
+	 */
+	public static function save_new_term( $term_id ) {
+
+		check_admin_referer( 'add-tag', '_wpnonce_add-tag' );
+		if ( ! current_user_can( 'edit_term', $term_id ) ) {
+			return;
+		}
+
+			self::save_term( $term_id );
+	}
+
+	/**
+	 * Save label settings for existing term.
+	 *
+	 * @param int $term_id Term ID.
+	 */
+	public static function save_existing_term( $term_id ) {
+
+		// See wp-admin/edit-tag-form.php for this nonce.
+		check_admin_referer( 'update-tag_' . $term_id );
+		if ( ! current_user_can( 'edit_term', $term_id ) ) {
+			return;
+		}
+
+		self::save_term( $term_id );
 	}
 }
 
