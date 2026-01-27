a7923822518dfe46f8c04605da274f65d3a694e5
dkoo <derrick.koo@automattic.com>
2026-01-27 10:41:34 -0700

    fix: full-width header buttons instead of dropdown at mobile viewports

--- includes/plugins/woocommerce/my-account/templates/v1/subscription-header.php
+++ includes/plugins/woocommerce/my-account/templates/v1/subscription-header.php
@@ -62,6 +62,7 @@ if ( ! empty( $actions['change_payment_method']['name'] ) ) {
 	}
 	?>
 	<div class="newspack-my-account__subscription--actions">
+		<div class="newspack-my-account__subscription--actions-container">
 		<?php
 		$items = $subscription->get_items();
 		if ( 1 < count( $items ) ) {
@@ -72,7 +73,6 @@ if ( ! empty( $actions['change_payment_method']['name'] ) ) {
 						'wcs-switch-link',
 						'newspack-ui__button',
 						'newspack-ui__button--ghost',
-						'newspack-ui__button--small',
 					];
 				}
 			);
@@ -91,7 +91,7 @@ if ( ! empty( $actions['change_payment_method']['name'] ) ) {
 			);
 			?>
 			<div class="newspack-ui__dropdown newspack-my-account__subscription--change-subscription-dropdown">
-				<button class="newspack-ui__button newspack-ui__button--secondary newspack-ui__button--small newspack-ui__dropdown__toggle">
+				<button class="newspack-ui__button newspack-ui__button--secondary newspack-ui__dropdown__toggle">
 					<span><?php esc_html_e( 'Change subscription', 'newspack-plugin' ); ?></span>
 					<?php Newspack_UI_Icons::print_svg( 'more' ); ?>
 				</button>
@@ -113,7 +113,6 @@ if ( ! empty( $actions['change_payment_method']['name'] ) ) {
 						'wcs-switch-link',
 						'newspack-ui__button',
 						'newspack-ui__button--secondary',
-						'newspack-ui__button--small',
 						'newspack-my-account__subscription--change-subscription-item',
 					];
 				}
@@ -128,6 +127,25 @@ if ( ! empty( $actions['change_payment_method']['name'] ) ) {
 		}
 		?>
 		<?php if ( ! empty( $actions ) ) : ?>
+			<?php foreach ( $actions as $key => $action_link ) : ?>
+				<?php
+				$classes = [
+					'newspack-ui__button',
+					'newspack-my-account__subscription--action-link',
+					\sanitize_html_class( $key ),
+				];
+				if ( 'cancel' === $key ) {
+					$classes[] = 'newspack-ui__button--outline';
+					$classes[] = 'newspack-ui__button--destructive';
+				} else {
+					$classes[] = 'newspack-ui__button--secondary';
+				}
+				?>
+				<a class="<?php echo \esc_attr( implode( ' ', $classes ) ); ?>" href="<?php echo \esc_url( $action_link['url'] ); ?>"><?php echo \esc_html( $action_link['name'] ); ?></a>
+			<?php endforeach; ?>
+		<?php endif; ?>
+		</div>
+		<?php if ( ! empty( $actions ) ) : ?>
 		<div class="newspack-ui__dropdown newspack-my-account__subscription--actions-dropdown">
 			<button class="newspack-ui__button newspack-ui__button--secondary newspack-ui__button--small newspack-ui__dropdown__toggle">
 				<span><?php \esc_html_e( 'More', 'newspack-plugin' ); ?></span>
@@ -135,22 +153,6 @@ if ( ! empty( $actions['change_payment_method']['name'] ) ) {
 			</button>
 			<div class="newspack-ui__dropdown__content">
 				<ul>
-					<?php
-					\add_filter(
-						'woocommerce_subscriptions_switch_link_classes',
-						function( $classes ) {
-							return [
-								'wcs-switch-link',
-								'newspack-ui__button',
-								'newspack-ui__button--ghost',
-								'newspack-ui__button--small',
-							];
-						}
-					);
-					?>
-					<?php foreach ( $items as $item ) : ?>
-						<li class="newspack-my-account__subscription--change-subscription-item"><?php \WC_Subscriptions_Switcher::print_switch_link( $item->get_id(), $item, $subscription ); ?></li>
-					<?php endforeach; ?>
 					<?php foreach ( $actions as $key => $action_link ) : ?>
 						<?php
 						$classes = [
