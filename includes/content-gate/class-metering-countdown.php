63041aee1d3516db19518b0b23a6cf5986aec99f
Miguel Peixe <miguel.peixe@automattic.com>
2026-01-27 10:01:12 -0300

    fix: method name

--- includes/content-gate/class-metering-countdown.php
+++ includes/content-gate/class-metering-countdown.php
@@ -221,7 +221,7 @@ class Metering_Countdown {
 		if ( $views === 0 || Metering::is_frontend_metering() ) {
 			$classes[] = 'newspack-countdown-banner__cta--hidden';
 		}
-		$metering_settings = Metering::get_anonymous_settings( Content_Gate::get_gate_post_id() );
+		$metering_settings = Metering::get_registered_settings( Content_Gate::get_gate_post_id() );
 		$registered_count  = $metering_settings['count'];
 		?>
 		<div class="newspack-ui">
