38b85fbd2dfc072b5beb38a574ae555c0ae43c69
Ramon Corrales <ramon.corrales@automattic.com>
2026-01-30 12:44:39 -0500

    refactor: simplify published collection filtering

--- includes/collections/class-content-inserter.php
+++ includes/collections/class-content-inserter.php
@@ -64,14 +64,9 @@ class Content_Inserter {
 			return;
 		}
 
-		$collections = Query_Helper::get_post_collections( $post );
-		if ( empty( $collections ) ) {
-			return;
-		}
-
 		// Only show indicators for published collections.
 		$collections = array_filter(
-			$collections,
+			Query_Helper::get_post_collections( $post ),
 			function ( $collection_id ) {
 				return 'publish' === get_post_status( $collection_id );
 			}
