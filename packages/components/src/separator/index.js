37f38972c0de89201baaba7d09dffa92d0421204
dkoo <derrick.koo@automattic.com>
2026-02-06 12:08:06 -0700

    refactor: use function-type component syntax

--- packages/components/src/separator/index.js
+++ packages/components/src/separator/index.js
@@ -3,11 +3,6 @@
  */
 
 /**
- * WordPress dependencies
- */
-import { Component } from '@wordpress/element';
-
-/**
  * Internal dependencies
  */
 import './style.scss';
@@ -17,27 +12,20 @@ import './style.scss';
  */
 import classNames from 'classnames';
 
-class Separator extends Component {
-	/**
-	 * Render
-	 */
-	render() {
-		const { alignment = 'full-width', className, marginBottom = 64, marginTop = 64, variant = 'tertiary', ...otherProps } = this.props;
+const Separator = ( { alignment = 'full-width', className, marginBottom = 64, marginTop = 64, variant = 'tertiary', ...otherProps } ) => {
+	const classes = classNames(
+		'newspack-separator',
+		className,
+		alignment && `newspack-separator--alignment-${ alignment }`,
+		variant && `newspack-separator--variant-${ variant }`
+	);
 
-		const classes = classNames(
-			'newspack-separator',
-			className,
-			alignment && `newspack-separator--alignment-${ alignment }`,
-			variant && `newspack-separator--variant-${ variant }`
-		);
+	const style = {
+		'--separator-margin-bottom': typeof marginBottom === 'number' ? `${ marginBottom }px` : marginBottom,
+		'--separator-margin-top': typeof marginTop === 'number' ? `${ marginTop }px` : marginTop,
+	};
 
-		const style = {
-			'--separator-margin-bottom': typeof marginBottom === 'number' ? `${ marginBottom }px` : marginBottom,
-			'--separator-margin-top': typeof marginTop === 'number' ? `${ marginTop }px` : marginTop,
-		};
-
-		return <hr className={ classes } style={ style } { ...otherProps } />;
-	}
-}
+	return <hr className={ classes } style={ style } { ...otherProps } />;
+};
 
 export default Separator;
