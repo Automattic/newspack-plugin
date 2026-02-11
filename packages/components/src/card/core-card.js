3d728c8143d9f0328d6c158ad441c4070a7efe47
dkoo <derrick.koo@automattic.com>
2026-02-11 16:38:20 -0700

    fix: prettier formatting

--- packages/components/src/card/core-card.js
+++ packages/components/src/card/core-card.js
@@ -76,11 +76,7 @@ const CoreCard = ( {
 					{ header && <div className="newspack-card--core__header-content">{ header }</div> }
 					{ actionType === 'chevron' && <Icon className="newspack-card--core__action" icon={ chevronRight } height={ 24 } width={ 24 } /> }
 					{ actionType === 'toggle' && (
-						<ToggleControl
-							className="newspack-card--core__action"
-							checked={ isActive }
-							onChange={ () => {} }
-						/>
+						<ToggleControl className="newspack-card--core__action" checked={ isActive } onChange={ () => {} } />
 					) }
 				</CardHeader>
 			) }
