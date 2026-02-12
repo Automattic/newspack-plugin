a55e583d4547685a2033ec165f26e6b310663e9d
Laurel Fulford <laurel.fulford@automattic.com>
2026-02-12 11:11:55 -0800

    fix: tweaking order

--- includes/plugins/woocommerce/my-account/class-my-account-ui-v1.php
+++ includes/plugins/woocommerce/my-account/class-my-account-ui-v1.php
@@ -185,7 +185,7 @@ class My_Account_UI_V1 {
 			case 'notices/error.php':
 				// Only override error notices on My Account pages to avoid breaking checkout validation.
 				// Guard is_account_page() to avoid running before the main query.
-				if ( \did_action( 'wp' ) && function_exists( 'is_account_page' ) && \is_account_page() ) {
+				if ( function_exists( 'is_account_page' ) && function_exists( 'did_action' ) && \did_action( 'wp' ) && \is_account_page() ) {
 					return __DIR__ . '/templates/v1/notices/error.php';
 				}
 				return $template;
