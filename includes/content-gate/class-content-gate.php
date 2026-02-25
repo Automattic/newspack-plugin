adbc91f83dea18bfcb26aeb30890bcd94718bc92
leogermani <leogermani@gmail.com>
2026-02-25 18:00:48 -0300

    feat: basic cache booster for IP access

--- includes/content-gate/class-content-gate.php
+++ includes/content-gate/class-content-gate.php
@@ -84,6 +84,7 @@ class Content_Gate {
 		include __DIR__ . '/class-metering.php';
 		include __DIR__ . '/class-metering-countdown.php';
 		include __DIR__ . '/content-gifting/class-content-gifting.php';
+		include __DIR__ . '/class-ip-access-rule.php';
 	}
 
 	/**
