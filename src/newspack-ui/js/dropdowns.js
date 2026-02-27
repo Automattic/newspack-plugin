316568f7cdd03cdb31767ccab1709b70a7599ee7
dkoo <derrick.koo@automattic.com>
2026-02-27 15:21:50 -0700

    fix: avoid covering kebab menu icon with dropdown menu

--- src/newspack-ui/js/dropdowns.js
+++ src/newspack-ui/js/dropdowns.js
@@ -32,7 +32,7 @@ domReady( function () {
 				// If content would overflow the bottom edge of the viewport.
 				if ( rect.bottom + rect.height > window.innerHeight ) {
 					content.style.top = 'auto';
-					content.style.bottom = '0';
+					content.style.bottom = '40px';
 				} else {
 					// Reset position if no overflow
 					content.style.removeProperty( 'top' );
