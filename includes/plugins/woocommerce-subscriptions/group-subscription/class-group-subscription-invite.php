f2b519f1faa0c24f54706958117fcff7ad201a4d
dkoo <derrick.koo@automattic.com>
2026-03-02 17:46:23 -0700

    docs: update docblock

--- includes/plugins/woocommerce-subscriptions/group-subscription/class-group-subscription-invite.php
+++ includes/plugins/woocommerce-subscriptions/group-subscription/class-group-subscription-invite.php
@@ -185,7 +185,7 @@ class Group_Subscription_Invite {
 	 * @param \WC_Subscription|int $subscription The subscription object or ID.
 	 * @param string               $email The email address receiving the invitation.
 	 *
-	 * @return bool Whether the invite was cancelled.
+	 * @return bool|WP_Error Whether the invite was cancelled, or a WP_Error if the invite cannot be cancelled.
 	 */
 	public static function cancel_invite( $subscription, $email ) {
 		if ( ! function_exists( 'wcs_get_subscription' ) ) {
