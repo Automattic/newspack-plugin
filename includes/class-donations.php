b9f90938dfefb9bd73cc5729c4ce6140afcceaee
leogermani <leogermani@gmail.com>
2026-04-07 15:54:46 -0300

    fix: get all donation products

--- includes/class-donations.php
+++ includes/class-donations.php
@@ -308,7 +308,7 @@ class Donations {
 		$flagged_products = get_posts(
 			[
 				'post_type'      => 'product',
-				'post_status'    => 'publish',
+				'post_status'    => 'any',
 				'posts_per_page' => -1,
 				'fields'         => 'ids',
 				'meta_query'     => [ // phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query
