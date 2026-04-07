6fd301bf6eb5a41cfb90f30943ee3fece9b3b9fb
leogermani <leogermani@gmail.com>
2026-04-07 15:49:37 -0300

    chore: add memoization

--- includes/class-donations.php
+++ includes/class-donations.php
@@ -298,6 +298,10 @@ class Donations {
 	 * @return int[] Array of product IDs.
 	 */
 	public static function get_flagged_donation_product_ids() {
+		static $memo = null;
+		if ( null !== $memo ) {
+			return $memo;
+		}
 		if ( ! function_exists( 'wc_bool_to_string' ) ) {
 			return [];
 		}
@@ -315,7 +319,8 @@ class Donations {
 				],
 			]
 		);
-		return array_map( 'intval', $flagged_products );
+		$memo = array_map( 'intval', $flagged_products );
+		return $memo;
 	}
 
 	/**
