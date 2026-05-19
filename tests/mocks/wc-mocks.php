406ca541e69a9f805744f37442b5446c26117800
Miguel Peixe <miguel.peixe@automattic.com>
2026-05-19 13:04:00 -0300

    test(wc-subscriptions): add get_total to order-item mock

--- tests/mocks/wc-mocks.php
+++ tests/mocks/wc-mocks.php
@@ -135,6 +135,9 @@ class WC_Order_Item_Product {
 	public function get_subtotal() {
 		return $this->data['subtotal'] ?? 0;
 	}
+	public function get_total() {
+		return $this->data['total'] ?? 0;
+	}
 	public function get_product() {
 		global $products_database;
 		$product_id = $this->data['product_id'] ?? 0;
