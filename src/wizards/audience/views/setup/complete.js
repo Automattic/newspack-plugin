1deffd1dc33336f3c6f31dcceef61c86592ce7f8
Miguel Peixe <miguel.peixe@automattic.com>
2026-05-25 16:43:40 -0300

    refactor(audience): make campaign completion view standalone

--- src/wizards/audience/views/setup/complete.js
+++ src/wizards/audience/views/setup/complete.js
@@ -15,27 +15,11 @@ import WizardsTab from '../../../wizards-tab';
 import { Button, withWizardScreen, Card, Notice, ProgressBar, StepsList } from '../../../../../packages/components/src';
 
 const listItems = [
-	{
-		text: __( 'Your <strong>current segments and prompts</strong> will be deactivated and archived.', 'newspack-plugin' ),
-		isSkipped: '<span class="is-skipped">[skipped]</span>',
-	},
-	{
-		text: __(
-			'<strong>Reader registration</strong> will be activated to enable better targeting for driving engagement and conversations.',
-			'newspack-plugin'
-		),
-	},
-	{
-		text: __( 'The <strong>Audience Management campaign</strong> will be activated with default segments and settings.', 'newspack-plugin' ),
-		isSkipped: '<span class="is-skipped">[skipped]</span>',
-	},
+	__( 'Your <strong>current segments and prompts</strong> will be deactivated and archived.', 'newspack-plugin' ),
+	__( 'The <strong>Audience Management campaign</strong> will be activated with default segments and settings.', 'newspack-plugin' ),
 ];
 
-const DEFAULT_ACTIVATION_STEPS = {
-	campaignsSegments: __( 'Setting up new segments…', 'newspack-plugin' ),
-	readerRegistration: __( 'Activating reader registration…', 'newspack-plugin' ),
-	campaignsPrompts: __( 'Activating Audience Management Campaign…', 'newspack-plugin' ),
-};
+const ACTIVATION_STEPS = [ __( 'Setting up new segments…', 'newspack-plugin' ), __( 'Activating Audience Management Campaign…', 'newspack-plugin' ) ];
 
 /**
  * Get a random number between min and max.
@@ -55,29 +39,7 @@ export default withWizardScreen( ( { fetchConfig } ) => {
 	const [ progressLabel, setProgressLabel ] = useState( false );
 	const [ completed, setCompleted ] = useState( false );
 	const timer = useRef();
-	const [ activationSteps, setActivationSteps ] = useState( Object.values( DEFAULT_ACTIVATION_STEPS ) );
-	const { reader_activation_url, is_skipped_campaign_setup = '' } = newspackAudience;
-	const isSkippedCampaignSetup = is_skipped_campaign_setup === '1';
-
-	useEffect( () => {
-		if ( isSkippedCampaignSetup ) {
-			setActivationSteps( [ DEFAULT_ACTIVATION_STEPS.readerRegistration ] );
-		}
-	}, [ isSkippedCampaignSetup ] );
-
-	/**
-	 * Generate step list strings
-	 */
-	for ( const listItemIndex in listItems ) {
-		if ( ! listItems[ listItemIndex ].text ) {
-			continue;
-		}
-		const suffix = isSkippedCampaignSetup ? ` ${ listItems[ listItemIndex ].isSkipped ?? '' }` : '';
-		listItems[ listItemIndex ] = `${ listItems[ listItemIndex ].text }${ suffix }`;
-		if ( isSkippedCampaignSetup ) {
-			listItems[ listItemIndex ] += ` ${ listItems[ listItemIndex ].isSkipped ?? '' }`;
-		}
-	}
+	const { reader_activation_url } = newspackAudience;
 
 	useEffect( () => {
 		if ( timer.current ) {
@@ -86,8 +48,8 @@ export default withWizardScreen( ( { fetchConfig } ) => {
 		if ( error ) {
 			setInFlight( false );
 		}
-		if ( ! error && inFlight && 0 <= progress && progress < activationSteps.length ) {
-			setProgressLabel( activationSteps[ progress ] );
+		if ( ! error && inFlight && 0 <= progress && progress < ACTIVATION_STEPS.length ) {
+			setProgressLabel( ACTIVATION_STEPS[ progress ] );
 			timer.current = setTimeout(
 				() => {
 					setProgress( _progress => _progress + 1 );
@@ -95,8 +57,8 @@ export default withWizardScreen( ( { fetchConfig } ) => {
 				generateRandomNumber( 1000, 2000 )
 			);
 		}
-		if ( progress >= activationSteps.length && completed ) {
-			setProgress( activationSteps.length + 1 ); // Plus one to account for the "Done!" step.
+		if ( progress >= ACTIVATION_STEPS.length && completed ) {
+			setProgress( ACTIVATION_STEPS.length + 1 ); // Plus one to account for the "Done!" step.
 			setProgressLabel( __( 'Done!', 'newspack-plugin' ) );
 			setTimeout( () => {
 				fetchConfig().finally( () => {
@@ -117,9 +79,6 @@ export default withWizardScreen( ( { fetchConfig } ) => {
 				await apiFetch( {
 					path: '/newspack/v1/wizard/newspack-audience/audience-management/activate',
 					method: 'post',
-					data: {
-						skip_activation: isSkippedCampaignSetup,
-					},
 				} )
 			);
 		} catch ( err ) {
@@ -130,11 +89,11 @@ export default withWizardScreen( ( { fetchConfig } ) => {
 	return (
 		<div className="newspack-ras-campaign__completed">
 			<WizardsTab
-				title={ __( 'Enable Audience Management', 'newspack-plugin' ) }
+				title={ __( 'Audience Management Campaign', 'newspack-plugin' ) }
 				description={
 					<>
 						{ __(
-							'An easy way to let your readers register for your site, sign up for newsletters, or become donors and paid members. ',
+							'Publish a set of prompts with default segments and settings, optimized for audience management. ',
 							'newspack-plugin'
 						) }
 
@@ -148,14 +107,14 @@ export default withWizardScreen( ( { fetchConfig } ) => {
 						<ProgressBar
 							completed={ progress }
 							displayFraction={ false }
-							total={ activationSteps.length + 1 } // Plus one to account for the "Done!" step.
+							total={ ACTIVATION_STEPS.length + 1 } // Plus one to account for the "Done!" step.
 							label={ progressLabel }
 						/>
 					</Card>
 				) }
 				{ ! inFlight && (
 					<Card className="newspack-ras-campaign__completed-card">
-						<h2>{ __( "You're all set to enable Audience Management!", 'newspack-plugin' ) }</h2>
+						<h2>{ __( "You're all set to publish the Audience Management campaign!", 'newspack-plugin' ) }</h2>
 						<p>{ __( 'This is what will happen next:', 'newspack-plugin' ) }</p>
 
 						<Card noBorder className="justify-center">
@@ -166,7 +125,7 @@ export default withWizardScreen( ( { fetchConfig } ) => {
 
 						<Card buttonsCard noBorder className="justify-center">
 							<Button isPrimary onClick={ () => activate() }>
-								{ __( 'Enable Audience Management', 'newspack-plugin' ) }
+								{ __( 'Publish campaign', 'newspack-plugin' ) }
 							</Button>
 						</Card>
 					</Card>
