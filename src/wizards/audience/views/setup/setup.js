b2ab51a0dab5483573178a85fd18152271389c34
Miguel Peixe <miguel.peixe@automattic.com>
2026-05-25 20:49:38 -0300

    fix(audience): gate Newsletters-dependent settings on plugin presence

--- src/wizards/audience/views/setup/setup.js
+++ src/wizards/audience/views/setup/setup.js
@@ -73,6 +73,8 @@ export default withWizardScreen(
 			}
 		}, [ prerequisites ] );
 
+		const hasNewsletters = Boolean( prerequisites?.esp?.plugins?.[ 'newspack-newsletters' ] );
+
 		return (
 			<WizardsTab
 				title={ __( 'Audience Management', 'newspack-plugin' ) }
@@ -134,39 +136,41 @@ export default withWizardScreen(
 				{ config.enabled && (
 					<Card noBorder>
 						<Divider alignment="full-width" variant="tertiary" />
-						<ActionCard
-							title={ __( 'Present newsletter signup after checkout and registration', 'newspack-plugin' ) }
-							description={ __(
-								'Ask readers to sign up for newsletters after creating an account or completing a purchase.',
-								'newspack-plugin'
-							) }
-							hasGreyHeader={ config.use_custom_lists }
-							isMedium
-							toggleChecked={ config.use_custom_lists }
-							toggleOnChange={ value => updateConfig( 'use_custom_lists', value ) }
-						>
-							{ config.use_custom_lists && (
-								<Grid columns={ 4 }>
-									<SortableNewsletterListControl
-										lists={ newspackAudience.available_newsletter_lists }
-										selected={ config.newsletter_lists }
-										onChange={ selected => updateConfig( 'newsletter_lists', selected ) }
-									/>
-									<RangeControl
-										min={ 1 }
-										max={ 10 }
-										initialPosition={ 2 }
-										label={ __( 'Initial list size', 'newspack-plugin' ) }
-										help={ __(
-											'Number of newsletters initially visible during signup. Additional newsletters will be hidden behind a "See all" button.',
-											'newspack-plugin'
-										) }
-										value={ config.newsletter_list_initial_size || '' }
-										onChange={ value => updateConfig( 'newsletter_list_initial_size', parseInt( value ) ) }
-									/>
-								</Grid>
-							) }
-						</ActionCard>
+						{ hasNewsletters && (
+							<ActionCard
+								title={ __( 'Present newsletter signup after checkout and registration', 'newspack-plugin' ) }
+								description={ __(
+									'Ask readers to sign up for newsletters after creating an account or completing a purchase.',
+									'newspack-plugin'
+								) }
+								hasGreyHeader={ config.use_custom_lists }
+								isMedium
+								toggleChecked={ config.use_custom_lists }
+								toggleOnChange={ value => updateConfig( 'use_custom_lists', value ) }
+							>
+								{ config.use_custom_lists && (
+									<Grid columns={ 4 }>
+										<SortableNewsletterListControl
+											lists={ newspackAudience.available_newsletter_lists }
+											selected={ config.newsletter_lists }
+											onChange={ selected => updateConfig( 'newsletter_lists', selected ) }
+										/>
+										<RangeControl
+											min={ 1 }
+											max={ 10 }
+											initialPosition={ 2 }
+											label={ __( 'Initial list size', 'newspack-plugin' ) }
+											help={ __(
+												'Number of newsletters initially visible during signup. Additional newsletters will be hidden behind a "See all" button.',
+												'newspack-plugin'
+											) }
+											value={ config.newsletter_list_initial_size || '' }
+											onChange={ value => updateConfig( 'newsletter_list_initial_size', parseInt( value ) ) }
+										/>
+									</Grid>
+								) }
+							</ActionCard>
+						) }
 
 						<ActionCard
 							title={ __( 'Use My Account login screen for OAuth clients', 'newspack-plugin' ) }
@@ -179,93 +183,97 @@ export default withWizardScreen(
 							toggleOnChange={ value => updateConfig( 'oauth_redirect_to_ras', value ) }
 						/>
 
-						<Divider alignment="full-width" variant="tertiary" />
+						{ hasNewsletters && (
+							<>
+								<Divider alignment="full-width" variant="tertiary" />
 
-						<SectionHeader
-							title={ __( 'Email Service Provider (ESP) Advanced Settings', 'newspack-plugin' ) }
-							description={ __( 'Settings for Newspack Newsletters integration.', 'newspack-plugin' ) }
-						/>
-						<TextControl
-							label={ __( 'Newsletter subscription text on registration', 'newspack-plugin' ) }
-							help={ __( 'The text to display while subscribing to newsletters from the sign-in modal.', 'newspack-plugin' ) }
-							{ ...getSharedProps( 'newsletters_label', 'text' ) }
-						/>
-						<ActionCard
-							description={ __( 'Configure options for syncing reader data to the connected ESP.', 'newspack-plugin' ) }
-							hasGreyHeader={ config.sync_esp }
-							isMedium
-							title={ __( 'Sync contacts to ESP', 'newspack-plugin' ) }
-							toggleChecked={ config.sync_esp }
-							toggleOnChange={ value => updateConfig( 'sync_esp', value ) }
-						>
-							{ config.sync_esp && (
-								<>
-									{ 0 < Object.keys( espSyncErrors ).length && (
-										<Notice noticeText={ Object.values( espSyncErrors ).join( ' ' ) } isError />
-									) }
-									{ esp === 'mailchimp' && (
-										<Settings
-											title={ 'Mailchimp' }
-											value={ {
-												audienceId: config.mailchimp_audience_id,
-												readerDefaultStatus: config.mailchimp_reader_default_status,
-											} }
-											onChange={ ( key, value ) => {
-												if ( key === 'audienceId' ) {
-													updateConfig( 'mailchimp_audience_id', value );
-												}
-												if ( key === 'readerDefaultStatus' ) {
-													updateConfig( 'mailchimp_reader_default_status', value );
-												}
-											} }
-										/>
-									) }
-									{ esp === 'active_campaign' && (
-										<Settings
-											title={ 'ActiveCampaign' }
-											value={ {
-												masterList: config.active_campaign_master_list,
-											} }
-											onChange={ ( key, value ) => {
-												if ( key === 'masterList' ) {
-													updateConfig( 'active_campaign_master_list', value );
-												}
-											} }
-										/>
-									) }
-									{ esp === 'constant_contact' && (
-										<Settings
-											title={ 'Constant Contact' }
-											value={ { masterList: config.constant_contact_list_id } }
-											onChange={ ( key, value ) => {
-												if ( key === 'masterList' ) {
-													updateConfig( 'constant_contact_list_id', value );
-												}
-											} }
-										/>
-									) }
+								<SectionHeader
+									title={ __( 'Email Service Provider (ESP) Advanced Settings', 'newspack-plugin' ) }
+									description={ __( 'Settings for Newspack Newsletters integration.', 'newspack-plugin' ) }
+								/>
+								<TextControl
+									label={ __( 'Newsletter subscription text on registration', 'newspack-plugin' ) }
+									help={ __( 'The text to display while subscribing to newsletters from the sign-in modal.', 'newspack-plugin' ) }
+									{ ...getSharedProps( 'newsletters_label', 'text' ) }
+								/>
+								<ActionCard
+									description={ __( 'Configure options for syncing reader data to the connected ESP.', 'newspack-plugin' ) }
+									hasGreyHeader={ config.sync_esp }
+									isMedium
+									title={ __( 'Sync contacts to ESP', 'newspack-plugin' ) }
+									toggleChecked={ config.sync_esp }
+									toggleOnChange={ value => updateConfig( 'sync_esp', value ) }
+								>
+									{ config.sync_esp && (
+										<>
+											{ 0 < Object.keys( espSyncErrors ).length && (
+												<Notice noticeText={ Object.values( espSyncErrors ).join( ' ' ) } isError />
+											) }
+											{ esp === 'mailchimp' && (
+												<Settings
+													title={ 'Mailchimp' }
+													value={ {
+														audienceId: config.mailchimp_audience_id,
+														readerDefaultStatus: config.mailchimp_reader_default_status,
+													} }
+													onChange={ ( key, value ) => {
+														if ( key === 'audienceId' ) {
+															updateConfig( 'mailchimp_audience_id', value );
+														}
+														if ( key === 'readerDefaultStatus' ) {
+															updateConfig( 'mailchimp_reader_default_status', value );
+														}
+													} }
+												/>
+											) }
+											{ esp === 'active_campaign' && (
+												<Settings
+													title={ 'ActiveCampaign' }
+													value={ {
+														masterList: config.active_campaign_master_list,
+													} }
+													onChange={ ( key, value ) => {
+														if ( key === 'masterList' ) {
+															updateConfig( 'active_campaign_master_list', value );
+														}
+													} }
+												/>
+											) }
+											{ esp === 'constant_contact' && (
+												<Settings
+													title={ 'Constant Contact' }
+													value={ { masterList: config.constant_contact_list_id } }
+													onChange={ ( key, value ) => {
+														if ( key === 'masterList' ) {
+															updateConfig( 'constant_contact_list_id', value );
+														}
+													} }
+												/>
+											) }
 
-									<SectionHeader
-										title={ __( 'Sync user account deletion', 'newspack-plugin' ) }
-										description={ __(
-											'If enabled, the contact will be deleted from the ESP when a user account is deleted. If disabled, the contact will be unsubscribed from all lists, but not deleted.',
-											'newspack-plugin'
-										) }
-									/>
-									<CheckboxControl
-										label={ __( 'Sync user account deletion', 'newspack-plugin' ) }
-										checked={ config.sync_esp_delete }
-										onChange={ value => updateConfig( 'sync_esp_delete', value ) }
-									/>
-									<MetadataFields
-										availableFields={ newspackAudience.esp_metadata_fields || [] }
-										selectedFields={ config.metadata_fields }
-										updateConfig={ updateConfig }
-										getSharedProps={ getSharedProps }
-									/>
-								</>
-							) }
-						</ActionCard>
+											<SectionHeader
+												title={ __( 'Sync user account deletion', 'newspack-plugin' ) }
+												description={ __(
+													'If enabled, the contact will be deleted from the ESP when a user account is deleted. If disabled, the contact will be unsubscribed from all lists, but not deleted.',
+													'newspack-plugin'
+												) }
+											/>
+											<CheckboxControl
+												label={ __( 'Sync user account deletion', 'newspack-plugin' ) }
+												checked={ config.sync_esp_delete }
+												onChange={ value => updateConfig( 'sync_esp_delete', value ) }
+											/>
+											<MetadataFields
+												availableFields={ newspackAudience.esp_metadata_fields || [] }
+												selectedFields={ config.metadata_fields }
+												updateConfig={ updateConfig }
+												getSharedProps={ getSharedProps }
+											/>
+										</>
+									) }
+								</ActionCard>
+							</>
+						) }
 						<div className="newspack-buttons-card">
 							<Button
 								isPrimary
