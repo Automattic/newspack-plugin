c769f22499c842f895dc4b73bd45cf28f22d08af
Thomas Guillot <thomas@automattic.com>
2026-05-26 12:49:28 +0100

    refactor(blocks): move style registration to block.json

--- src/blocks/overlay-search/index.js
+++ src/blocks/overlay-search/index.js
@@ -1,8 +1,6 @@
 /**
  * WordPress dependencies
  */
-import { registerBlockStyle } from '@wordpress/blocks';
-import { __ } from '@wordpress/i18n';
 import { search as icon } from '@wordpress/icons';
 
 /**
@@ -16,22 +14,6 @@ import './style.scss';
 const { name } = metadata;
 
 export { metadata, name };
-
-registerBlockStyle( name, {
-	name: 'default',
-	label: __( 'Default', 'newspack-plugin' ),
-	isDefault: true,
-} );
-
-registerBlockStyle( name, {
-	name: 'icon-only',
-	label: __( 'Icon only', 'newspack-plugin' ),
-} );
-
-registerBlockStyle( name, {
-	name: 'text-only',
-	label: __( 'Text only', 'newspack-plugin' ),
-} );
 
 export const settings = {
 	title: metadata.title,
